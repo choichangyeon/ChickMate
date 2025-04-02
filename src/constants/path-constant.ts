98229c9f6a8c46cf615cc9a1ffa0a14d9493ae85
Hye10 <dhye410@daum.net>
2025-04-02 17:08:31 +0900

    fix:#9 middleware 상수 처리 및 private menu 상수 삭제 - middleware config 파일에는 상수 사용 불가 (빌드타임에 평가되기 때문)

--- src/constants/path-constant.ts
+++ src/constants/path-constant.ts
@@ -5,6 +5,3 @@ export const PATH = {
   ON_BOARDING: '/on-boarding',
   MY_PAGE: '/my-page',
 };
-
-const { MY_PAGE } = PATH;
-export const PRIVATE_PATHS = [MY_PAGE]; //권한 없는 사용자 접근 막을 페이지 여기에 넣어주세요!!
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,12 @@
 import { NextRequest, NextResponse } from 'next/server';
+import { PATH } from './constants/path-constant';
 
-export function middleware(request: NextRequest) {
+const { ON_BOARDING } = PATH;
+export const middleware = (request: NextRequest) => {
   const isOurUser = !!request.cookies.get('next-auth.session-token');
-  if (!isOurUser) return NextResponse.redirect(new URL('/on-boarding', request.url));
+  if (!isOurUser) return NextResponse.redirect(new URL(ON_BOARDING, request.url));
   return NextResponse.next();
-}
+};
 
 export const config = {
   matcher: ['/my-page'],

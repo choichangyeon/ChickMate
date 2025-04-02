541fcb4d115afb7462edbf3e80ae194616970669
ImJaeOne <dlawi0108@naver.com>
2025-04-02 22:45:56 +0900

    feat:#17 회원가입 message 상수 분리

--- src/constants/message-constants.ts
+++ src/constants/message-constants.ts
@@ -6,4 +6,7 @@ export const AUTH_MESSAGE = {
     PASSWORD_LENGTH: '비밀번호는 6자 이상이어야 합니다.',
     PASSWORD_SPECIAL_CHAR: '비밀번호는 최소 하나의 특수 문자를 포함해야 합니다.',
   },
+  RESULT: {
+    SIGN_UP_SUCCESS: '회원 가입에 성공했습니다.',
+  },
 };

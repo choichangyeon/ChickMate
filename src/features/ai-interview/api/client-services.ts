a5bd41f0bbb3c28c4c3942b07b4066d3267e4674
choichangyeon <changyon99@naver.com>
2025-04-02 22:15:30 +0900

    feat:#18 Route Handler 요청 구현

--- src/features/ai-interview/api/client-services.ts
+++ src/features/ai-interview/api/client-services.ts
@@ -1,1 +1,56 @@
-const playTextToSpeech = async (text, voice = 'nova') => {};
+/**
+ *
+ * @param text - 서버로부터 받아온 텍스트 입력
+ * @param model - 고정된 TTS 모델 ('gpt-4o-mini-tts')
+ * @param voice - default = calm('ash'), pressure('sage')
+ * @param speed - default = calm(1), pressure(2.5)
+ * @param response_format  - format 형식 default = 'wav'
+ * @returns {void}
+ */
+
+type Props = {
+  text: string;
+  model?: string;
+  voice?: string;
+  speed?: number;
+  response_format?: string;
+};
+
+const DEFAULT_OPTIONS = {
+  MODEL: 'gpt-4o-mini-tts',
+  VOICE: 'ash',
+  SPEED: 1,
+  FORMAT: 'wav',
+};
+
+export const speakText = async ({
+  text,
+  model = DEFAULT_OPTIONS.MODEL,
+  voice = DEFAULT_OPTIONS.VOICE,
+  speed = DEFAULT_OPTIONS.SPEED,
+  response_format = DEFAULT_OPTIONS.FORMAT,
+}: Props) => {
+  const res = await fetch('/api/ai', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      text,
+      model,
+      voice,
+      speed,
+      response_format,
+    }),
+  });
+
+  const data = await res.json();
+  console.log(data);
+
+  if (res.status !== 200) {
+    // TODO : ERROR 처리
+    console.error('TTS 요청 실패', data.error || data.message);
+    return;
+  }
+
+  const audio = new Audio(data.audio_url);
+  audio.play();
+};

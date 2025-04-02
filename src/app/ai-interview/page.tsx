c0b355cd8deea37d5000bda3392df0d7e6d91f53
choichangyeon <changyon99@naver.com>
2025-04-02 15:52:20 +0900

    feat:#18 TTS Component

---
+++ src/app/ai-interview/page.tsx
@@ -0,0 +1,11 @@
+import TTSComponent from '@/features/ai-interview/test-component';
+
+const page = () => {
+  return (
+    <section>
+      <TTSComponent />
+    </section>
+  );
+};
+
+export default page;
---
+++ src/features/ai-interview/test-component.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+const TTSComponent = () => {
+  const handleClick = async () => {};
+  return (
+    <div>
+      <button onClick={handleClick}>테스트</button>
+    </div>
+  );
+};
+
+export default TTSComponent;

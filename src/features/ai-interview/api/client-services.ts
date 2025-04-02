bdbd26aa6c6bca703eed11384a85b1d66f6f6929
choichangyeon <changyon99@naver.com>
2025-04-02 22:53:18 +0900

    feat:#18 Options 설정

--- src/features/ai-interview/api/client-services.ts
+++ src/features/ai-interview/api/client-services.ts
@@ -5,45 +5,50 @@
  * @param voice - default = calm('ash'), pressure('sage')
  * @param speed - default = calm(1), pressure(2.5)
  * @param response_format  - format 형식 default = 'wav'
+ * @param instruction  - 목소리 형식 default = calm
  * @returns {void}
  */
 
 type Props = {
   text: string;
-  model?: string;
-  voice?: string;
-  speed?: number;
-  response_format?: string;
+  type: 'PRESSURE' | 'CALM';
 };
 
 const DEFAULT_OPTIONS = {
   MODEL: 'gpt-4o-mini-tts',
-  VOICE: 'ash',
-  SPEED: 1,
   FORMAT: 'wav',
 };
 
-export const speakText = async ({
-  text,
-  model = DEFAULT_OPTIONS.MODEL,
-  voice = DEFAULT_OPTIONS.VOICE,
-  speed = DEFAULT_OPTIONS.SPEED,
-  response_format = DEFAULT_OPTIONS.FORMAT,
-}: Props) => {
+const CALM_OPTIONS = {
+  VOICE: 'ash',
+  SPEED: 1,
+  INSTRUCTION: `Uses a friendly and gentle tone of voice.
+  Rather than challenging the candidate's answers, frequently provides emotional empathy or positive reactions.`,
+};
+
+const PRESSURE_OPTIONS = {
+  VOICE: 'sage',
+  SPEED: 2.5,
+  INSTRUCTION: `Uses a firm and dry tone of voice.
+Avoids showing emotional empathy or positive reactions to the candidate's responses.`,
+};
+
+export const speakText = async ({ text, type }: Props) => {
+  const options = type === 'PRESSURE' ? PRESSURE_OPTIONS : CALM_OPTIONS;
   const res = await fetch('/api/ai', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
       text,
-      model,
-      voice,
-      speed,
-      response_format,
+      model: DEFAULT_OPTIONS.MODEL,
+      response_format: DEFAULT_OPTIONS.FORMAT,
+      voice: options.VOICE,
+      speed: options.SPEED,
+      instruction: options.INSTRUCTION,
     }),
   });
 
   const data = await res.json();
-  console.log(data);
 
   if (res.status !== 200) {
     // TODO : ERROR 처리

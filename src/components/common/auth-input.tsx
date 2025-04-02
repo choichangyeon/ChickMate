1081c1661bdfe78af923a1a723997f75ebd3ad6b
ImJaeOne <dlawi0108@naver.com>
2025-04-02 22:45:32 +0900

    feat:#17 AuthInput 공통 컴포넌트 구현

---
+++ src/components/common/auth-input.tsx
@@ -0,0 +1,20 @@
+import { FormData } from '@/features/auth/sign-up/sign-up-auth-form';
+import { UseFormRegister } from 'react-hook-form';
+
+type AuthInputProps = {
+  label: string;
+  id: keyof FormData;
+  register: UseFormRegister<FormData>;
+  error?: { message?: string };
+  type: string;
+};
+
+const AuthInput = ({ label, id, register, error, type }: AuthInputProps) => (
+  <div className='flex w-full flex-col px-3 py-2'>
+    <label>{label}</label>
+    <input {...register(id)} type={type} className='border-b border-gray-300' />
+    <div className='h-6 text-sm'>{error && <p className='text-primary'>{error.message}</p>}</div>
+  </div>
+);
+
+export default AuthInput;

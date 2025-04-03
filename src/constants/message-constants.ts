e5eb4001c9d295b0139868bee66b21891f999173
ImJaeOne <dlawi0108@naver.com>
2025-04-03 16:18:09 +0900

    refactor:#17 이름 관련 메세지 변경

--- src/constants/message-constants.ts
+++ src/constants/message-constants.ts
@@ -1,7 +1,7 @@
 export const AUTH_MESSAGE = {
   VALIDATION: {
-    NAME_LENGTH: '닉네임은 2~8자여야 합니다.',
-    NAME_SPECIAL_CHAR: '닉네임에는 특수 문자를 포함할 수 없습니다.',
+    NAME_LENGTH: '이름은 2~8자여야 합니다.',
+    NAME_SPECIAL_CHAR: '이름에는 특수 문자를 포함할 수 없습니다.',
     EMAIL_INVALID: '올바른 이메일 형식이어야 합니다.',
     PASSWORD_LENGTH: '비밀번호는 6자 이상이어야 합니다.',
     PASSWORD_SPECIAL_CHAR: '비밀번호는 최소 하나의 특수 문자를 포함해야 합니다.',

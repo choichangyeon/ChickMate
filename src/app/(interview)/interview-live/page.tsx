83d897c083f211c5301d39a0d9f0614bd9b0a465
choichangyeon <changyon99@naver.com>
2025-04-04 15:56:13 +0900

    feat:#18 stt 컴포넌트 추가

--- src/app/(interview)/interview-live/page.tsx
+++ src/app/(interview)/interview-live/page.tsx
@@ -1,13 +1,11 @@
 import CameraView from '@/features/interview/camera-view';
 import VoiceInputButton from '@/features/interview/voice-input-button';
-import STTComponent from '@/features/ai-interview/stt-component';
 
 const InterviewPage = () => {
   return (
     <div>
       <CameraView />
       <VoiceInputButton />
-      <STTComponent />
     </div>
   );
 };

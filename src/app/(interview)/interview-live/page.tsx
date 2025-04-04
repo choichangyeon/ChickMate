2a7d37876dda12f7a911924232e327674985f2b0
Minjo Park <consistent.312@gmail.com>
2025-04-04 12:59:25 +0900

    refactor:#28 녹음 버튼 분리

--- src/app/(interview)/interview-live/page.tsx
+++ src/app/(interview)/interview-live/page.tsx
@@ -1,7 +1,13 @@
 import CameraView from '@/features/interview/camera-view';
+import VoiceInputButton from '@/features/interview/voice-input-button';
 
 const InterviewPage = () => {
-  return <CameraView />;
+  return (
+    <div>
+      <CameraView />
+      <VoiceInputButton />
+    </div>
+  );
 };
 
 export default InterviewPage;
--- src/features/interview/camera-view.tsx
+++ src/features/interview/camera-view.tsx
@@ -1,18 +1,13 @@
 'use client';
 
-import { useAudioRecorder } from '@/features/interview/hooks/use-audio-recorder';
 import { useWebcamStream } from '@/features/interview/hooks/use-webcam-stream';
 
 const CameraView = () => {
   const videoRef = useWebcamStream();
-  const { isRecording, audioBlob, startRecording, stopRecording } = useAudioRecorder();
 
   return (
     <div className='w-[300px]'>
       <video ref={videoRef} autoPlay />
-      {/** 녹음 버튼 */}
-      <button onClick={isRecording ? stopRecording : startRecording}>{isRecording ? '녹음 중지' : '녹음하기'}</button>
-      {audioBlob && <audio controls src={URL.createObjectURL(audioBlob)} />}
     </div>
   );
 };

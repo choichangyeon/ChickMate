81b563ee52d62cff3c55733ee1f8395eb47058e3
Minjo Park <consistent.312@gmail.com>
2025-04-04 23:19:07 +0900

    design:#31 Text 공통 컴포넌트 기본 틀 완료

--- src/components/common/text.tsx
+++ src/components/common/text.tsx
@@ -1,5 +1,32 @@
-const Text = () => {
-  return <div>Text</div>;
+import clsx from 'clsx';
+
+type TextSize = 'xs' | 'sm' | 'md' | 'lg';
+type TextColor = 'default' | 'primary' | 'danger' | 'gray';
+
+type Props = {
+  size?: TextSize;
+  color?: TextColor;
+  children: React.ReactNode;
+};
+
+const sizeClassName = {
+  xs: 'text-xs',
+  sm: 'text-sm',
+  md: 'text-base',
+  lg: 'text-lg',
+};
+
+const colorClassName = {
+  default: 'text-black',
+  primary: 'text-primary',
+  danger: 'text-red-600',
+  gray: 'text-gray-400',
+};
+
+const Text = ({ size = 'md', color = 'default', children }: Props) => {
+  const textClassName = clsx('font-normal', sizeClassName[size], colorClassName[color]);
+
+  return <p className={textClassName}>{children}</p>;
 };
 
 export default Text;

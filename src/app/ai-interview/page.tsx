e00617ae0a1bf74698154e5b6adc359af7eee467
choichangyeon <changyon99@naver.com>
2025-04-04 14:00:57 +0900

    feat:#18 stt component 추가

--- src/app/ai-interview/page.tsx
+++ src/app/ai-interview/page.tsx
@@ -1,9 +1,11 @@
-import TTSComponent from '@/features/ai-interview/test-component';
+import TTSComponent from '@/features/ai-interview/tts-component';
+import STTComponent from '@/features/ai-interview/stt-component';
 
 const AiInterviewPage = () => {
   return (
     <section>
       <TTSComponent />
+      <STTComponent />
     </section>
   );
 };

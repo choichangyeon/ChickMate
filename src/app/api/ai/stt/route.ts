27c5800437286cf13e8b97a1d4c64a38bd13250a
choichangyeon <changyon99@naver.com>
2025-04-04 14:00:39 +0900

    feat:#18 stt api 연결

---
+++ src/app/api/ai/stt/route.ts
@@ -0,0 +1,39 @@
+import { ENV } from '@/constants/env-constants';
+import { NextRequest, NextResponse } from 'next/server';
+import OpenAI from 'openai';
+
+const openAi = new OpenAI({
+  apiKey: ENV.OPENAI_API_KEY,
+  dangerouslyAllowBrowser: true,
+});
+
+/**
+ * POST 요청 함수
+ */
+export const POST = async (req: NextRequest) => {
+  try {
+    const formData = await req.formData();
+    // multipart/form-data의 경우 req.formData()를 사용하여 데이터를 파싱합니다.
+    const file = formData.get('file') as File;
+    const model = formData.get('model') as string;
+    const language = formData.get('language') as string;
+    console.log(file);
+    if (!file) {
+      console.log('here');
+      return NextResponse.json({ message: '파일이 제공되지 않았습니다.', status: 400 });
+    }
+    // OpenAI STT API 호출
+    const res = await openAi.audio.transcriptions.create({
+      file,
+      model,
+      language,
+    });
+    console.log('here');
+    console.log(res);
+
+    return NextResponse.json({ status: 200, res });
+  } catch (error) {
+    const error_message = (error as Error).message;
+    return NextResponse.json({ message: error_message, status: 503 });
+  }
+};
--- src/features/ai-interview/api/client-services.ts
+++ src/features/ai-interview/api/client-services.ts
@@ -1,22 +1,17 @@
-/**
- *
- * @param text - 서버로부터 받아온 텍스트 입력
- * @param model - 고정된 TTS 모델 ('gpt-4o-mini-tts')
- * @param voice - default = calm('ash'), pressure('sage')
- * @param speed - default = calm(1), pressure(2.5)
- * @param response_format  - format 형식 default = 'wav'
- * @param instruction  - 목소리 형식 default = calm
- * @returns {void}
- */
-
-type Props = {
+type TTS_Props = {
   text: string;
   type: 'PRESSURE' | 'CALM';
 };
 
-const DEFAULT_OPTIONS = {
+const TTS_DEFAULT_OPTIONS = {
   MODEL: 'gpt-4o-mini-tts',
-  FORMAT: 'wav',
+  FORMAT: 'mp3',
+};
+
+const STT_DEFAULT_OPTIONS = {
+  MODEL: 'whisper-1',
+  FORMAT: 'webm',
+  LANGUAGE: 'ko',
 };
 
 const CALM_OPTIONS = {
@@ -33,13 +28,23 @@ const PRESSURE_OPTIONS = {
 Avoids showing emotional empathy or positive reactions to the candidate's responses.`,
 };
 
-const { MODEL, FORMAT } = DEFAULT_OPTIONS;
+/**
+ * @function textToSpeech
+ * @param text - 서버로부터 받아온 텍스트 입력
+ * @param model - 고정된 TTS 모델 ('gpt-4o-mini-tts')
+ * @param voice - default = calm('ash'), pressure('sage')
+ * @param speed - default = calm(1), pressure(2.5)
+ * @param response_format  - format 형식 default = 'mp3'
+ * @param instruction  - 목소리 형식 default = calm
+ * @returns {void}
+ */
 
-export const speakText = async ({ text, type }: Props) => {
+export const textToSpeech = async ({ text, type }: TTS_Props): Promise<void> => {
+  const { MODEL, FORMAT } = TTS_DEFAULT_OPTIONS;
   const { VOICE, SPEED, INSTRUCTION } = type === 'PRESSURE' ? PRESSURE_OPTIONS : CALM_OPTIONS;
   try {
     // TODO : Path명 상수화 진행
-    const res = await fetch('/api/ai', {
+    const res = await fetch('/api/ai/tts', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
@@ -60,9 +65,55 @@ export const speakText = async ({ text, type }: Props) => {
       throw new Error(data.error || data.message);
     }
 
-    const audio = new Audio(data.audio_url);
+    const audio = new Audio(data.audioUrl);
     await audio.play();
   } catch (error) {
     throw new Error('TTS 서버 에러');
+  }
+};
+
+type STT_Props = {
+  blob: Blob;
+};
+
+/**
+ * @function speechToText
+ * @param blob - 사용자 목소리 파일
+ * @param model - default('whisper-1')
+ * @param format - default('webm')
+ * @param language - default('ko')
+ * @returns {text}
+ */
+
+export const speechToText = async ({ blob }: STT_Props): Promise<string> => {
+  const { MODEL, FORMAT, LANGUAGE } = STT_DEFAULT_OPTIONS;
+
+  try {
+    const formData = new FormData();
+
+    // file 필드에 Blob과 파일 이름, MIME 타입 지정 (브라우저는 blob 객체를 File 객체처럼 처리)
+    formData.append('file', blob);
+    formData.append('model', MODEL);
+    formData.append('language', LANGUAGE);
+    formData.append('format', FORMAT);
+
+    // TODO : Path명 상수화 진행
+    const res = await fetch('/api/ai/stt', {
+      method: 'POST',
+      body: formData,
+    });
+
+    const data = await res.json();
+
+    if (res.status !== 200) {
+      // TODO : ERROR 처리
+      console.error('STT 요청 실패', data.error || data.message);
+      throw new Error(data.error || data.message);
+    }
+
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw new Error('STT 서버 에러');
   }
 };

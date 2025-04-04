5441edc23f6917c7bd2fbdb0b8a7d3bb102cb9aa
Minjo Park <consistent.312@gmail.com>
2025-04-04 09:52:28 +0900

    feat:#28 녹음 버튼 구현

--- src/features/interview/camera-view.tsx
+++ src/features/interview/camera-view.tsx
@@ -1,14 +1,18 @@
 'use client';
 
+import { useAudioRecorder } from './hooks/useAudioRecorder';
 import { useWebcamStream } from './hooks/useWebcamStream';
 
 const CameraView = () => {
   const videoRef = useWebcamStream();
+  const { isRecording, audioURL, startRecording, stopRecording } = useAudioRecorder();
 
   return (
     <div className='w-[300px]'>
       <video ref={videoRef} autoPlay />
-      <button>녹음하기</button>
+      {/** 녹음 버튼 */}
+      <button onClick={isRecording ? stopRecording : startRecording}>{isRecording ? '녹음 중지' : '녹음하기'}</button>
+      {audioURL && <audio controls src={audioURL} />}
     </div>
   );
 };

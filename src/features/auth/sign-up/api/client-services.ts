ebae1ff0c9dec8f8d6dfdd9ddb083de796518a41
ImJaeOne <dlawi0108@naver.com>
2025-04-05 23:56:34 +0900

    refactor:#33 method 상수 처리

--- src/features/auth/sign-up/api/client-services.ts
+++ src/features/auth/sign-up/api/client-services.ts
@@ -1,4 +1,5 @@
 import { ROUTE_HANDLER_PATH } from '@/constants/path-constant';
+import { fetchWithSentry } from '@/utils/fetch-with-sentry';
 
 type Props = {
   name: string;
@@ -9,22 +10,26 @@ type Props = {
 const { AUTH } = ROUTE_HANDLER_PATH;
 const { SIGN_UP } = AUTH;
 
-export const postSignUp = async (signUpData: Props) => {
-  try {
-    const res = await fetch(SIGN_UP, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(signUpData),
-    });
+// 추후 merge 시 변경할 부분
+const API_METHOD = {
+  POST: 'POST',
+};
 
-    const data = await res.json();
+const { POST } = API_METHOD;
 
-    if (!res.ok) {
-      throw new Error(data.error);
-    }
-
-    return data;
-  } catch (error) {
-    throw error;
-  }
+/**
+ * 회원가입 요청을 서버에 전송합니다.
+ *
+ * @param {Object} data - 회원가입에 필요한 사용자 정보
+ * @param {string} data.name - 사용자 이름
+ * @param {string} data.email - 사용자 이메일
+ * @param {string} data.password - 사용자 비밀번호
+ * @returns 서버로 요청을 보낸 후 아무 값도 반환하지 않습니다.
+ */
+export const postSignUp = async (data: Props) => {
+  await fetchWithSentry(SIGN_UP, {
+    method: POST,
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data),
+  });
 };

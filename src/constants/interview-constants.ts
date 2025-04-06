02318b639d3bc0543108665b22c5bc9e7c8186da
choichangyeon <changyon99@naver.com>
2025-04-07 00:03:33 +0900

    feat:#7 인터뷰 옵션 상수화

--- src/constants/interview-constants.ts
+++ src/constants/interview-constants.ts
@@ -2,3 +2,30 @@ export const INTERVIEW_TYPE = {
   PRESSURE: 'pressure',
   CALM: 'calm',
 };
+
+export const INTERVIEW_CONVERT_OPTIONS = {
+  TTS_OPTIONS: {
+    MODEL: 'gpt-4o-mini-tts',
+    FORMAT: 'mp3',
+  },
+  STT_OPTIONS: {
+    MODEL: 'gpt-4o-transcribe',
+    FORMAT: 'webm',
+    LANGUAGE: 'ko',
+  },
+};
+
+export const INTERVIEW_VOICE_OPTIONS = {
+  CALM_OPTIONS: {
+    VOICE: 'ash',
+    SPEED: 1,
+    INSTRUCTION: `Uses a friendly and gentle tone of voice.
+    Rather than challenging the candidate's answers, frequently provides emotional empathy or positive reactions.`,
+  },
+  PRESSURE_OPTIONS: {
+    VOICE: 'sage',
+    SPEED: 2.5,
+    INSTRUCTION: `Uses a firm and dry tone of voice.
+    Avoids showing emotional empathy or positive reactions to the candidate's responses.`,
+  },
+};

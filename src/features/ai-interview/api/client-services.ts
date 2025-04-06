c9f3e0e0a5bd07eec15eaeb59ee14b0aed2d3080
choichangyeon <changyon99@naver.com>
2025-04-06 22:19:42 +0900

    refactor:#7 route handler 수정

--- src/features/ai-interview/api/client-services.ts
+++ src/features/ai-interview/api/client-services.ts
@@ -1,32 +1,19 @@
-import OpenAI from 'openai';
-
 export const getOpenAIResponse = async (messageList) => {
-  const openai = new OpenAI({
-    apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
-    dangerouslyAllowBrowser: true,
-  });
   try {
-    const response = await openai.chat.completions.create({
-      model: 'gpt-4o-mini',
-      messages: messageList,
-      response_format: {
-        'type': 'text',
-      },
-      temperature: 1,
-      max_completion_tokens: 200,
-      top_p: 1,
-      store: false,
+    const res = await fetch('/api/ai/interview', {
+      method: 'POST',
+      body: JSON.stringify({ messageList: messageList }),
     });
 
-    messageList.push({
-      role: 'assistant',
-      content: [
-        {
-          type: 'text',
-          text: response.choices[0].message.content,
-        },
-      ],
-    });
+    // messageList.push({
+    //   role: 'assistant',
+    //   content: [
+    //     {
+    //       type: 'text',
+    //       text: response.choices[0].message.content,
+    //     },
+    //   ],
+    // });
 
     return messageList;
   } catch (error) {

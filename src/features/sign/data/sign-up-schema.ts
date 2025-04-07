5b31928f98b0574e59e7670f2c7376f958d5b4dd
ImJaeOne <dlawi0108@naver.com>
2025-04-08 02:22:27 +0900

    feat:#49 비밀번호 확인 schema 추가

--- src/features/sign/data/sign-up-schema.ts
+++ src/features/sign/data/sign-up-schema.ts
@@ -7,21 +7,31 @@ const NAME_VALIDATION_REGEX = /^[a-zA-Z0-9가-힣]+$/;
 const PASSWORD_VALIDATION_MIN = 6;
 
 const { VALIDATION } = AUTH_MESSAGE;
-const { NAME_LENGTH, NAME_SPECIAL_CHAR, EMAIL_INVALID, PASSWORD_LENGTH, PASSWORD_SPECIAL_CHAR } = VALIDATION;
+const { NAME_LENGTH, NAME_SPECIAL_CHAR, EMAIL_INVALID, PASSWORD_LENGTH, PASSWORD_SPECIAL_CHAR, PASSWORD_CHECK } =
+  VALIDATION;
 
-export const schema = z.object({
-  name: z
-    .string()
-    .trim()
-    .min(NAME_VALIDATION_MIN, NAME_LENGTH)
-    .max(NAME_VALIDATION_MAX, NAME_LENGTH)
-    .regex(NAME_VALIDATION_REGEX, NAME_SPECIAL_CHAR),
-  email: z.string().trim().email(EMAIL_INVALID),
-  password: z
-    .string()
-    .trim()
-    .min(PASSWORD_VALIDATION_MIN, PASSWORD_LENGTH)
-    .regex(/[^a-zA-Z0-9]/, PASSWORD_SPECIAL_CHAR),
-});
+export const schema = z
+  .object({
+    name: z
+      .string()
+      .trim()
+      .min(NAME_VALIDATION_MIN, NAME_LENGTH)
+      .max(NAME_VALIDATION_MAX, NAME_LENGTH)
+      .regex(NAME_VALIDATION_REGEX, NAME_SPECIAL_CHAR),
+    email: z.string().trim().email(EMAIL_INVALID),
+    password: z
+      .string()
+      .trim()
+      .min(PASSWORD_VALIDATION_MIN, PASSWORD_LENGTH)
+      .regex(/[^a-zA-Z0-9]/, PASSWORD_SPECIAL_CHAR),
+    passwordCheck: z.string().trim(),
+  })
+  .refine(
+    (data) => {
+      console.log(data);
+      return data.password === data.passwordCheck;
+    },
+    { message: PASSWORD_CHECK, path: ['passwordCheck'] }
+  );
 
 export type SignUpFormData = z.infer<typeof schema>;

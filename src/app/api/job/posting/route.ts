ba60da33893baa1ec93b7b0646f0c456dbcab2d2
choichangyeon <changyon99@naver.com>
2025-04-07 15:03:54 +0900

    feat:#40 채용공고 DB route handler 구현

---
+++ src/app/api/job/posting/route.ts
@@ -0,0 +1,23 @@
+import { DB_MESSAGE } from '@/constants/message-constants';
+import { prisma } from '@/lib/prisma';
+import { NextRequest, NextResponse } from 'next/server';
+
+/**
+ * GET
+ */
+export const GET = async (req: NextRequest): Promise<NextResponse> => {
+  const { NOT_FOUND_DATA, DB_SERVER_ERROR } = DB_MESSAGE.ERROR;
+  const {} = await req.json();
+  try {
+    // TODO : prisma 로직 재구현
+    const data = await prisma.sample.findMany({});
+
+    if (!data) {
+      return NextResponse.json({ message: NOT_FOUND_DATA }, { status: 400 });
+    }
+
+    return NextResponse.json({ data }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: DB_SERVER_ERROR }, { status: 500 });
+  }
+};

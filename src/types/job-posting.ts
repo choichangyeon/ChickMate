f96b5417d27be2530c2ca35c971872eafff82f4e
choichangyeon <changyon99@naver.com>
2025-04-07 15:06:11 +0900

    feat:#40 채용공고 타입 설정

---
+++ src/types/job-posting.ts
@@ -0,0 +1,11 @@
+export type JobPosting = {
+  company: String;
+  id: String;
+  title: String;
+  location: String;
+  url: string;
+  educationLevel: string;
+  postedAt: Date;
+  deadline: Date;
+  createdAt: Date;
+};

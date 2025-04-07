aa13d6aedd509e207974ec18ff3f6bf0c0f718ca
Minjo Park <consistent.312@gmail.com>
2025-04-08 03:42:52 +0900

    refactor:#41 자소서 작성 페이지 컴포넌트 분리

---
+++ src/app/auth/resume/page.tsx
@@ -0,0 +1,19 @@
+import { Metadata } from 'next';
+import ResumeForm from '@/features/resume/resume-form';
+import UserInfoSummary from '@/features/resume/user-info-summary';
+
+export const metadata: Metadata = {
+  title: '자기소개서 작성',
+  description: 'Chick Mate에서 자기소개서를 관리해보세요.',
+};
+
+const ResumePage = () => {
+  return (
+    <div className='flex gap-2'>
+      <ResumeForm />
+      <UserInfoSummary />
+    </div>
+  );
+};
+
+export default ResumePage;

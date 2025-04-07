8a623deda306d84962788e57cf240b5e9857f6fc
choichangyeon <changyon99@naver.com>
2025-04-07 16:58:13 +0900

    feat:#40 mock data 입력

--- src/app/api/job/posting/route.ts
+++ src/app/api/job/posting/route.ts
@@ -7,10 +7,17 @@ import { NextRequest, NextResponse } from 'next/server';
  */
 export const GET = async (req: NextRequest): Promise<NextResponse> => {
   const { NOT_FOUND_DATA, DB_SERVER_ERROR } = DB_MESSAGE.ERROR;
-  const {} = await req.json();
+  //   const {} = await req.json();
   try {
     // TODO : prisma 로직 재구현
-    const data = await prisma.sample.findMany({});
+    // const data = await prisma.sample.findMany({});
+    const data = [
+      {
+        id: 12,
+        company: '삼성물산',
+        url: 'www.naver.com',
+      },
+    ];
 
     if (!data) {
       return NextResponse.json({ message: NOT_FOUND_DATA }, { status: 400 });

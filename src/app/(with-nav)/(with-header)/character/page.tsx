62fe66852d007a91aef5c16019e96d9bb7923471
ImJaeOne <dlawi0108@naver.com>
2025-04-08 23:57:05 +0900

    feat:#47 경험치 patch page 구현

---
+++ src/app/(with-nav)/(with-header)/character/page.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { usePostExperienceMutation } from '@/hooks/mutations/use-post-experience-mutation';
+import { useCharacterStore } from '@/store/use-character-store';
+
+const CharacterPage = () => {
+  const characterId = useCharacterStore((state) => state.characterId);
+  
+  const { mutate: updateExp } = usePostExperienceMutation();
+
+  const handleExperienceUp = () => {
+    updateExp({ characterId, amount: 10 });
+  };
+  
+  return (
+    <div className='flex flex-col'>
+      <button className='rounded-md border border-cyan-900 text-red-400' onClick={handleExperienceUp}>
+        캐릭터 경험치
+      </button>
+    </div>
+  );
+};
+
+export default CharacterPage;

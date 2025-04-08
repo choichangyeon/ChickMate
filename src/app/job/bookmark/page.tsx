92f8e8d07cd2b4448026e50c76afa87da9a23ba9
choichangyeon <changyon99@naver.com>
2025-04-08 11:57:43 +0900

    check

--- src/app/job/bookmark/page.tsx
+++ src/app/job/bookmark/page.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import BookmarkComponent from '@/features/(job)/bookmark-component';
-import { serverSession } from '@/utils/server-session';
 
 const BookmarkPage = async () => {
-  const user = await serverSession();
-
   return (
     <div>
       BookmarkPage

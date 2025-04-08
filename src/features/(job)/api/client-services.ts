bb2f70449d3a5105db7e8efa1f55370aa2053ba5
choichangyeon <changyon99@naver.com>
2025-04-08 17:35:15 +0900

    feat:#51 북마크 route handler 브라우저 함수 구현

--- src/features/(job)/api/client-services.ts
+++ src/features/(job)/api/client-services.ts
@@ -1,20 +1,59 @@
 import { API_METHOD } from '@/constants/api-method-constants';
 import { fetchWithSentry } from '@/utils/fetch-with-sentry';
 
-const { POST } = API_METHOD;
+const { POST, DELETE, GET } = API_METHOD;
+const EMPTY_LIST_NUMBER = 0;
 
 /**
  * @function postBookmarkWithUserId
  * @param jobPostingId - 채용 공고 id(string)
+ * @param isBookmarked - 북마크 여부(boolean)
  * @returns {boolean} - 북마킹 여부
  */
-export const postBookmarkWithUserId = async (jobPostingId: number): Promise<boolean> => {
-  const res = await fetchWithSentry('/api/job/bookmark', {
-    method: POST,
-    body: JSON.stringify({
-      jobPostingId,
-    }),
+type BookmarkProps = {
+  jobPostingId: number;
+  isMarked: boolean;
+};
+export const postBookmarkWithJobPostingId = async ({ jobPostingId, isMarked }: BookmarkProps): Promise<boolean> => {
+  // 북마킹 여부에 따라 요청 method 달라짐
+  if (isMarked) {
+    const res = await fetchWithSentry('/api/job/bookmark', {
+      method: DELETE,
+      body: JSON.stringify({
+        jobPostingId,
+      }),
+    });
+
+    return res ? false : true;
+  } else {
+    const res = await fetchWithSentry('/api/job/bookmark', {
+      method: POST,
+      body: JSON.stringify({
+        jobPostingId,
+      }),
+    });
+
+    return res ? true : false;
+  }
+};
+
+/**
+ * @function getBookmarkByJobPostingId
+ * @param jobPostingId - 채용 공고 id(string)
+ * @returns {boolean} - 북마킹 여부
+ */
+export const getBookmarkByJobPostingId = async (jobPostingId: number): Promise<boolean> => {
+  const queryParams = new URLSearchParams({
+    jobPostingId: jobPostingId.toString(),
+  });
+  const url = `/api/job/bookmark?${queryParams.toString()}`;
+  const { data } = await fetchWithSentry(url, {
+    method: GET,
   });
 
-  return res ? true : false;
+  if (data.length !== EMPTY_LIST_NUMBER) {
+    return true;
+  }
+
+  return false;
 };

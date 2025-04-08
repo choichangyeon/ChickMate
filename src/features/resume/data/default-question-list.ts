e23e34a6a6204b63280a57c0768904464c734735
Minjo Park <consistent.312@gmail.com>
2025-04-08 09:12:44 +0900

    refactor:#41 default 질문 리스트에 id값 추가

--- src/features/resume/data/default-question-list.ts
+++ src/features/resume/data/default-question-list.ts
@@ -1,17 +1,17 @@
 export const defaultQuestionList = [
   {
-    id: '',
+    id: 'abc',
     question: '해당 회사를 지원한 이유와 입사 후 회사에서 이루고 싶은 꿈을 기술하십시오.',
     answer: '',
   },
   {
-    id: '',
+    id: 'abcd',
     question:
       '본인의 성장 과정을 간략히 기술하되 현재의 자신에게 가장 큰 영향을 끼친 사건, 인물 등을 포함하여 기술하십시오.',
     answer: '',
   },
   {
-    id: '',
+    id: 'abcde',
     question:
       '지원 직무 관련 프로젝트/과제 중 기술적으로 가장 어려웠던 과제와 해결 방안에 대해 구체적으로 서술하십시오.',
     answer: '',

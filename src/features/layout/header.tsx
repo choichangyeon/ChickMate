58acee643ecf1fa2acb43e34594b1b369618da43
ImJaeOne <dlawi0108@naver.com>
2025-04-08 23:42:41 +0900

    feat:#47 캐릭터 정보 prefetch

---
+++ src/features/layout/header.tsx
@@ -0,0 +1,31 @@
+import { User } from '@/types/user';
+import HeaderCharacter from '../chraracter/header-character';
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
+import { prisma } from '@/lib/prisma';
+import { CHARACTER_QUERY_KEY } from '@/constants/query-key';
+import { getCharacterByUserId } from '../chraracter/api/server-services';
+import { serverActionWithSentry } from '@/utils/server-action-with-sentry';
+
+type Props = {
+  user?: User;
+};
+
+const { CHARACTER } = CHARACTER_QUERY_KEY;
+
+export const Header = async ({ user }: Props) => {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: [CHARACTER],
+    queryFn: () => serverActionWithSentry(getCharacterByUserId),
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <header className='flex justify-around bg-white pl-20'>
+        <span>헤더입니다.</span>
+        <HeaderCharacter user={user} />
+      </header>
+    </HydrationBoundary>
+  );
+};

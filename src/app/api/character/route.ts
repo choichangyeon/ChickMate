995213da1909a17325c34008c633cd0de00c69e2
ImJaeOne <dlawi0108@naver.com>
2025-04-08 22:03:33 +0900

    feat:#47 캐릭터 정보 가져오는 라우트 핸들러 구현

---
+++ src/app/api/character/route.ts
@@ -0,0 +1,28 @@
+import { AUTH_MESSAGE, CHARACTER_MESSAGE } from '@/constants/message-constants';
+import { prisma } from '@/lib/prisma';
+import { authOptions } from '@/utils/auth-option';
+import { getServerSession } from 'next-auth';
+import { NextResponse } from 'next/server';
+
+const { AUTH_REQUIRED } = AUTH_MESSAGE.RESULT;
+const { GET_DATA_FAILED } = CHARACTER_MESSAGE.INFO;
+
+export const GET = async (): Promise<NextResponse> => {
+  const session = await getServerSession(authOptions);
+
+  if (!session || !session.user) {
+    return NextResponse.json({ message: AUTH_REQUIRED }, { status: 403 });
+  }
+  try {
+    const { user } = await getServerSession(authOptions);
+    const userId = user.id;
+
+    const response = await prisma.character.findFirst({
+      where: { userId },
+    });
+
+    return NextResponse.json({ response }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: GET_DATA_FAILED }, { status: 500 });
+  }
+};

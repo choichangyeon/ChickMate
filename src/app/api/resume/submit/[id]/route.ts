d7018f9691f64daf7d3316788ae0b2b8ba6ec281
Minjo Park <consistent.312@gmail.com>
2025-04-08 23:54:18 +0900

    feat:#56 자소서가 이미 자동 완료가 된 후에 작성 완료 버튼을 눌렀을 때 실행하는 PATCH 요청

---
+++ src/app/api/resume/submit/[id]/route.ts
@@ -0,0 +1,60 @@
+import { NextResponse } from 'next/server';
+import { getServerSession } from 'next-auth';
+import { prisma } from '@/lib/prisma';
+import { authOptions } from '@/utils/auth-option';
+import { AUTH_MESSAGE, RESUME_MESSAGE } from '@/constants/message-constants';
+import { RESUME_STATUS } from '@/constants/resume-constants';
+import { getValidTitle } from '@/features/resume/utils/get-valid-title';
+import type { RouteParams } from '@/types/route-params';
+import type { ResumeData } from '@/types/resume';
+
+const { AUTH_REQUIRED } = AUTH_MESSAGE.RESULT;
+const { NOT_FOUND, FORBIDDEN, DRAFT_SERVER_ERROR } = RESUME_MESSAGE.DRAFT;
+
+const { SUBMIT } = RESUME_STATUS;
+
+/**
+ * 임시 저장된 자소서가 있을 경우 해당 자소서 내용을 수정하는 요청 (완전히 등록된 자소서)
+ * @param request 자소서에서 변경한 내용
+ * @param params resumeId
+ */
+export const PATCH = async (request: Request, { params }: RouteParams) => {
+  const session = await getServerSession(authOptions);
+
+  if (!session || !session.user) {
+    return NextResponse.json({ message: AUTH_REQUIRED }, { status: 403 });
+  }
+
+  try {
+    const { id: resumeId } = params;
+    const id = Number(resumeId);
+
+    const body: ResumeData = await request.json();
+    const { title, fieldList } = body;
+
+    const resume = await prisma.resume.findUnique({
+      where: { id },
+    });
+
+    if (!resume) {
+      return NextResponse.json({ message: NOT_FOUND }, { status: 404 });
+    }
+
+    if (resume.userId && resume.userId !== session.user.id) {
+      return NextResponse.json({ message: FORBIDDEN }, { status: 403 });
+    }
+
+    const updatedResume = await prisma.resume.update({
+      where: { id },
+      data: {
+        status: SUBMIT,
+        title,
+        content: fieldList,
+      },
+    });
+
+    return NextResponse.json(updatedResume);
+  } catch (error) {
+    return NextResponse.json({ message: DRAFT_SERVER_ERROR }, { status: 500 });
+  }
+};

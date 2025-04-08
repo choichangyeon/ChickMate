0610ce58eb183ebd2f2bd0b0f76bfbe0cfe1a255
ImJaeOne <dlawi0108@naver.com>
2025-04-09 01:23:20 +0900

    refactor:#47 반환값 타입 추가

--- src/features/chraracter/utils/get-level-and-percent.ts
+++ src/features/chraracter/utils/get-level-and-percent.ts
@@ -1,7 +1,11 @@
-// utils/get-level-and-percent.ts
 import { LEVEL_EXP } from '@/constants/character-constants';
 
-export const getLevelAndPercentage = (exp: number) => {
+type LevelInfo = {
+  level: number;
+  percent: number;
+};
+
+export const getLevelAndPercentage = (exp: number): LevelInfo => {
   if (exp >= LEVEL_EXP[3]) return { level: 4, percent: 100 };
   if (exp >= LEVEL_EXP[2]) return { level: 3, percent: ((exp - LEVEL_EXP[2]) / 100) * 100 };
   if (exp >= LEVEL_EXP[1]) return { level: 2, percent: ((exp - LEVEL_EXP[1]) / 100) * 100 };

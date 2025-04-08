28f941da39be43914117093336c741486a76c4f4
choichangyeon <changyon99@naver.com>
2025-04-08 17:34:33 +0900

    feat:#51 북마크 컴포넌트 상태에 따른 로직 변경 설정

--- src/features/(job)/bookmark-component.tsx
+++ src/features/(job)/bookmark-component.tsx
@@ -1,15 +1,38 @@
 'use client';
 
-import { postBookmarkWithUserId } from '@/features/(job)/api/client-services';
+import { postBookmarkWithJobPostingId } from '@/features/(job)/api/client-services';
+import { useBookmark } from '@/features/(job)/hooks/use-bookmark';
+import { useEffect, useState } from 'react';
 
-const BookmarkComponent = () => {
+type Props = {
+  jobPostingId: number;
+};
+
+const BookmarkComponent = ({ jobPostingId }: Props) => {
+  const { isBookmarked, loading, error } = useBookmark(jobPostingId);
+  const [isMarked, setIsMarked] = useState(isBookmarked);
+
+  useEffect(() => {
+    setIsMarked(isBookmarked);
+  }, [isBookmarked]);
+
   const handleClick = async () => {
-    await postBookmarkWithUserId(1);
+    const check = await postBookmarkWithJobPostingId({ jobPostingId, isMarked });
+    setIsMarked(check);
   };
+
+  const handleClickTest = () => {
+    console.log(isMarked);
+  };
+
+  if (loading) return <div>...loading</div>;
+  if (error) return <div>error</div>;
+
   return (
     <div>
       BookmarkComponent
       <button onClick={handleClick}>북마크 테스트</button>
+      <button onClick={handleClickTest}>useState</button>
     </div>
   );
 };

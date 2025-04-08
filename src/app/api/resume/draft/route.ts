c1f07adcf9e61a898968cda3fb9e03ac2df06004
Minjo Park <consistent.312@gmail.com>
2025-04-08 22:50:56 +0900

    feat:#56 처음으로 자소서를 자동 저장할 경우 POST 요청 보내는 로직 구현

--- src/app/api/resume/draft/route.ts
+++ src/app/api/resume/draft/route.ts
@@ -4,23 +4,18 @@ import { prisma } from '@/lib/prisma';
 import { authOptions } from '@/utils/auth-option';
 import { AUTH_MESSAGE, RESUME_MESSAGE } from '@/constants/message-constants';
 import { RESUME_STATUS } from '@/constants/resume-constants';
-import type { Field } from '@/types/resume';
-
-type RequestBody = {
-  title: string;
-  fieldList: Field[];
-};
+import { getValidTitle } from '@/features/resume/utils/get-valid-title';
+import type { ResumeData } from '@/types/resume';
 
 const { AUTH_REQUIRED } = AUTH_MESSAGE.RESULT;
 const { REQUEST_FAILURE, SUBMIT_SERVER_ERROR } = RESUME_MESSAGE.SUBMIT;
-const { NOT_FOUND, FORBIDDEN, DRAFT_SERVER_ERROR } = RESUME_MESSAGE.DRAFT;
 const { DRAFT } = RESUME_STATUS;
 
 /**
  * 임시 저장한 자소서 등록하는 요청
  * @param request 자소서 제목, 자소서 질문/답변
  */
-export async function POST(request: Request) {
+export const POST = async (request: Request) => {
   const session = await getServerSession(authOptions);
 
   if (!session || !session.user) {
@@ -28,10 +23,10 @@ export async function POST(request: Request) {
   }
 
   try {
-    const body: RequestBody = await request.json();
+    const body: ResumeData = await request.json();
     const { title, fieldList } = body;
 
-    if (!title || !fieldList) {
+    if (!fieldList) {
       return NextResponse.json({ message: REQUEST_FAILURE }, { status: 400 });
     }
 
@@ -39,7 +34,7 @@ export async function POST(request: Request) {
       data: {
         userId: session.user.id,
         status: DRAFT,
-        title,
+        title: getValidTitle(title),
         content: fieldList,
       },
     });
@@ -48,54 +43,4 @@ export async function POST(request: Request) {
   } catch (error) {
     return NextResponse.json({ message: SUBMIT_SERVER_ERROR }, { status: 500 });
   }
-}
-
-type RouteParams = {
-  params: {
-    id: string;
-  };
 };
-
-/**
- * 수정된 자소서 내용을 등록하는 요청
- * @param request 자소서에서 변경한 내용
- * @param params resumeId
- */
-export async function PATCH(request: Request, { params }: RouteParams) {
-  const session = await getServerSession(authOptions);
-
-  if (!session || !session.user) {
-    return NextResponse.json({ message: AUTH_REQUIRED }, { status: 403 });
-  }
-
-  try {
-    const { id: resumeId } = params;
-    const id = Number(resumeId);
-    const { title, content, isDone } = await request.json();
-
-    const resume = await prisma.resume.findUnique({
-      where: { id },
-    });
-
-    if (!resume) {
-      return NextResponse.json({ message: NOT_FOUND }, { status: 404 });
-    }
-
-    if (resume.userId && resume.userId !== session.user.id) {
-      return NextResponse.json({ message: FORBIDDEN }, { status: 403 });
-    }
-
-    const updatedTodo = await prisma.resume.update({
-      where: { id },
-      data: {
-        ...(title !== undefined && { title }),
-        ...(content !== undefined && { content }),
-        ...(isDone !== undefined && { isDone }),
-      },
-    });
-
-    return NextResponse.json(updatedTodo);
-  } catch (error) {
-    return NextResponse.json({ message: DRAFT_SERVER_ERROR }, { status: 500 });
-  }
-}

4c6395cec3e3872802426fc3c5548275174325ef
ImJaeOne <dlawi0108@naver.com>
2025-04-08 22:22:06 +0900

    feat:#47 header 상수 추가

--- src/constants/api-method-constants.ts
+++ src/constants/api-method-constants.ts
@@ -1,7 +1,15 @@
+// 추후 파일명 api-constants.ts
+
 export const API_METHOD = {
   POST: 'POST',
   GET: 'GET',
   DELETE: 'DELETE',
   PUT: 'PUT',
   PATCH: 'PATCH',
+};
+
+export const API_HEADER = {
+  JSON: {
+    'Content-Type': 'application/json',
+  },
 };

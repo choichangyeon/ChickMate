e09d299364b1e59f969ce43ff5064382b905bab5
choichangyeon <changyon99@naver.com>
2025-04-08 12:25:11 +0900

    fix:#52 유틸함수 삭제

--- src/utils/server-session-about-user.ts
+++
@@ -1,8 +0,0 @@
-import { getServerSession } from 'next-auth';
-import { authOptions } from '@/utils/auth-option';
-import { User } from '@/types/user';
-
-export const serverSessionAboutUser = async (): Promise<User> => {
-  const { user } = await getServerSession(authOptions);
-  return user;
-};

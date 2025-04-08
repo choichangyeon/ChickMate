fda0f8c9e907119a868705eec6fa329b7cf53032
ImJaeOne <dlawi0108@naver.com>
2025-04-08 12:31:45 +0900

    refactor:#47 session server에서 받아오도록 수정

--- src/app/(with-nav)/layout.tsx
+++ src/app/(with-nav)/layout.tsx
@@ -1,10 +1,14 @@
 import { Nav } from '@/features/layout/nav';
+import { authOptions } from '@/utils/auth-option';
+import { getServerSession } from 'next-auth';
 import React from 'react';
 
-const MainLayout = ({ children }: { children: React.ReactNode }) => {
+const MainLayout = async ({ children }: { children: React.ReactNode }) => {
+  const { user } = await getServerSession(authOptions);
+
   return (
     <>
-      <Nav />
+      <Nav user={user} />
       <main className='flex-1 bg-green-200'>{children}</main>
     </>
   );

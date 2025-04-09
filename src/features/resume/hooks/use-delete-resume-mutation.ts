79fa5c7506c00131fc401c90d68d12baba7c64f2
Minjo Park <consistent.312@gmail.com>
2025-04-10 04:47:14 +0900

    refactor:#64 optimistic update 구현

--- src/features/resume/hooks/use-delete-resume-mutation.ts
+++ src/features/resume/hooks/use-delete-resume-mutation.ts
@@ -1,6 +1,7 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { deleteResume } from '../api/client-services';
 import { QUERY_KEY } from '@/constants/query-key';
+import { Resume } from '@prisma/client';
 
 export const useDeleteResumeMutation = () => {
   const { RESUME } = QUERY_KEY;
@@ -9,8 +10,17 @@ export const useDeleteResumeMutation = () => {
 
   return useMutation({
     mutationFn: (resumeId: number) => deleteResume(resumeId),
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: [RESUME] });
+    onMutate: async (resumeId) => {
+      await queryClient.cancelQueries({ queryKey: [RESUME] });
+      const previousResume = queryClient.getQueryData([RESUME]);
+      queryClient.setQueryData([RESUME], (old: Resume[]) => old.filter((resume) => resume.id !== resumeId));
+
+      return { previousResume };
     },
+
+    onError: (err, resumeId, context) => {
+      queryClient.setQueryData([RESUME], context.previousResume);
+    },
+    onSettled: () => queryClient.invalidateQueries({ queryKey: [RESUME] }),
   });
 };

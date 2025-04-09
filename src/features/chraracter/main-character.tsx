2a4ab2c39a904374ac0fd391c8891dccb9a31c7d
ImJaeOne <dlawi0108@naver.com>
2025-04-09 22:11:40 +0900

    feat:#63 data ui에 적용

--- src/features/chraracter/main-character.tsx
+++ src/features/chraracter/main-character.tsx
@@ -4,11 +4,11 @@ import Image from 'next/image';
 import { useGetCharacterQuery } from './hooks/use-get-character-query';
 import { useCharacterStore } from '@/store/use-character-store';
 import { useEffect } from 'react';
+import { getLevelAndPercentage } from './utils/get-level-and-percent';
 
 const MainCharacter = () => {
   const { data, isPending, isError } = useGetCharacterQuery();
 
-  // 캐릭터 id zustand로 관리
   const setCharacterId = useCharacterStore((state) => state.setCharacterId);
 
   useEffect(() => {
@@ -23,19 +23,23 @@ const MainCharacter = () => {
   if (isError) {
     return null;
   }
+  const { level, percent } = getLevelAndPercentage(data.experience);
   return (
     <div className='flex flex-col rounded-lg border p-7'>
       <div className='flex items-center gap-3 text-black/50'>
         <span className='text-2xl font-extrabold'>EXP</span>
         <div className='flex w-full justify-around'>
           {Array.from({ length: 10 }).map((_, i) => (
-            <div key={i} className={`h-[6px] w-5 rounded-sm ${i < 6 ? 'bg-green-500' : 'bg-gray-300'}`} />
+            <div
+              key={i}
+              className={`h-[6px] w-5 rounded-sm ${i < Math.floor(percent / 10) ? 'bg-green-500' : 'bg-gray-300'}`}
+            />
           ))}
         </div>
       </div>
-      <Image src='/assets/character/card/level1.png' width={344} height={344} alt='character-img' />
+      <Image src={`/assets/character/card/level${level}.png`} width={344} height={344} alt='character-img' />
       <div className='flex gap-6'>
-        <span className='text-[40px] font-extrabold text-red-500'>LV : 20</span>
+        <span className='text-[40px] font-extrabold text-red-500'>LV : {level}</span>
         <div className='flex items-center justify-center rounded-3xl border border-black px-9 py-3'>
           <span className='text-xl'>똑똑한 병아리</span>
         </div>

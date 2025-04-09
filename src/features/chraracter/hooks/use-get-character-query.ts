447ed2f1c81587ba96b81f62c690a4e491c092eb
ImJaeOne <dlawi0108@naver.com>
2025-04-10 01:48:49 +0900

    refactor:#63 retry option 추가

--- src/features/chraracter/hooks/use-get-character-query.ts
+++ src/features/chraracter/hooks/use-get-character-query.ts
@@ -3,10 +3,16 @@ import { getCharacterByUserId } from '../api/client-services';
 import { QUERY_KEY } from '@/constants/query-key';
 
 const { CHARACTER } = QUERY_KEY;
+const RETRY_OPTION = {
+  RETRY_CHARACTER: 0, 
+};
+
+const { RETRY_CHARACTER } = RETRY_OPTION;
 
 export const useGetCharacterQuery = () => {
   return useQuery({
     queryKey: [CHARACTER],
     queryFn: getCharacterByUserId,
+    retry: RETRY_CHARACTER,
   });
 };

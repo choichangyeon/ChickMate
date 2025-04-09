f6f70920c3566c4b52e2ba17936812dc69312907
ImJaeOne <dlawi0108@naver.com>
2025-04-09 22:05:32 +0900

    feat:#63 character 정보 dehydration

--- src/app/(with-nav)/layout.tsx
+++ src/app/(with-nav)/layout.tsx
@@ -1,18 +1,31 @@
+import { QUERY_KEY } from '@/constants/query-key';
+import { getCharacterByUserId } from '@/features/chraracter/api/server-services';
 import { Nav } from '@/features/layout/nav';
 import { User } from '@/types/user';
 import { authOptions } from '@/utils/auth-option';
+import { serverActionWithSentry } from '@/utils/server-action-with-sentry';
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
 import { getServerSession } from 'next-auth';
 import React from 'react';
 
+const { CHARACTER } = QUERY_KEY;
+
 const MainLayout = async ({ children }: { children: React.ReactNode }) => {
   const session = await getServerSession(authOptions);
-
   const user = session?.user as User;
+
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: [CHARACTER],
+    queryFn: () => serverActionWithSentry(getCharacterByUserId),
+  });
+
   return (
-    <>
+    <HydrationBoundary state={dehydrate(queryClient)}>
       <Nav user={user} />
       <main className='flex-1 bg-green-200'>{children}</main>
-    </>
+    </HydrationBoundary>
   );
 };
 

6fc39d4ab4b46b453c4e6cb7005c65a8cb7a8a29
ImJaeOne <dlawi0108@naver.com>
2025-04-09 22:08:06 +0900

    feat:#63 useQuery를 통한 data fetch 및 zustand를 통한 캐릭터 id 관리

--- src/features/chraracter/main-character.tsx
+++ src/features/chraracter/main-character.tsx
@@ -1,11 +1,33 @@
+'use client';
+
 import Image from 'next/image';
+import { useGetCharacterQuery } from './hooks/use-get-character-query';
+import { useCharacterStore } from '@/store/use-character-store';
+import { useEffect } from 'react';
 
 const MainCharacter = () => {
+  const { data, isPending, isError } = useGetCharacterQuery();
+
+  // 캐릭터 id zustand로 관리
+  const setCharacterId = useCharacterStore((state) => state.setCharacterId);
+
+  useEffect(() => {
+    if (data) {
+      setCharacterId(data.id);
+    }
+  }, [data, setCharacterId]);
+
+  if (isPending) {
+    return null;
+  }
+  if (isError) {
+    return null;
+  }
   return (
     <div className='flex flex-col rounded-lg border p-7'>
       <div className='flex items-center gap-3 text-black/50'>
         <span className='text-2xl font-extrabold'>EXP</span>
-        <div className='flex justify-around w-full'>
+        <div className='flex w-full justify-around'>
           {Array.from({ length: 10 }).map((_, i) => (
             <div key={i} className={`h-[6px] w-5 rounded-sm ${i < 6 ? 'bg-green-500' : 'bg-gray-300'}`} />
           ))}

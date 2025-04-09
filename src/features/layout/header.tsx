638d291e011719ccce5fc5f2a6d0e17bf169b848
ImJaeOne <dlawi0108@naver.com>
2025-04-10 00:58:57 +0900

    refactor:#63 prefetch 삭제

--- src/features/layout/header.tsx
+++ src/features/layout/header.tsx
@@ -1,26 +1,11 @@
 import { User } from '@/types/user';
-import HeaderCharacter from '@/features/chraracter/header-character';
-import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
-import { QUERY_KEY } from '@/constants/query-key';
-import { getCharacterByUserId } from '@/features/chraracter/api/server-services';
-import { serverActionWithSentry } from '@/utils/server-action-with-sentry';
-
-const { CHARACTER } = QUERY_KEY;
+import HeaderCharacter from '../chraracter/header-character';
 
 export const Header = async ({ user }: { user: User }) => {
-  const queryClient = new QueryClient();
-
-  await queryClient.prefetchQuery({
-    queryKey: [CHARACTER],
-    queryFn: () => serverActionWithSentry(getCharacterByUserId),
-  });
-
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <header className='flex justify-around bg-white pl-20'>
-        <span>헤더입니다.</span>
-        <HeaderCharacter user={user} />
-      </header>
-    </HydrationBoundary>
+    <header className='flex justify-around bg-white pl-20'>
+      <span>헤더입니다.</span>
+      <HeaderCharacter user={user} />
+    </header>
   );
 };

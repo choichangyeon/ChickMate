be7cc5c160b1d67548cb479f8a78e619d804e1aa
choichangyeon <changyon99@naver.com>
2025-04-09 14:27:03 +0900

    feat:#51 tanstack query 구현

---
+++ src/features/job/hooks/use-bookmark-query.ts
@@ -0,0 +1,21 @@
+import { useQuery } from '@tanstack/react-query';
+import { getBookmarkByJobPostingId } from '@/features/job/api/client-services';
+import { JOB_QUERY_KEY } from '@/constants/query-key';
+import { STALE_TIME } from '@/constants/time';
+
+const { BOOKMARK } = JOB_QUERY_KEY;
+const { MIN } = STALE_TIME;
+
+type Props = {
+  jobPostingId: number;
+};
+
+const useBookmarkQuery = ({ jobPostingId }: Props) => {
+  return useQuery({
+    queryKey: [BOOKMARK, jobPostingId],
+    queryFn: () => getBookmarkByJobPostingId({ jobPostingId }),
+    staleTime: MIN,
+  });
+};
+
+export default useBookmarkQuery;

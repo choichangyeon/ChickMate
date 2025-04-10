8977e5093f444940ee14f1a0fd7018781eda1a5e
ImJaeOne <dlawi0108@naver.com>
2025-04-11 00:48:17 +0900

    feat:#63 캐릭터 생성 라우트 핸들러 구현

--- src/app/api/character/route.ts
+++ src/app/api/character/route.ts
@@ -2,10 +2,11 @@ import { AUTH_MESSAGE, CHARACTER_MESSAGE } from '@/constants/message-constants';
 import { prisma } from '@/lib/prisma';
 import { authOptions } from '@/utils/auth-option';
 import { getServerSession } from 'next-auth';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 const { AUTH_REQUIRED } = AUTH_MESSAGE.RESULT;
 const { GET_DATA_FAILED } = CHARACTER_MESSAGE.INFO;
+const { POST_DATA_FAILED } = CHARACTER_MESSAGE.POST;
 
 export const GET = async () => {
   try {
@@ -16,6 +17,7 @@ export const GET = async () => {
     }
 
     const userId = session.user.id;
+
     const response = await prisma.character.findFirst({
       where: { userId },
     });
@@ -23,5 +25,30 @@ export const GET = async () => {
     return NextResponse.json({ response }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ message: GET_DATA_FAILED }, { status: 500 });
+  }
+};
+
+export const POST = async (request: NextRequest) => {
+  try {
+    const session = await getServerSession(authOptions);
+
+    if (!session || !session.user) {
+      return NextResponse.json({ message: AUTH_REQUIRED }, { status: 401 });
+    }
+
+    const userId = session.user.id;
+
+    const { type } = await request.json();
+
+    const response = await prisma.character.create({
+      data: {
+        userId,
+        type,
+      },
+    });
+
+    return NextResponse.json({ response }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: POST_DATA_FAILED }, { status: 500 });
   }
 };

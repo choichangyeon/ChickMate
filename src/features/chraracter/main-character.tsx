bb9a92053cf291e2f410e82e4aa8e71e017cadee
ImJaeOne <dlawi0108@naver.com>
2025-04-10 17:17:56 +0900

    refactor:#63 characterId 동기화 훅 적용

--- src/features/chraracter/main-character.tsx
+++ src/features/chraracter/main-character.tsx
@@ -2,83 +2,31 @@
 
 import { useGetCharacterQuery } from '@/features/chraracter/hooks/use-get-character-query';
 import { useCharacterStore } from '@/store/use-character-store';
-import { getLevelAndPercentage } from '@/features/chraracter/utils/get-level-and-percent';
 import { useEffect } from 'react';
 import MainCharacterCard from './main-character-card';
-import { PATH } from '@/constants/path-constant';
-import { useRouter } from 'next/navigation';
 import { Session } from 'next-auth';
+import { useCharacterStoreSync } from './hooks/use-character-store-sync';
 
 type Props = {
   session: Session | null;
 };
 
-const { SIGN_IN } = PATH.AUTH;
-
 const MainCharacter = ({ session }: Props) => {
   const { data: characterData, isPending, isError } = useGetCharacterQuery();
-  const setCharacterId = useCharacterStore((state) => state.setCharacterId);
-  const router = useRouter();
 
-  useEffect(() => {
-    if (characterData) {
-      setCharacterId(characterData.id);
-    }
-  }, [characterData, setCharacterId]);
+  useCharacterStoreSync(characterData);
 
-  if (!session)
-    return (
-      <MainCharacterCard
-        level={4}
-        percent={100}
-        name='ChickMate'
-        type='normal'
-        disabled
-        overlayText='로그인이 필요합니다.'
-        onClick={() => router.push(SIGN_IN)}
-      />
-    );
+  if (!session) return <MainCharacterCard disabled overlayText='로그인이 필요합니다.' />;
 
-  // 세션은 있는데 캐릭터가 없음 → 캐릭터 생성 유도
   if (isError || (!isPending && !characterData))
-    return (
-      <MainCharacterCard
-        level={4}
-        percent={0}
-        name='ChickMate'
-        type='normal'
-        disabled
-        overlayText='캐릭터를 생성해주세요.'
-        // onClick={() => router.push()}
-      />
-    );
+    return <MainCharacterCard disabled requiredModal overlayText='캐릭터를 생성해주세요.' />;
 
   if (isPending) {
-    <MainCharacterCard
-      level={4}
-      percent={0}
-      name='ChickMate'
-      type='normal'
-      disabled
-      overlayText='캐릭터 정보를 불러오는 중입니다.'
-      // onClick={() => router.push(CHARACTER_CREATE)}
-    />;
+    <MainCharacterCard disabled overlayText='캐릭터 정보를 불러오는 중입니다.' />;
   }
 
-  // 캐릭터 있음 → 정상 출력
   if (characterData) {
-    const { level, percent } = getLevelAndPercentage(characterData.experience);
-    return (
-      <MainCharacterCard
-        level={level}
-        percent={percent}
-        name='똑똑한 병아리'
-        type={characterData.type}
-        onClick={() => {
-          // 캐릭터 상세 or 모달로 연결 가능
-        }}
-      />
-    );
+    return <MainCharacterCard characterData={characterData} />;
   }
 };
 

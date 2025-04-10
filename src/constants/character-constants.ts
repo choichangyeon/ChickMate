263b94ceda2bb64a12ffa6ea02be70ee74997749
ImJaeOne <dlawi0108@naver.com>
2025-04-11 01:40:32 +0900

    feat:#63 타입별 캐릭터 정보 추가

--- src/constants/character-constants.ts
+++ src/constants/character-constants.ts
@@ -1,3 +1,19 @@
-type LEVEL_EXP_TYPE = [0, 100, 200, 300];
+export const LEVEL_EXP = [0, 100, 300, 1000];
 
-export const LEVEL_EXP: LEVEL_EXP_TYPE = [0, 100, 200, 300];
+export const CHARACTER_INFO = {
+  yellow: {
+    1: { name: '노란 병아리' },
+    2: { name: '노란 병아리' },
+    3: { name: '노란 병아리' },
+    4: { name: '노란 병아리' },
+  },
+  //   추후 캐릭터 타입 추가될 경우
+  //   green: {
+  //     1: { name: '초록 병아리' },
+  //     2: { name: '초록 병아리' },
+  //     3: { name: '초록 병아리' },
+  //     4: { name: '초록 병아리' },
+  //   },
+};
+
+export type CharacterType = keyof typeof CHARACTER_INFO;

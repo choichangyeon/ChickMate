a02f8db345514031d13185270c0d6d623a4360fc
Minjo Park <consistent.312@gmail.com>
2025-04-10 10:29:30 +0900

    refactor:#64 context가 있을 경우 실행하도록 수정

--- src/features/resume/hooks/use-delete-resume-mutation.ts
+++ src/features/resume/hooks/use-delete-resume-mutation.ts
@@ -1,7 +1,7 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { deleteResume } from '../api/client-services';
 import { QUERY_KEY } from '@/constants/query-key';
 import { Resume } from '@prisma/client';
+import { deleteResume } from '@/features/resume/api/client-services';
 
 export const useDeleteResumeMutation = () => {
   const { RESUME } = QUERY_KEY;
@@ -12,14 +12,16 @@ export const useDeleteResumeMutation = () => {
     mutationFn: (resumeId: number) => deleteResume(resumeId),
     onMutate: async (resumeId) => {
       await queryClient.cancelQueries({ queryKey: [RESUME] });
-      const previousResume = queryClient.getQueryData([RESUME]);
+      const previousResume = queryClient.getQueryData([RESUME]) as Resume[] | undefined;
       queryClient.setQueryData([RESUME], (old: Resume[]) => old.filter((resume) => resume.id !== resumeId));
 
       return { previousResume };
     },
 
     onError: (err, resumeId, context) => {
-      queryClient.setQueryData([RESUME], context.previousResume);
+      if (context) {
+        queryClient.setQueryData([RESUME], context.previousResume);
+      }
     },
     onSettled: () => queryClient.invalidateQueries({ queryKey: [RESUME] }),
   });

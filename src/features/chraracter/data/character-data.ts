746e5175cc3858f96af32b2bd4a14e27dc8892cd
ImJaeOne <dlawi0108@naver.com>
2025-04-10 20:52:03 +0900

    refactor:#63 캐릭터 default 변경

--- src/features/chraracter/data/character-data.ts
+++ src/features/chraracter/data/character-data.ts
@@ -3,7 +3,7 @@ import { Character } from '@prisma/client';
 export const defaultCharacter: Character = {
   id: 0,
   userId: 'guest',
-  type: 'normal',
+  type: 'yellow',
   level: 4,
   experience: 0,
   createdAt: new Date(),

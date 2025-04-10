23726ae4044b1f3f912bbdfd394654f22a5d360a
choichangyeon <changyon99@naver.com>
2025-04-10 14:17:49 +0900

    comment:#69 주석 추가

--- src/utils/sanitize-query-params.ts
+++ src/utils/sanitize-query-params.ts
@@ -1,7 +1,7 @@
 /**
  * @function sanitizeQueryParams
  * @param searchParams - URLSearchParams
- * @returns {Record<string, string>} - sanitized query parameters
+ * @returns {Record<string, string>} - 정리된 쿼리 파라미터 객체 구조 분해로 할당
  * @description - 쿼리 파라미터를 정리하는 함수
  */
 export const sanitizeQueryParams = (searchParams: URLSearchParams): Record<string, string> => {

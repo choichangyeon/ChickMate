cb454b9fef15cc48bd3df6d1920a124964a0f484
Minjo Park <consistent.312@gmail.com>
2025-04-10 20:14:46 +0900

    refactor:#78 Title 컴포넌트 cva로 수정

--- src/components/ui/title.tsx
+++ src/components/ui/title.tsx
@@ -1,31 +1,51 @@
-import clsx from 'clsx';
-import { fontAlign, fontColor, titleFontSize } from '@/styles/typography-styles';
+import { cva, VariantProps } from 'class-variance-authority';
+import React from 'react';
 
-type HeadingTag = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
-type TitleSize = keyof typeof titleFontSize;
-type TitleColor = keyof typeof fontColor;
-type TextAlign = keyof typeof fontAlign;
+const titleVariant = cva('font-bold', {
+  variants: {
+    size: {
+      lg: 'text-lg',
+      xl: 'text-xl',
+      '2xl': 'text-2xl',
+      '3xl': 'text-3xl',
+      '4xl': 'text-4xl',
+      '5xl': 'text-5xl',
+      '6xl': 'text-6xl',
+    },
+    color: {
+      default: 'text-black',
+      gray: 'text-gray-500',
+      red: 'text-red-500',
+      primary: 'text-primary',
+    },
+    align: {
+      left: 'text-left',
+      center: 'text-center',
+      right: 'text-right',
+    },
+  },
+  defaultVariants: {
+    size: 'lg',
+    color: 'default',
+    align: 'left',
+  },
+});
 
-type Props = {
-  as?: HeadingTag;
-  size?: TitleSize;
-  color?: TitleColor;
-  align?: TextAlign;
+type AllowedHeadings = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+
+type TextProps<T extends AllowedHeadings = 'h1'> = {
+  as?: T;
   children: React.ReactNode;
-};
+} & VariantProps<typeof titleVariant> &
+  Omit<React.ComponentPropsWithoutRef<T>, 'as'>;
 
-/**
- * Title 컴포넌트
- * @param as heading 태그
- * @param size 폰트 크기
- * @param color 폰트 컬러
- * @returns JSX
- */
-const Title = ({ as = 'h1', size = 'xl', color = 'default', align = 'left', children }: Props) => {
-  const Component = as;
-  const titleClassName = clsx('font-bold', titleFontSize[size], fontColor[color], fontAlign[align]);
-
-  return <Component className={titleClassName}>{children}</Component>;
+const Title = <T extends AllowedHeadings = 'h1'>({ as, size, color, align, children, ...props }: TextProps<T>) => {
+  const Component = as || 'h1';
+  return (
+    <Component className={titleVariant({ size, color, align })} {...props}>
+      {children}
+    </Component>
+  );
 };
 
 export default Title;

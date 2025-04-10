149d581f162fd1081eed2fb0cf5ec3d4625b0326
ImJaeOne <dlawi0108@naver.com>
2025-04-10 16:26:07 +0900

    refactor:#63 character store persist 삭제

--- src/store/use-character-store.ts
+++ src/store/use-character-store.ts
@@ -1,5 +1,4 @@
 import { create } from 'zustand';
-import { persist } from 'zustand/middleware';
 
 type CharacterState = {
   characterId: number | null;
@@ -11,14 +10,7 @@ const initialState: CharacterState = {
   setCharacterId: () => {},
 };
 
-export const useCharacterStore = create<CharacterState>()(
-  persist(
-    (set) => ({
-      characterId: initialState.characterId,
-      setCharacterId: (id) => set({ characterId: id }),
-    }),
-    {
-      name: 'character-store',
-    }
-  )
-);
+export const useCharacterStore = create<CharacterState>()((set) => ({
+  characterId: initialState.characterId,
+  setCharacterId: (id) => set({ characterId: id }),
+}));

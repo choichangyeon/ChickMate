8cb19cd31fd82b3c26e70b4e62071f0dae03fb54
ImJaeOne <dlawi0108@naver.com>
2025-04-11 15:33:58 +0900

    refactor:#83 session header에서 받아오도록 수정

--- src/app/(with-nav)/(with-header)/layout.tsx
+++ src/app/(with-nav)/(with-header)/layout.tsx
@@ -1,14 +1,9 @@
 import { Header } from '@/features/layout/header';
-import { User } from '@/types/user';
-import { authOptions } from '@/utils/auth-option';
-import { getServerSession } from 'next-auth';
 
 const HeaderLayout = async ({ children }: { children: React.ReactNode }) => {
-  const session = await getServerSession(authOptions);
-  const user = session?.user as User;
   return (
     <>
-      <Header user={user} />
+      <Header />
       {children}
     </>
   );
--- src/features/layout/header.tsx
+++ src/features/layout/header.tsx
@@ -1,11 +1,14 @@
-import { User } from '@/types/user';
 import HeaderCharacter from '@/features/character/header-character';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/utils/auth-option';
 
-export const Header = async ({ user }: { user: User }) => {
+export const Header = async () => {
+  const session = await getServerSession(authOptions);
+
   return (
     <header className='flex justify-around bg-white pl-20'>
       <span>헤더입니다.</span>
-      <HeaderCharacter user={user} />
+      <HeaderCharacter session={session} />
     </header>
   );
 };

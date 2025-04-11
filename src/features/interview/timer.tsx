15b01891d86cab4b729b523dad14117ab2c1efe0
Minjo Park <consistent.312@gmail.com>
2025-04-11 14:33:49 +0900

    refactor:#42 타이머 컴포넌트 분리

---
+++ src/features/interview/timer.tsx
@@ -0,0 +1,9 @@
+const Timer = () => {
+  return (
+    <div className='bg-primary-orange-600 w-[526px] flex-shrink-0 p-6'>
+      ㅇㄴㅁㅇㄴㅁㅇㅁㄴㅇㄴㅁㅇㅁㄴㅇㄴㅁㅇㅁㄴㅇㄴㅁdsadsadasdasdasdㄴ
+    </div>
+  );
+};
+
+export default Timer;

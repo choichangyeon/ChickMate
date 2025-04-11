4575c0033bdc64c8c25a3134ce92f41abb14a386
ImJaeOne <dlawi0108@naver.com>
2025-04-11 12:57:27 +0900

    refactor: 캐릭터 타입 지정

--- src/constants/character-constants.ts
+++ src/constants/character-constants.ts
@@ -1,19 +1,19 @@
 export const LEVEL_EXP = [0, 100, 300, 1000];
 
-export const CHARACTER_INFO = {
-  yellow: {
-    1: { name: '노란 병아리' },
-    2: { name: '노란 병아리' },
-    3: { name: '노란 병아리' },
-    4: { name: '노란 병아리' },
-  },
-  //   추후 캐릭터 타입 추가될 경우
-  //   green: {
-  //     1: { name: '초록 병아리' },
-  //     2: { name: '초록 병아리' },
-  //     3: { name: '초록 병아리' },
-  //     4: { name: '초록 병아리' },
-  //   },
-};
+type CharacterDescription = Record<number, { name: string }>;
 
-export type CharacterType = keyof typeof CHARACTER_INFO;
+type CharacterInfo = Record<string, CharacterDescription>;
+
+export const CHARACTER_INFOMATIONS: CharacterInfo = {
+  yellow: {
+    1: { name: '아기 병아리' },
+    2: { name: '안경 병아리' },
+    3: { name: '화난 병아리' },
+    4: { name: '미친 병아리' },
+  },
+  // 추후 확장 가능
+  // green: {
+  //   1: { name: '초록 병아리' },
+  //   ...
+  // },
+};

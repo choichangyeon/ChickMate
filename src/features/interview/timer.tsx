733543baef829efba4eb3e2f28d8044fb3e49501
Minjo Park <consistent.312@gmail.com>
2025-04-11 15:10:50 +0900

    design:#42 타이머 디자인 스타일 적용

--- src/features/interview/timer.tsx
+++ src/features/interview/timer.tsx
@@ -1,7 +1,22 @@
+import Typography from '@/components/ui/typography';
+
 const Timer = () => {
   return (
-    <div className='bg-primary-orange-600 w-[526px] flex-shrink-0 p-6'>
-      ㅇㄴㅁㅇㄴㅁㅇㅁㄴㅇㄴㅁㅇㅁㄴㅇㄴㅁㅇㅁㄴㅇㄴㅁdsadsadasdasdasdㄴ
+    <div className='flex h-[220px] w-[526px] flex-shrink-0 flex-col items-center justify-center gap-4 border-2 p-6'>
+      <div className='flex flex-col items-center'>
+        <Typography size='2xl'>제한시간 안에 답변을 완료하세요</Typography>
+        <Typography size='sm' weight='medium'>
+          타이머가 종료되면 자동으로 답변이 종료됩니다
+        </Typography>
+      </div>
+      <div>
+        <Typography color='primary-600' size='3xl' weight='black'>
+          3:00
+        </Typography>
+      </div>
+      <div>
+        <button>답변 종료</button>
+      </div>
     </div>
   );
 };

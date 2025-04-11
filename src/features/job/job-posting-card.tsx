01fee1f848789ff9611d579b71f5e42601defb23
choichangyeon <changyon99@naver.com>
2025-04-11 16:29:10 +0900

    refactor:#69 채용공고 카드 리팩토링

--- src/features/job/job-posting-card.tsx
+++ src/features/job/job-posting-card.tsx
@@ -9,14 +9,24 @@ import clsx from 'clsx';
 type Props = {
   jobPosting: JobPosting;
   children?: React.ReactNode;
+  // TODO: iconButton type 수정
+  iconButton?: boolean;
 };
 
-const JobPostingCard = ({ jobPosting, children }: Props) => {
+const JobPostingCard = ({ jobPosting,  iconButton, children, }: Props) => {
   const { company, title, experienceType, expiredAt, postedAt } = jobPosting;
   const postedAtDate = formatDate({ input: postedAt });
   const expiredAtDate = formatDate({ input: expiredAt });
   return (
-    <Card iconButton={true}>
+    <Card>
+      {iconButton && (
+        // TODO: iconButton component 적용
+        <div className='absolute right-[20px] top-[12px] h-6 w-6'>
+          <div className='absolute left-[4px] top-[4px] h-0.5 w-4 bg-black' />
+          <div className='absolute left-[8px] top-[2px] h-1 w-2 bg-black' />
+          <div className='absolute left-[5px] top-[8px] h-3.5 w-3.5 bg-black' />
+        </div>
+      )}
       <div>
         <Typography weight='bold' color='gray-500'>
           {company}
@@ -33,7 +43,7 @@ const JobPostingCard = ({ jobPosting, children }: Props) => {
           </Typography>
         </div>
       </div>
-      {/* badge area */}
+      {/* TODO: badge area */}
       <div className={badgeClassName}>{children} </div>
     </Card>
   );

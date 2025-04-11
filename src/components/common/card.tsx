eb03092826e8b0c48cd57f69ed05dd6e1e2a52c1
choichangyeon <changyon99@naver.com>
2025-04-11 19:05:13 +0900

    refactor:#69 shadow 속성 삭제

--- src/components/common/card.tsx
+++ src/components/common/card.tsx
@@ -18,7 +18,7 @@ const Card = ({ children, className, onClick }: Props) => {
 export default Card;
 //TODO: bg-emerald-900/0 -> color 수정, outline-gray-200 -> color 수정
 const cardClassName = clsx(
-  'relative items-start justify-between rounded-lg bg-emerald-900/0 shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] outline outline-1 outline-offset-[-1px] outline-gray-200',
+  'relative items-start justify-between rounded-lg bg-emerald-900/0 outline outline-1 outline-offset-[-1px] outline-gray-200',
   'w-[307px] h-[173px] p-[24px]'
 );
 

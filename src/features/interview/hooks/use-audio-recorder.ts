0cada1642341044e08e2752869158dd5cb42fa4b
Minjo Park <consistent.312@gmail.com>
2025-04-12 00:39:30 +0900

    comment:#42 주석 제거

--- src/features/interview/hooks/use-audio-recorder.ts
+++ src/features/interview/hooks/use-audio-recorder.ts
@@ -1,24 +1,16 @@
 import { useRef, useState } from 'react';
 
 export const useAudioRecorder = () => {
-  // 창연님을 위한 주석이니 PR 하시는 분들은 자세히 안 읽으셔도 됩니다.
-
-  // MediaRecorder 인스턴스를 저장할 곳, 녹음 시작/중지 때 사용됨
   const audioRecorderRef = useRef<MediaRecorder | null>(null);
-  // 녹음 중인지 아닌지의 상태
   const [isRecording, setIsRecording] = useState(false);
-  // 녹음이 끝난 뒤, 재생하거나 다운로드할 수 있도록 오디오 Blob을 저장함
   const [audioBlob, setAudioBlob] = useState<Blob | null>(null);
-  // MediaRecorder가 전달하는 오디오 데이터를 작은 조각(blob) 단위로 모아두는 곳
   const audioChunksRef = useRef<Blob[]>([]);
 
   //녹음 시작
   const startRecording = async () => {
     try {
-      // 사용자한테 마이크 접근 권한 요청, 승인되면 MediaStream을 받아옴
       const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
 
-      // 오디오 포맷 설정 (MIME 타입 수정)
       const mediaRecorder = new MediaRecorder(stream, {
         mimeType: 'audio/webm;codecs=opus',
       });

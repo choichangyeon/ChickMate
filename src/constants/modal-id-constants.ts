4e9d2ee9dc55dd9f72f5788148883dff88efbd2b
Minjo Park <consistent.312@gmail.com>
2025-04-13 21:46:16 +0900

    refactor:#86 모달 사용하는 로직 수정

--- src/constants/modal-id-constants.ts
+++ src/constants/modal-id-constants.ts
@@ -1,4 +1,5 @@
 export const MODAL_ID = {
   DRAFT_RESUME: 'draftResume',
   CHARACTER_CREATE: 'characterCreate',
+  USER_META_DATA: 'useMetaData',
 };
--- src/features/my-page/viewing-field.tsx
+++ src/features/my-page/viewing-field.tsx
@@ -1,20 +1,23 @@
 'use client';
 import React from 'react';
-import Modal from '@/components/common/modal';
+import Modal from '@/components/ui/modal';
 import UserMetaDataModal from '@/features/user-meta-data/user-meta-data-modal';
 import { useModalStore } from '@/store/use-modal-store';
 import Typography from '@/components/ui/typography';
+import { MODAL_ID } from '@/constants/modal-id-constants';
+
+const { USER_META_DATA } = MODAL_ID;
 
 const ViewingField = () => {
-  const isModalOpen = useModalStore((state) => state.isModalOpen);
   const toggleModal = useModalStore((state) => state.toggleModal);
+  const isModalOpen = useModalStore((state) => state.getIsModalOpen(USER_META_DATA));
 
   return (
     <section className='text- w-1/2 border-2'>
       <Typography as='h3'>삐약이카드</Typography>
-      <button onClick={toggleModal}>모달열자</button>
+      <button onClick={() => toggleModal(USER_META_DATA)}>모달열자</button>
       {isModalOpen && (
-        <Modal>
+        <Modal modalId={USER_META_DATA}>
           <UserMetaDataModal />
         </Modal>
       )}

18bc7a654e64a2b52e0c1b5857f0b9b3e19fdc41
ImJaeOne <dlawi0108@naver.com>
2025-04-13 23:26:42 +0900

    refactor:#83 optimistic update 적용

--- src/hooks/mutations/use-post-experience-mutation.ts
+++ src/hooks/mutations/use-post-experience-mutation.ts
@@ -1,24 +1,32 @@
-import { API_HEADER, API_METHOD } from '@/constants/api-method-constants';
-import { ROUTE_HANDLER_PATH } from '@/constants/path-constant';
-import { fetchWithSentry } from '@/utils/fetch-with-sentry';
+import { QUERY_KEY } from '@/constants/query-key';
+import { patchCharacterExperience, PatchProps } from '@/features/character/api/client-services';
+import { Character } from '@prisma/client';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
-const { PATCH } = API_METHOD;
-const { JSON_HEADER } = API_HEADER;
-const { EXPERIENCE } = ROUTE_HANDLER_PATH.CHARACTER;
+const { CHARACTER } = QUERY_KEY;
 
 export const usePostExperienceMutation = () => {
   const queryClient = useQueryClient();
   return useMutation({
-    mutationFn: async ({ characterId, amount }: { characterId: number; amount: number }) => {
-      await fetchWithSentry(EXPERIENCE, {
-        method: PATCH,
-        headers: JSON_HEADER,
-        body: JSON.stringify({ characterId, amount }),
+    mutationFn: ({ characterId, amount, history }: PatchProps) =>
+      patchCharacterExperience({ characterId, amount, history }),
+    onMutate: async ({ characterId, amount }) => {
+      await queryClient.cancelQueries({ queryKey: [CHARACTER] });
+      const previousCharacter = queryClient.getQueryData([CHARACTER]) as Character | undefined;
+      queryClient.setQueryData([CHARACTER], (old: Character | undefined) => {
+        if (!old) return old;
+        return {
+          ...old,
+          experience: old.experience + amount,
+        };
       });
+      return { previousCharacter };
     },
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['character'] });
+    onError: (err, characterId, context) => {
+      if (context) {
+        queryClient.setQueryData([CHARACTER], context.previousCharacter);
+      }
     },
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: [CHARACTER] }),
   });
 };

3ae1001216bec3f65f7142bccf4bc01c96741744
ImJaeOne <dlawi0108@naver.com>
2025-04-13 23:31:29 +0900

    move:#83 파일 위치 이동

--- src/app/(with-nav)/(with-header)/character/page.tsx
+++ src/app/(with-nav)/(with-header)/character/page.tsx
@@ -1,16 +1,16 @@
 'use client';
 
-import { usePostExperienceMutation } from '@/hooks/mutations/use-post-experience-mutation';
+import { usePostExperienceMutation } from '@/features/character/hooks/use-post-experience-mutation';
 import { useCharacterStore } from '@/store/use-character-store';
 
 const CharacterPage = () => {
   const characterId = useCharacterStore((state) => state.characterId);
 
-  const { mutate: updateExp } = usePostExperienceMutation();
+  const { mutate: expUpdateMutation } = usePostExperienceMutation();
 
   const handleExperienceUp = () => {
     if (characterId === null) return; 
-    updateExp({ characterId, amount: 10 });
+    expUpdateMutation({ characterId, amount: 10, history: 'history' });
   };
 
   return (

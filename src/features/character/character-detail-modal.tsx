5620e7a34e5f7dc3bfe93d53df1b44abdd03764b
ImJaeOne <dlawi0108@naver.com>
2025-04-14 00:21:41 +0900

    feat:#83 character detail 히스토리 탭 구현

--- src/features/character/character-detail-modal.tsx
+++ src/features/character/character-detail-modal.tsx
@@ -7,6 +7,7 @@ import { useGetCharacterQuery } from './hooks/use-get-character-query';
 import { useCharacterStoreSync } from './hooks/use-character-store-sync';
 import { useState } from 'react';
 import { MODAL_ID } from '@/constants/modal-id-constants';
+import CharacterHistoryList from './character-history';
 
 type Props = {
   session: Session;
@@ -16,31 +17,45 @@ type Tab = 'info' | 'history';
 
 const CharacterDetailModal = ({ session }: Props) => {
   const [selectedTab, setSelectedTab] = useState<Tab>('info');
-  const { data: characterData } = useGetCharacterQuery();
+  const { data: characterData, isPending, isError } = useGetCharacterQuery();
 
   useCharacterStoreSync(characterData);
 
+  if (isPending) {
+    return <div>캐릭터 정보를 불러오는 중입니다...</div>;
+  }
+
+  if (isError) {
+    return <div>캐릭터 정보를 불러올 수 없습니다...</div>;
+  }
+
   return (
     <Modal modalId={MODAL_ID.CHARACTER_DETAIL}>
-      <div className='flex justify-around border-gray-300'>
-        <div
-          onClick={() => setSelectedTab('info')}
-          className={`cursor-pointer px-4 py-2 ${
-            selectedTab === 'info' ? 'border-b-2 border-black font-bold' : 'text-gray-500'
-          }`}
-        >
-          내 캐릭터
+      <div className='h-[410px]'>
+        <div className='flex justify-around border-gray-300'>
+          <div
+            onClick={() => setSelectedTab('info')}
+            className={`cursor-pointer px-4 py-2 ${
+              selectedTab === 'info' ? 'border-b-2 border-black font-bold' : 'text-gray-500'
+            }`}
+          >
+            내 캐릭터
+          </div>
+          <div
+            onClick={() => setSelectedTab('history')}
+            className={`cursor-pointer px-4 py-2 ${
+              selectedTab === 'history' ? 'border-b-2 border-black font-bold' : 'text-gray-500'
+            }`}
+          >
+            성장 히스토리
+          </div>
         </div>
-        <div
-          onClick={() => setSelectedTab('history')}
-          className={`cursor-pointer px-4 py-2 ${
-            selectedTab === 'history' ? 'border-b-2 border-black font-bold' : 'text-gray-500'
-          }`}
-        >
-          성장 히스토리
-        </div>
+        {selectedTab === 'info' && !!characterData ? (
+          <MyCharacter session={session} characterData={characterData} />
+        ) : (
+          <CharacterHistoryList characterData={characterData} />
+        )}
       </div>
-      {selectedTab === 'info' ? <MyCharacter session={session} characterData={characterData} /> : null}
     </Modal>
   );
 };

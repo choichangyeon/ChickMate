a63f46b9e1f5157940117f982a4ff9ecabcfa688
ImJaeOne <dlawi0108@naver.com>
2025-04-14 02:55:56 +0900

    feat:#83 history 추가

--- src/constants/character-constants.ts
+++ src/constants/character-constants.ts
@@ -17,3 +17,32 @@ export const CHARACTER_INFOMATIONS: CharacterInfo = {
   //   ...
   // },
 };
+
+export const CHARACTER_HISTORY_KEY = {
+  LOGIN: 'LOGIN',
+  RESUME_SUBMISSION: 'RESUME_SUBMISSION',
+  INTERVIEW_COMPLETION: 'INTERVIEW_COMPLETION',
+  JOB_BOOKMARK: 'JOB_BOOKMARK',
+  // 테스트용
+  GENERAL_HISTORY: 'GENERAL_HISTORY',
+} as const;
+
+export const CHARACTER_HISTORY_KOR = {
+  LOGIN: '로그인',
+  RESUME_SUBMISSION: '자소서 작성 완료',
+  INTERVIEW_COMPLETION: '면접 완료',
+  JOB_BOOKMARK: '채용공고 북마크',
+  // 테스트용
+  GENERAL_HISTORY: '테스트용 히스토리',
+};
+
+const { LOGIN, RESUME_SUBMISSION, INTERVIEW_COMPLETION, JOB_BOOKMARK, GENERAL_HISTORY } = CHARACTER_HISTORY_KEY;
+
+export const CHARACTER_HISTORY = {
+  [LOGIN]: { amount: 10 },
+  [RESUME_SUBMISSION]: { amount: 50 },
+  [INTERVIEW_COMPLETION]: { amount: 100 },
+  [JOB_BOOKMARK]: { amount: 5 },
+  // 테스트용
+  [GENERAL_HISTORY]: { amount: 10 },
+} as const;

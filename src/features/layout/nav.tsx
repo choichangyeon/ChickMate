48c0f29ffc6ff66bc533a7baa9e63e178563d44d
Hye10 <dhye410@daum.net>
2025-04-13 22:49:03 +0900

    design:#90 nav figma 적용

--- src/features/layout/nav.tsx
+++ src/features/layout/nav.tsx
@@ -2,12 +2,13 @@
 
 import { useMemo } from 'react';
 import ButtonNav from '@/features/layout/button-nav';
-import type { NavItems } from '@/features/layout/data/nav-items';
-import { Private_Nav_Items, Public_Nav_Items } from '@/features/layout/data/nav-items';
+import { Private_Nav_Items, Public_Nav_Items, type NavItems } from '@/features/layout/data/nav-items';
+
 import LinkNav from '@/features/layout/link-nav';
 import { User } from '@/types/user';
 import { PATH } from '@/constants/path-constant';
 import Link from 'next/link';
+import clsx from 'clsx';
 
 type Props = {
   user?: User;
@@ -17,11 +18,13 @@ const { ON_BOARDING } = PATH;
 export const Nav = ({ user }: Props) => {
   const menus: NavItems[] = useMemo(() => (user ? Private_Nav_Items : Public_Nav_Items), [user]);
   return (
-    <nav className='w-[100px] bg-red-950 text-white opacity-90'>
-      <Link href={ON_BOARDING}>ChickMate</Link>
-      <ul>
+    <nav className='flex h-screen w-[52px] flex-col items-center justify-center bg-[#222] px-1 py-8 text-cool-gray-50'>
+      <Link href={ON_BOARDING} className='mb-12 block rounded-lg border'>
+        칰
+      </Link>
+      <ul className={clsx('flex h-full flex-col items-center gap-2', user ? 'justify-between' : 'flex-start')}>
         {menus.map((menu) => (
-          <li key={`menu_${menu.name}`}>
+          <li key={`menu_${menu.name}`} className={menu.class}>
             {menu.type === 'link' ? <LinkNav menu={menu} /> : <ButtonNav menu={menu} />}
           </li>
         ))}

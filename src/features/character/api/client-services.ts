f1b2f57c1dac8f6a8a617884f609d90ebf07a8d6
ImJaeOne <dlawi0108@naver.com>
2025-04-13 23:25:52 +0900

    feat:#83 경험치 획득 api 로직 구현

--- src/features/character/api/client-services.ts
+++ src/features/character/api/client-services.ts
@@ -1,10 +1,10 @@
 import { API_HEADER, API_METHOD } from '@/constants/api-method-constants';
 import { ROUTE_HANDLER_PATH } from '@/constants/path-constant';
 import { fetchWithSentry } from '@/utils/fetch-with-sentry';
-import { Character } from '@prisma/client';
+import { Character, CharacterHistory } from '@prisma/client';
 
-const { INFO } = ROUTE_HANDLER_PATH.CHARACTER;
-const { GET, POST } = API_METHOD;
+const { INFO, EXPERIENCE } = ROUTE_HANDLER_PATH.CHARACTER;
+const { GET, POST, PATCH } = API_METHOD;
 const { JSON_HEADER } = API_HEADER;
 
 /**
@@ -23,7 +23,7 @@ export const getCharacterByUserId = async (): Promise<Character> => {
   return response;
 };
 
-type Props = Pick<Character, 'type'>;
+type CreateProps = Pick<Character, 'type'>;
 
 /**
  * 새로운 캐릭터를 생성합니다.
@@ -33,11 +33,38 @@ type Props = Pick<Character, 'type'>;
  * @returns {Promise<Character>} 생성된 캐릭터 정보를 담은 Promise 객체
  * @throws {Error}
  */
-export const postCreateCharacter = async ({type}: Props): Promise<Character> => {
+export const postCreateCharacter = async ({ type }: CreateProps): Promise<Character> => {
   const { response } = await fetchWithSentry(INFO, {
     method: POST,
     headers: JSON_HEADER,
     body: JSON.stringify({ type }),
+  });
+
+  return response;
+};
+
+export type PatchProps = {
+  characterId: Character['id'];
+  amount: Character['experience'];
+  history: CharacterHistory['history'];
+};
+
+/**
+ * 캐릭터의 경험치를 증가시킵니다.
+ * @async
+ * @function patchCharacterExperience
+ * @param {Object} props - 캐릭터 ID, 획득한 경험치, 히스토리 메시지를 포함한 객체
+ * @param {number} props.characterId - 경험치를 획득할 캐릭터의 ID
+ * @param {number} props.amount - 증가시킬 경험치 양
+ * @param {string} props.history - 경험치 획득 내역(히스토리)
+ * @returns {Promise<Character>} 업데이트된 캐릭터 정보를 담은 Promise 객체
+ * @throws {Error} 요청 실패 시 에러를 던집니다
+ */
+export const patchCharacterExperience = async ({ characterId, amount, history }: PatchProps): Promise<Character> => {
+  const { response } = await fetchWithSentry(EXPERIENCE, {
+    method: PATCH,
+    headers: JSON_HEADER,
+    body: JSON.stringify({ characterId, amount, history }),
   });
 
   return response;

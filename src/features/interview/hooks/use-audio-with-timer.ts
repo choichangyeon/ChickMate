72fd48a5f2b9196df56f104b51fde5dc65baea62
Minjo Park <consistent.312@gmail.com>
2025-04-15 00:58:48 +0900

    feat:#42 AI 면접관 연결까지 완료

--- src/features/interview/hooks/use-audio-with-timer.ts
+++ src/features/interview/hooks/use-audio-with-timer.ts
@@ -1,14 +1,31 @@
 import { useState } from 'react';
 import { useAudioRecorder } from '@/features/interview/hooks/use-audio-recorder';
 import { useTimer } from '@/features/interview/hooks/use-timer';
-import { postSpeechToText } from '@/features/interview/api/client-services';
-import { InterviewHistory } from '@prisma/client';
+import { getOpenAIResponse, postSpeechToText } from '@/features/interview/api/client-services';
+import { INTERVIEW_PROMPT } from '@/constants/interview-constants';
+import type { Message } from '@/types/message';
+import type { InterviewHistoryWithResume } from '@/types/interview';
 
-export const useAudioWithTimer = (duration: number, interviewHistory: InterviewHistory | null) => {
-  const [transcribedText, setTranscribedText] = useState('');
+const { CALM_PROMPT, PRESSURE_PROMPT } = INTERVIEW_PROMPT;
 
+export const useAudioWithTimer = (duration: number, interviewHistory: InterviewHistoryWithResume) => {
+  const { interviewType, resume } = interviewHistory;
+  const type = interviewType === 'calm' ? CALM_PROMPT : PRESSURE_PROMPT;
+
+  /** state */
+  // const [transcribedText, setTranscribedText] = useState('');
+  const init_state: Message[] = [
+    {
+      role: 'system',
+      content: [...type.content, { type: 'text', text: `지원자의 자기소개서: ${resume}` }],
+    },
+  ];
+  const [messageList, setMessageList] = useState<Message[]>(init_state);
+
+  /** hook */
   const { isRecording, startRecording, stopRecording } = useAudioRecorder();
 
+  /** function */
   const handleTimerComplete = () => {
     stopRecording();
   };
@@ -20,7 +37,7 @@ export const useAudioWithTimer = (duration: number, interviewHistory: InterviewH
 
   const startRecordingWithTimer = () => {
     startRecording();
-    setTranscribedText('');
+    // setTranscribedText('');
     startTimer();
   };
 
@@ -29,11 +46,36 @@ export const useAudioWithTimer = (duration: number, interviewHistory: InterviewH
     const blob = await stopRecording();
 
     try {
-      const text = await postSpeechToText({ blob });
-      setTranscribedText(text);
+      const answerText = await postSpeechToText({ blob });
+      console.log(answerText);
+      getOpenAIInterviewContent(answerText);
     } catch (error) {
       if (error instanceof Error) {
         alert(error.message);
+      }
+    }
+  };
+
+  const getOpenAIInterviewContent = async (answerText: string) => {
+    try {
+      const updatedMessageList: Message[] = [
+        ...messageList,
+        {
+          role: 'user',
+          content: [
+            {
+              type: 'text',
+              text: answerText,
+            },
+          ],
+        },
+      ];
+      const response = await getOpenAIResponse({ messageList: updatedMessageList });
+      setMessageList(response);
+      console.log(response);
+    } catch (error) {
+      if (error instanceof Error) {
+        alert(error);
       }
     }
   };

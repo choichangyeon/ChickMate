3dd7fb0016a277cec031a924445d3538a22bf144
choichangyeon <changyon99@naver.com>
2025-04-15 01:35:45 +0900

    feat:#75 채용공고 페이지 UI 구현

--- src/app/(with-nav)/(with-header)/job/page.tsx
+++ src/app/(with-nav)/(with-header)/job/page.tsx
@@ -1,7 +1,6 @@
 import { QUERY_KEY } from '@/constants/query-key';
 import { STALE_TIME } from '@/constants/time-constants';
 import { getJobByUserMetaData } from '@/features/job/api/client-services';
-import BookmarkComponent from '@/features/job/bookmark-component';
 import JobPostingCard from '@/components/common/job-posting-card';
 import { getUserMetaData } from '@/features/user-meta-data/api/client-services';
 import { UserMetaDataType } from '@/types/user-meta-data-type';
@@ -9,6 +8,7 @@ import { authOptions } from '@/utils/auth-option';
 import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
 import { getServerSession } from 'next-auth';
 import JobPostingsBox from '@/features/job/job-postings-box';
+import Typography from '@/components/ui/typography';
 
 const { JOB_POSTING } = QUERY_KEY;
 const { AN_HOUR } = STALE_TIME;
@@ -28,11 +28,24 @@ const JobPage = async () => {
   });
 
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      JobPage
-      {user && <JobPostingsBox userId={user.id} />}
-      {/* <JobPostingCard jobPosting={mockJobPosting} iconButton={true}></JobPostingCard> */}
-    </HydrationBoundary>
+    <main className='px-12 py-8'>
+      <section className='mb-4'>
+        <Typography color='primary-600' as='h1' size='3xl' weight='bold'>
+          맞춤형
+          <span className='text-cool-gray-900'> 채용공고</span>
+        </Typography>
+        <Typography color='gray-500' size='xl'>
+          프로필에 작성된 정보를 통해 맞춤형 채용공고를 추천해드립니다
+        </Typography>
+      </section>
+      <article>
+        <HydrationBoundary state={dehydrate(queryClient)}>
+          JobPage
+          {user && <JobPostingsBox userId={user.id} />}
+          <JobPostingCard jobPosting={mockJobPosting} iconButton={true}></JobPostingCard>
+        </HydrationBoundary>
+      </article>
+    </main>
   );
 };
 

b48069a8b6a94d221e5b72d83c33898893b8bf5a
choichangyeon <changyon99@naver.com>
2025-04-14 17:48:30 +0900

    comment:#66 TODO 주석

--- src/features/interview/resume-cards-box.tsx
+++ src/features/interview/resume-cards-box.tsx
@@ -5,7 +5,7 @@ import useResumeStore from '@/features/interview/hooks/use-resume-store';
 import { useSubmitResumesQuery } from '@/hooks/queries/use-submit-resumes-query';
 
 const ResumeCardsBox = () => {
-  const { id: selectedId, setResume } = useResumeStore();
+  const { resumeId: selectedId, setResume } = useResumeStore();
   const { data: resumeList, isLoading, isError } = useSubmitResumesQuery();
 
   useEffect(() => {
@@ -18,7 +18,7 @@ const ResumeCardsBox = () => {
 
   if (isLoading) return <div>Loading...</div>;
   if (isError || !resumeList) return <div>Error...</div>;
-  // TODO: 자소서 작성 페이지 라우팅 UI 구현
+  // TODO: 자소서 작성 페이지 라우팅 UI 구현 -> 민철님에게 요청 완료
   if (resumeList.length === 0) return <div>no data 1</div>;
 
   return (

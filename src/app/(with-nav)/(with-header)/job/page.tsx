11025717e69069d7ff87e95260360a8f4533f262
choichangyeon <changyon99@naver.com>
2025-04-15 01:37:33 +0900

    move:#75 mock data 이동

--- src/app/(with-nav)/(with-header)/job/page.tsx
+++ src/app/(with-nav)/(with-header)/job/page.tsx
@@ -40,9 +40,7 @@ const JobPage = async () => {
       </section>
       <article>
         <HydrationBoundary state={dehydrate(queryClient)}>
-          JobPage
           {user && <JobPostingsBox userId={user.id} />}
-          <JobPostingCard jobPosting={mockJobPosting} iconButton={true}></JobPostingCard>
         </HydrationBoundary>
       </article>
     </main>
@@ -50,21 +48,3 @@ const JobPage = async () => {
 };
 
 export default JobPage;
-
-const mockJobPosting = {
-  id: 1,
-  company: '삼성전자',
-  title: '삼성전자 신입사원 모집',
-  experienceType: '신입',
-  expiredAt: new Date('2023-12-31'),
-  postedAt: new Date('2023-01-01'),
-  jobType: '의료',
-  location: {
-    mainRegion: '서울',
-    subRegion: '강남구',
-  },
-  educationLevel: '대졸(4년)',
-  url: 'https://www.samsung.com/sec/careers/',
-  employmentType: '정규직',
-  createdAt: new Date(),
-};

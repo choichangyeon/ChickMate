3c7ad955160b397710091ab3b31cea0bd239edbc
Minjo Park <consistent.312@gmail.com>
2025-04-16 01:05:19 +0900

    design:#111 자소서 입력창 디자인 완료

--- src/features/resume/question-answer-field.tsx
+++ src/features/resume/question-answer-field.tsx
@@ -1,3 +1,4 @@
+import Trash from '@/components/icons/trash';
 import Typography from '@/components/ui/typography';
 import type { Field } from '@/types/resume';
 
@@ -12,32 +13,40 @@ const QuestionAnswerField = ({ field, onChange, onDelete }: Props) => {
   const MAX_ANSWER_LENGTH = 1000;
 
   return (
-    <div className='flex w-[600px] flex-col'>
-      <textarea
-        id={String(id)}
-        name='question'
-        value={question}
-        onChange={onChange}
-        rows={3}
-        placeholder='질문을 입력해주세요.'
-        required
-        className='resize-none'
-      />
+    <div className='border-cool-gray-200 flex h-[444px] w-full flex-col gap-4 rounded-lg border-2 p-8'>
+      <div className='flex flex-col gap-2'>
+        <div className='flex w-full justify-between'>
+          <Typography weight='normal' color='primary-600'>
+            질문 1
+          </Typography>
+          <button type='button' onClick={() => onDelete(id)} className='flex gap-5'>
+            <span className='font-semibold text-cool-gray-500'>질문 삭제</span> <Trash />
+          </button>
+        </div>
+        <textarea
+          id={String(id)}
+          name='question'
+          value={question}
+          onChange={onChange}
+          rows={1}
+          placeholder='질문을 입력하세요.'
+          required
+          className='resize-none text-xl font-normal text-cool-gray-900 scrollbar-hide focus:outline-none'
+        />
+      </div>
+      <hr className='border-cool-gray-500' />
       <textarea
         id={String(id)}
         name='answer'
         value={answer}
         onChange={onChange}
         maxLength={MAX_ANSWER_LENGTH}
-        placeholder='1000자 이하의 답변을 입력해주세요.'
+        placeholder='답변을 입력하세요.'
         required
-        className='h-[300px]'
+        className='h-full font-normal text-cool-gray-900 placeholder-cool-gray-300 scrollbar-hide focus:outline-none'
       />
-      <div className='flex justify-between'>
-        <Typography>{answer.length} / 1000 (공백 포함)</Typography>
-        <button type='button' onClick={() => onDelete(id)}>
-          삭제
-        </button>
+      <div className='flex justify-end'>
+        <Typography color='gray-500'>{answer.length}/1000</Typography>
       </div>
     </div>
   );

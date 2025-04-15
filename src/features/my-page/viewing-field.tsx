d14a6eb0add482337b7af6b74b7387c0347aa6fb
ImJaeOne <dlawi0108@naver.com>
2025-04-16 01:50:59 +0900

    design:#109 layout frame

--- src/features/my-page/viewing-field.tsx
+++ src/features/my-page/viewing-field.tsx
@@ -1,19 +1,26 @@
 'use client';
 import Modal from '@/components/ui/modal';
-import Typography from '@/components/ui/typography';
 import { useModalStore } from '@/store/use-modal-store';
 import { MODAL_ID } from '@/constants/modal-id-constants';
 import UserMetaDataModal from '@/features/user-meta-data/user-meta-data-modal';
+import MyPageCharacter from '../character/my-page-character';
+import { Session } from 'next-auth';
+
+type Props = {
+  session: Session;
+};
 
 const { USER_META_DATA } = MODAL_ID;
 
-const ViewingField = () => {
+const ViewingField = ({ session }: Props) => {
   const toggleModal = useModalStore((state) => state.toggleModal);
   const isModalOpen = useModalStore((state) => state.getIsModalOpen(USER_META_DATA));
 
   return (
-    <section className='text- w-1/2 border-2'>
-      <Typography as='h3'>삐약이카드</Typography>
+    <section className='h-[80dvh] w-1/2 border-2'>
+      <div className='flex w-full items-center justify-center'>
+        <MyPageCharacter session={session} />
+      </div>
       <button onClick={() => toggleModal(USER_META_DATA)}>모달열자</button>
       {isModalOpen && (
         <Modal modalId={USER_META_DATA}>

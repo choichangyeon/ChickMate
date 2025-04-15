ab2167a6984ae5277d9bf93f4db62770f51401a7
choichangyeon <changyon99@naver.com>
2025-04-15 20:36:55 +0900

    fix:#102 오류구문 삭제

--- src/app/(with-nav)/layout.tsx
+++ src/app/(with-nav)/layout.tsx
@@ -3,10 +3,9 @@ import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query
 import { QUERY_KEY } from '@/constants/query-key';
 import { getCharacterByUserId } from '@/features/character/api/server-services';
 import { Nav } from '@/features/layout/nav';
-import type{ User } from '@/types/user';
+import type { User } from '@/types/user';
 import { authOptions } from '@/utils/auth-option';
 import { serverActionWithSentry } from '@/utils/server-action-with-sentry';
-import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
 import { getUserMetaData } from '@/features/user-meta-data/api/server-services';
 
 const { CHARACTER, META_DATA } = QUERY_KEY;
@@ -30,7 +29,7 @@ const MainLayout = async ({ children }: { children: React.ReactNode }) => {
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
       <Nav user={user} />
-      <div className='scrollbar-hide max-h-[100dvh] flex-1 overflow-scroll'>{children}</div>
+      <div className='max-h-[100dvh] flex-1 overflow-scroll scrollbar-hide'>{children}</div>
     </HydrationBoundary>
   );
 };

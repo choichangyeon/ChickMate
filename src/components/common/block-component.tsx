301bc0c74250192d6b99ac147dcabbfed023aa49
choichangyeon <changyon99@naver.com>
2025-04-15 19:45:24 +0900

    feat:#102 block 컴포넌트 구현

--- src/components/common/block-component.tsx
+++ src/components/common/block-component.tsx
@@ -1,26 +1,34 @@
 import clsx from 'clsx';
 import Typography from '@/components/ui/typography';
+import Button from '@/components/ui/button';
 
 type Props = {
   className: string;
   firstLine: string;
   secondLine: string;
   thirdLine: string;
+  buttonName: string;
+  href: string;
 };
 
 const defaultClassName = 'w-full flex flex-col justify-center items-center gap-4';
 
 const BlockComponent = (props: Props) => {
-  const { className, firstLine, secondLine, thirdLine } = props;
+  const { className, firstLine, secondLine, thirdLine, buttonName, href } = props;
   return (
     <section className={clsx(className, defaultClassName)}>
       <div>
         <Typography as='h3' color='primary-600' weight='bold'>
           {firstLine}
         </Typography>
-        <Typography as='h2'>{secondLine}</Typography>
-        <Typography>{thirdLine}</Typography>
+        <Typography as='h2' size='2xl' weight='bold'>
+          {secondLine}
+        </Typography>
+        <Typography size='sm'>{thirdLine}</Typography>
       </div>
+      <Button link href={href}>
+        <Typography weight='bold'>{buttonName}</Typography>
+      </Button>
     </section>
   );
 };

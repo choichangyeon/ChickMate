7a6b052402af636dd0efaa51b1db8835eedab48a
Minjo Park <consistent.312@gmail.com>
2025-04-16 01:17:44 +0900

    design:#111 자소서 작성 페이지 레이아웃 구현

--- src/app/(with-nav)/(with-header)/resume/[id]/page.tsx
+++ src/app/(with-nav)/(with-header)/resume/[id]/page.tsx
@@ -1,20 +1,39 @@
+import { Metadata } from 'next';
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
+import { QUERY_KEY } from '@/constants/query-key';
+import Typography from '@/components/ui/typography';
+import { serverActionWithSentry } from '@/utils/server-action-with-sentry';
 import ResumeForm from '@/features/resume/resume-form';
 import UserInfoSummary from '@/features/resume/user-info-summary';
+import { getDraftResumeList } from '@/features/resume/api/server-services';
 
-type Params = {
-  params: {
-    id: string;
-  };
+export const metadata: Metadata = {
+  title: '자기소개서 작성',
+  description: 'Chick Mate에서 자기소개서를 관리해보세요.',
 };
 
-const ResumePage = ({ params }: Params) => {
-  const id = params.id;
+const ResumePage = async () => {
+  const { RESUME_DRAFT } = QUERY_KEY;
+
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: [RESUME_DRAFT],
+    queryFn: () => serverActionWithSentry(getDraftResumeList),
+  });
 
   return (
-    <div className='flex gap-2'>
-      <ResumeForm />
-      <UserInfoSummary />
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <main className='flex w-full gap-4 px-[50px] py-8'>
+        <section className='flex w-full flex-col gap-4'>
+          <Typography size='2xl' weight='bold'>
+            <span className='text-primary-orange-600'>자소서</span>를 작성 해 볼까요?
+          </Typography>
+          <ResumeForm />
+        </section>
+        <UserInfoSummary />
+      </main>
+    </HydrationBoundary>
   );
 };
 
--- src/app/(with-nav)/(with-header)/resume/page.tsx
+++ src/app/(with-nav)/(with-header)/resume/page.tsx
@@ -1,11 +1,11 @@
 import { Metadata } from 'next';
 import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
 import { QUERY_KEY } from '@/constants/query-key';
+import Typography from '@/components/ui/typography';
 import { serverActionWithSentry } from '@/utils/server-action-with-sentry';
 import ResumeForm from '@/features/resume/resume-form';
 import UserInfoSummary from '@/features/resume/user-info-summary';
 import { getDraftResumeList } from '@/features/resume/api/server-services';
-import Typography from '@/components/ui/typography';
 
 export const metadata: Metadata = {
   title: '자기소개서 작성',
@@ -24,7 +24,7 @@ const ResumePage = async () => {
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <main className='flex w-full gap-2 px-[50px] py-8'>
+      <main className='flex w-full gap-4 px-[50px] py-8'>
         <section className='flex w-full flex-col gap-4'>
           <Typography size='2xl' weight='bold'>
             <span className='text-primary-orange-600'>자소서</span>를 작성 해 볼까요?

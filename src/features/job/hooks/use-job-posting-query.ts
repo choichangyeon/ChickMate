f350a19d4daae7c09dfb1f926635d84bb5c3c787
choichangyeon <changyon99@naver.com>
2025-04-15 16:07:45 +0900

    feat:#75 undefined 처리

--- src/features/job/hooks/use-job-posting-query.ts
+++ src/features/job/hooks/use-job-posting-query.ts
@@ -3,23 +3,19 @@ import { useQuery } from '@tanstack/react-query';
 import { getJobByUserMetaData } from '@/features/job/api/client-services';
 import { STALE_TIME } from '@/constants/time-constants';
 import { UserMetaDataType } from '@/types/user-meta-data-type';
-import { getUserMetaData } from '@/features/user-meta-data/api/client-services';
 
 const { JOB_POSTING } = QUERY_KEY;
 const { AN_HOUR } = STALE_TIME;
 
 type Props = {
-  userId: string;
+  userMetaData?: UserMetaDataType;
 };
 
-export const useJobPostingQuery = ({ userId }: Props) => {
+export const useJobPostingQuery = ({ userMetaData }: Props) => {
   return useQuery({
     queryKey: [JOB_POSTING],
-    queryFn: async () => {
-      const userMetaData: UserMetaDataType = await getUserMetaData(userId);
-      return getJobByUserMetaData(userMetaData);
-    },
+    queryFn: async () => getJobByUserMetaData(userMetaData!),
     staleTime: AN_HOUR,
-    enabled: !!userId,
+    enabled: !!userMetaData,
   });
 };

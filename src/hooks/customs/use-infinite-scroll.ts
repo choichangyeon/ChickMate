2c719aeba79a2b16f79d70372001e77e73a86b74
ImJaeOne <dlawi0108@naver.com>
2025-04-15 11:43:36 +0900

    feat:#95 무한 스크롤 관련 ref hook 분리

---
+++ src/hooks/customs/use-infinite-scroll.ts
@@ -0,0 +1,26 @@
+import { useEffect, useRef } from 'react';
+
+type Props = {
+  fetchNextPage: () => void;
+  hasNextPage: boolean;
+};
+
+export const useInfiniteScroll = ({ fetchNextPage, hasNextPage }: Props) => {
+  const targetRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    if (!targetRef.current || !hasNextPage) return;
+
+    const observer = new IntersectionObserver(([entry]) => {
+      if (entry.isIntersecting) {
+        fetchNextPage();
+      }
+    });
+
+    observer.observe(targetRef.current);
+
+    return () => observer.disconnect();
+  }, [hasNextPage, fetchNextPage]);
+
+  return targetRef;
+};

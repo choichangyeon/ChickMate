034a011897e5751de80330004b42050b5f52b3f7
Minjo Park <consistent.312@gmail.com>
2025-04-16 11:58:18 +0900

    design:#112 카메라 뷰 크기 수정

--- src/features/interview/camera-view.tsx
+++ src/features/interview/camera-view.tsx
@@ -7,7 +7,7 @@ const CameraView = () => {
 
   return (
     <div className='flex-1 rounded-lg border border-cool-gray-200 bg-white'>
-      <video ref={videoRef} autoPlay />
+      <video ref={videoRef} autoPlay className='h-full w-full rounded-lg object-cover' />
     </div>
   );
 };

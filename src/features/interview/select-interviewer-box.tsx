2813caf5b673de8ccf2afb9d5fcb5ce57382ed64
choichangyeon <changyon99@naver.com>
2025-04-16 11:14:39 +0900

    refactor:#102 면접관 선택 수정

--- src/features/interview/select-interviewer-box.tsx
+++ src/features/interview/select-interviewer-box.tsx
@@ -13,6 +13,8 @@ import { PATH } from '@/constants/path-constant';
 const { CALM, PRESSURE } = INTERVIEW_TYPE;
 const { LIVE } = PATH.INTERVIEW;
 
+const activeBgClass = 'bg-primary-orange-600';
+
 const SelectInterviewerBox = () => {
   const [interviewType, setInterviewType] = useState<string>(CALM);
   const router = useRouter();
@@ -31,13 +33,13 @@ const SelectInterviewerBox = () => {
       <aside className='mr-5 flex w-full items-start justify-start gap-5 self-stretch'>
         <div
           onClick={() => setInterviewType(CALM)}
-          className='flex w-full items-center justify-center self-stretch rounded-lg bg-primary-orange-600 outline outline-1 outline-cool-gray-300'
+          className={`flex w-full cursor-pointer items-center justify-center self-stretch rounded-lg outline outline-1 outline-cool-gray-300 ${interviewType === CALM ? activeBgClass : ''}`}
         >
           <Image src={`/assets/character/card/yellow-level2.png`} alt='침착한 면접관' width={220} height={220} />
         </div>
         <div
           onClick={() => setInterviewType(PRESSURE)}
-          className='flex w-full items-center justify-center self-stretch rounded-lg bg-primary-orange-600 outline outline-1 outline-cool-gray-300'
+          className={`flex w-full cursor-pointer items-center justify-center self-stretch rounded-lg outline outline-1 outline-cool-gray-300 ${interviewType === PRESSURE ? activeBgClass : ''}`}
         >
           <Image src={`/assets/character/card/yellow-level3.png`} alt='불타는 면접관' width={220} height={220} />
         </div>
@@ -71,7 +73,6 @@ const SelectInterviewerBox = () => {
             </>
           )}
         </div>
-        {/* TODO: 버튼 onClick 속성 추가시 변경 */}
         {resumeId && (
           <Button color='dark' variant='outline' onClick={handleClickSetInterviewType}>
             <Typography weight='bold'>면접 시작하기</Typography>

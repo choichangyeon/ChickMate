0b7936832f888e78dc5e045ee4fa58d26e674930
ImJaeOne <dlawi0108@naver.com>
2025-04-17 01:09:30 +0900

    design:#135 nav css

--- src/features/layout/data/nav-items.tsx
+++ src/features/layout/data/nav-items.tsx
@@ -3,15 +3,19 @@ import JobIcon from '@/components/icons/job-icon';
 import MyPageIcon from '@/components/icons/my-page-icon';
 import ResumeIcon from '@/components/icons/resume-icon';
 import SignInIcon from '@/components/icons/sign-in-icon';
-import SignUpIcon from '@/components/icons/sign-up-icon';
 import LogoutIcon from '@/components/icons/logout-icon';
 import { PATH } from '@/constants/path-constant';
+import InterviewFullIcon from '@/components/icons/interview-full-icon';
+import JobFullIcon from '@/components/icons/job-full-icon';
+import MyPageFullIcon from '@/components/icons/my-page-full-icon';
+import ResumeFullIcon from '@/components/icons/resume-full-icon';
 
 export type NavItems = {
   path: string;
   name: string;
   type: 'link' | 'button';
   icon?: React.ReactNode;
+  fullIcon?: React.ReactNode;
   class?: string;
 };
 
@@ -19,17 +23,11 @@ const { ON_BOARDING, AUTH, MY_PAGE, RESUME, INTERVIEW, JOB } = PATH;
 
 export const Private_Nav_Items: NavItems[] = [
   {
-    path: JOB,
-    type: 'link',
-    name: '채용공고',
-    icon: <JobIcon />,
-    class: 'mb-2',
-  },
-  {
     path: RESUME.ROOT,
     type: 'link',
     name: '자소서',
     icon: <ResumeIcon />,
+    fullIcon: <ResumeFullIcon />,
     class: 'mb-2',
   },
   {
@@ -37,13 +35,24 @@ export const Private_Nav_Items: NavItems[] = [
     type: 'link',
     name: 'AI 면접',
     icon: <InterviewIcon />,
+    fullIcon: <InterviewFullIcon />,
     class: 'mb-2',
   },
+  {
+    path: JOB,
+    type: 'link',
+    name: '맞춤 채용 공고',
+    icon: <JobIcon />,
+    fullIcon: <JobFullIcon />,
+    class: 'mb-2',
+  },
+
   {
     path: MY_PAGE,
     type: 'link',
     name: '마이페이지',
     icon: <MyPageIcon />,
+    fullIcon: <MyPageFullIcon />,
   },
   {
     path: ON_BOARDING,
@@ -62,10 +71,10 @@ export const Public_Nav_Items: NavItems[] = [
     icon: <SignInIcon />,
     class: 'mb-2',
   },
-  {
-    path: AUTH.SIGN_UP,
-    name: '회원가입',
-    type: 'link',
-    icon: <SignUpIcon />,
-  },
+  // {
+  //   path: AUTH.SIGN_UP,
+  //   name: '회원가입',
+  //   type: 'link',
+  //   icon: <SignUpIcon />,
+  // },
 ];

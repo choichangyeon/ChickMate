47e20c3cd75978c0973682c7b3d0adddcdca4878
Minjo Park <consistent.312@gmail.com>
2025-04-16 12:36:17 +0900

    refactor:#112 질문 순서 보여주는 컴포넌트 분리

--- src/app/(with-nav)/(with-header)/interview/live/[id]/page.tsx
+++ src/app/(with-nav)/(with-header)/interview/live/[id]/page.tsx
@@ -5,6 +5,7 @@ import CameraView from '@/features/interview/camera-view';
 import { getInterviewHistory } from '@/features/interview/api/server-services';
 import QuestionDisplayWithTimer from '@/features/interview/question-display-with-timer';
 import type { RouteParams } from '@/types/route-params';
+import QuestionStep from '@/features/interview/question-step';
 
 const InterviewPage = async ({ params }: RouteParams) => {
   const session = await getServerSession(authOptions);
@@ -20,13 +21,11 @@ const InterviewPage = async ({ params }: RouteParams) => {
   return (
     <main className='flex flex-col gap-8 px-[50px] py-8'>
       <section className='flex w-full flex-col gap-4'>
-        <div className='flex'>
+        <div className='flex items-center justify-between'>
           <Typography size='2xl' weight='bold'>
             집중하세요! <span className='text-primary-orange-600'>면접이 시작됐습니다</span>
           </Typography>
-          <span className='flex h-[24px] w-[40px] items-center justify-center rounded-full border-2 border-primary-orange-700 bg-primary-orange-600 px-[10px] text-white'>
-            Q1
-          </span>
+          <QuestionStep />
         </div>
         <div className='flex h-[335px] gap-5'>
           <div className='flex-1 rounded-lg border border-cool-gray-200 bg-white'>면접관</div>
---
+++ src/features/interview/question-step.tsx
@@ -0,0 +1,9 @@
+const QuestionStep = () => {
+  return (
+    <span className='flex h-[24px] w-[40px] items-center justify-center rounded-full border border-primary-orange-700 bg-primary-orange-600 px-[10px] text-white'>
+      Q1
+    </span>
+  );
+};
+
+export default QuestionStep;

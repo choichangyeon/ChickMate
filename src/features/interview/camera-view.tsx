5fcbd9a34b85f7c0750e284f1486196c4fb025f2
Minjo Park <consistent.312@gmail.com>
2025-04-18 00:29:49 +0900

    refactor:#121 버튼명 수정

--- src/features/interview/camera-view.tsx
+++ src/features/interview/camera-view.tsx
@@ -12,7 +12,7 @@ const CameraView = () => {
   return (
     <div className='flex-1 rounded-lg border border-cool-gray-200 bg-white'>
       <video ref={videoRef} autoPlay playsInline className='h-full w-full rounded-lg object-cover' />
-      <button onClick={handleToggleButtonClick}>클릭해보세요</button>
+      <button onClick={handleToggleButtonClick}>카메라 온오프</button>
     </div>
   );
 };

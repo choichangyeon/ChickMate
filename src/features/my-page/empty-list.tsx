fb907e6e824d0a526a343d12290b7e01bc6b6ae1
choichangyeon <changyon99@naver.com>
2025-04-17 20:07:52 +0900

    feat:#146 empty list 컴포넌트 생성

---
+++ src/features/my-page/empty-list.tsx
@@ -0,0 +1,31 @@
+import { TABS } from '@/constants/my-page-constants';
+
+const { BOOKMARK, RESUME, HISTORY } = TABS;
+const EMPTY_MESSAGE: Record<string, React.ReactNode> = {
+  [HISTORY]: (
+    <>
+      면접 결과가 없습니다.
+      <br />
+      면접을 먼저 진행해 주세요.
+    </>
+  ),
+  [BOOKMARK]: (
+    <>
+      북마크 된 채용 공고가 없습니다.
+      <br />
+      채용공고 페이지에서 원하는 내용을 찾아보세요.
+    </>
+  ),
+  // @TODO: 이 부분 추후에 수정 부탁드립니다!
+  [RESUME]: <>아직 작성된 이력서가 없습니다.</>,
+};
+
+type Props = {
+  tab: (typeof TABS)[keyof typeof TABS];
+};
+
+const EmptyList = ({ tab }: Props) => {
+  return <div className='text-center text-cool-gray-500'>{EMPTY_MESSAGE[tab]}</div>;
+};
+
+export default EmptyList;

e5c929dc524ce1a3b867a0493e8667132b509769
ImJaeOne <dlawi0108@naver.com>
2025-04-17 21:28:03 +0900

    design:#140 sign-in css

--- src/features/sign/sign-in-auth-form.tsx
+++ src/features/sign/sign-in-auth-form.tsx
@@ -9,6 +9,7 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { schema, SignInFormData } from '@/features/sign/data/sign-in-schema';
 import Image from 'next/image';
 import { useSignInResult } from './hooks/use-sign-in-result';
+import Typography from '@/components/ui/typography';
 
 const { ON_BOARDING } = PATH;
 
@@ -33,12 +34,20 @@ const SignInAuthForm = () => {
   };
 
   return (
-    <div className='mx-auto w-full max-w-md rounded-lg bg-white p-6 shadow-md'>
-      <p className='mb-1 text-center text-2xl font-light'>
-        만나서 반가워요.<span className='font-normal'>병아리</span>씨!
-      </p>
-      <p className='mb-4 text-center font-extralight'>우리 같이 취업을 향한 여정을 떠나볼까요?</p>
-      <p className='mb-10 text-center font-extralight text-black/30'>원할한 서비스 이용을 위해 로그인 해주세요.</p>
+    <div className='mx-auto w-full max-w-md rounded-3xl bg-white p-6 shadow-md'>
+      <div className='mb-8'>
+        <Typography size='2xl' weight='normal' align='center'>
+          만나서 반가워요.<span className='font-bold'>병아리</span>씨!
+        </Typography>
+        <Typography weight='normal' align='center'>
+          우리 같이 취업을 향한 여정을 떠나볼까요?
+        </Typography>
+      </div>
+      <div className='mb-8'>
+        <Typography color='primary-600' weight='bold' align='center'>
+          원할한 서비스 이용을 위해 로그인 해주세요.
+        </Typography>
+      </div>
       <form onSubmit={handleSubmit(onSubmit)} noValidate>
         <AuthInput label='EMAIL' id='email' register={register} error={errors.email} type='email' />
         <AuthInput label='PASSWORD' id='password' register={register} error={errors.password} type='password' />
@@ -72,7 +81,7 @@ const SignInAuthForm = () => {
           <div className='flex-1 border-t'></div>
         </div>
         <Link href={PATH.AUTH.SIGN_UP} className='font-extralight'>
-          30초만에 회원가입하기
+          <Typography align='center'> 30초만에 회원가입하기</Typography>
         </Link>
       </div>
     </div>

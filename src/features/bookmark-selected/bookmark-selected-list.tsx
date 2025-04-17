7de79e4b233dc868e8b8e7684d86e641d0e1e87f
choichangyeon <changyon99@naver.com>
2025-04-18 02:52:44 +0900

    refactor:#146 상단 훅 처리

--- src/features/bookmark-selected/bookmark-selected-list.tsx
+++ src/features/bookmark-selected/bookmark-selected-list.tsx
@@ -3,33 +3,61 @@ import { TABS } from '@/constants/my-page-constants';
 import EmptyList from '../my-page/empty-list';
 import { useSession } from 'next-auth/react';
 import { User } from '@prisma/client';
-import { useRouter } from 'next/navigation';
 import { useInfiniteScroll } from '@/hooks/customs/use-infinite-scroll';
 import ErrorComponent from '@/components/common/error-component';
+import LoadingSpinner from '@/components/ui/loading-spinner';
 import { useBookmarkSelectedInfiniteQuery } from './hook/use-bookmark-selected-infinite-query';
+import BookmarkTab from '@/features/bookmark-selected/bookmark-tab';
 
 const { BOOKMARK } = TABS;
 
 const BookmarkSelectedList = () => {
-  const { data: session } = useSession();
-  const userId: User['id'] | undefined = session?.user?.id;
+  const { data: session, status } = useSession();
+  const userId = session?.user?.id;
 
   const { data, isPending, isError, fetchNextPage, hasNextPage, isFetchingNextPage } = useBookmarkSelectedInfiniteQuery(
     userId!
   );
 
-  const targetRef = useInfiniteScroll({
-    fetchNextPage,
-    hasNextPage,
-  });
+  const targetRef = useInfiniteScroll({ fetchNextPage, hasNextPage });
 
-  if (isPending) return <div className='text-center'>로딩 중..</div>;
+  if (status === 'loading') {
+    return (
+      <section className='flex h-[70dvh] flex-col items-center justify-center'>
+        <LoadingSpinner />
+      </section>
+    );
+  }
+
+  if (!userId) return <ErrorComponent />;
+
+  if (isPending) {
+    return (
+      <section className='flex h-[70dvh] flex-col items-center justify-center'>
+        <LoadingSpinner />
+      </section>
+    );
+  }
+
   if (isError) return <ErrorComponent />;
 
   const bookmarkList = data.pages.flatMap((page) => page.bookmarkList);
-
   if (bookmarkList.length === 0) return <EmptyList tab={BOOKMARK} />;
-  return <EmptyList tab={BOOKMARK} />;
+
+  return (
+    <section className='flex h-[70dvh] flex-col'>
+      <div className='flex-1 overflow-hidden'>
+        <ul className='h-full overflow-y-auto scrollbar-hide'>
+          {bookmarkList.map((bookmark, idx, arr) => (
+            <BookmarkTab key={bookmark.id} bookmark={bookmark} index={idx} length={arr.length} userId={userId} />
+          ))}
+          <div ref={targetRef} className='flex h-10 w-full items-center justify-center text-sm text-gray-400'>
+            {isFetchingNextPage && <LoadingSpinner size='sm' />}
+          </div>
+        </ul>
+      </div>
+    </section>
+  );
 };
 
 export default BookmarkSelectedList;

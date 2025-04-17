e1c3ed1ed9049c5a684cfc4a619d93e2db0a210d
choichangyeon <changyon99@naver.com>
2025-04-18 02:50:09 +0900

    feat:#146 북마크 mutation infinite query 트리거

--- src/features/job/hooks/use-bookmark-mutation.ts
+++ src/features/job/hooks/use-bookmark-mutation.ts
@@ -6,19 +6,22 @@ const { BOOKMARK } = QUERY_KEY;
 
 type Props = {
   jobPostingId: number;
+  userId?: string;
 };
 
-export const useBookmarkMutation = ({ jobPostingId }: Props) => {
-  const QueryClient = useQueryClient();
+export const useBookmarkMutation = ({ jobPostingId, userId }: Props) => {
+  const queryClient = useQueryClient();
   return useMutation({
     mutationKey: [BOOKMARK, jobPostingId],
     mutationFn: async (isBookmarked: boolean) => {
       return await postBookmarkWithJobPostingId({ jobPostingId, isBookmarked });
     },
     onSuccess: () => {
-      QueryClient.invalidateQueries({
+      queryClient.invalidateQueries({
         queryKey: [BOOKMARK, jobPostingId],
       });
+
+      if (userId) queryClient.invalidateQueries({ queryKey: [BOOKMARK, userId] });
     },
     onError: (error) => {
       throw error;

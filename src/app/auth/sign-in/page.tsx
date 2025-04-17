b8cc3ec24cd5aa2db717db12ae9109fa50c1a97f
ImJaeOne <dlawi0108@naver.com>
2025-04-17 20:11:46 +0900

    refactor:#140 동적 import 삭제

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -1,14 +1,10 @@
+import SignInAuthForm from '@/features/sign/sign-in-auth-form';
 import { Metadata } from 'next';
-import dynamic from 'next/dynamic';
 
 export const metadata: Metadata = {
   title: '로그인 - ChickMate',
-  description: 'ChickMate에 가입하고 취업을 위한 여정을 떠나보아요~.',
+  description: 'ChickMate에 가입하고 취업을 위한 여정을 떠나보아요.',
 };
-
-const SignInAuthForm = dynamic(() => import('@/features/sign/sign-in-auth-form'), {
-  ssr: false,
-});
 
 const SignInPage = () => {
   return <SignInAuthForm />;

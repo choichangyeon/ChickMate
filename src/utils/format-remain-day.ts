5f43be451d079d92ef524f0f9e1803a01a174626
choichangyeon <changyon99@naver.com>
2025-04-17 15:41:20 +0900

    rename:#132 상수화 처리

--- src/utils/format-remain-day.ts
+++ src/utils/format-remain-day.ts
@@ -1,8 +1,8 @@
 export const formatRemainDay = (date: Date): number | null => {
   try {
     const currentDate = new Date();
-    const milliSecondsInOneDay = 1000 * 60 * 60 * 24;
-    const daysRemaining = Math.ceil((date.getTime() - currentDate.getTime()) / milliSecondsInOneDay);
+    const MILLISECONDS_IN_ONE_DAY = 1000 * 60 * 60 * 24;
+    const daysRemaining = Math.ceil((date.getTime() - currentDate.getTime()) / MILLISECONDS_IN_ONE_DAY);
     return daysRemaining;
   } catch (error) {
     return null;

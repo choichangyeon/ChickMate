d5af449220ec8dfb24d99d99732e1fc4e3cd5b6f
ImJaeOne <dlawi0108@naver.com>
2025-04-18 14:00:34 +0900

    refactor:#152 속성 camelCase로 변경

--- src/components/icons/right-arrow-icon.tsx
+++ src/components/icons/right-arrow-icon.tsx
@@ -1,7 +1,7 @@
 const RightArrowIcon = () => {
   return (
     <svg width='30' height='30' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
-      <path d='M9 6L15 12L9 18' stroke='#33363F' stroke-width='2' />
+      <path d='M9 6L15 12L9 18' stroke='#33363F' strokeWidth='2' />
     </svg>
   );
 };

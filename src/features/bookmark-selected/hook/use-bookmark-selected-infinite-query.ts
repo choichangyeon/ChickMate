02757e21349a4bf848aebc0a6c2e50f47377871b
choichangyeon <changyon99@naver.com>
2025-04-18 09:18:28 +0900

    refactor:#146 에러 메세지 상수화

--- src/features/bookmark-selected/hook/use-bookmark-selected-infinite-query.ts
+++ src/features/bookmark-selected/hook/use-bookmark-selected-infinite-query.ts
@@ -3,6 +3,8 @@ import { QUERY_KEY } from '@/constants/query-key';
 import { useInfiniteQuery } from '@tanstack/react-query';
 import { getSelectedBookmark } from '@/features/bookmark-selected/api/client-services';
 import { REFETCH_TIME } from '@/constants/time-constants';
+import { AUTH_MESSAGE } from '@/constants/message-constants';
+const { SESSION_NO_USER } = AUTH_MESSAGE.ERROR;
 const { BOOKMARK } = QUERY_KEY;
 const ITEMS_PER_PAGE = 8;
 const { MIN } = REFETCH_TIME;
@@ -11,7 +13,7 @@ export const useBookmarkSelectedInfiniteQuery = (userId: User['id']) => {
   return useInfiniteQuery({
     queryKey: [BOOKMARK, userId],
     queryFn: async ({ pageParam = 1 }) => {
-      if (!userId) throw new Error('사용자 ID가 필요합니다');
+      if (!userId) throw new Error(SESSION_NO_USER);
       return await getSelectedBookmark({
         pageParam,
         limit: ITEMS_PER_PAGE,

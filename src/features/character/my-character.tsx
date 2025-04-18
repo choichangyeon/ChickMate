00d13f56ddafb25986d0862219b4d987a1d5efa8
ImJaeOne <dlawi0108@naver.com>
2025-04-18 13:33:41 +0900

    design:#152 캐릭터 detail card css

--- src/features/character/my-character.tsx
+++ src/features/character/my-character.tsx
@@ -22,52 +22,37 @@ const MyCharacter = ({ session, characterData = defaultCharacter, overlayText, r
     requiredModal,
     session,
   });
-  // 글씨 크기 한 사이즈씩 줄인 상태
   return (
     <>
-      <div className='relative flex items-center gap-4 px-8'>
-        <div className='flex items-center gap-4'>
-          <div>
-            <div className='flex flex-col items-center justify-center'>
-              {/* 추후 크기 변경 필요 */}
-              <div className='py-4'>
-                <Image
-                  src={`/assets/character/card/${type}-level${level}.png`}
-                  width={200}
-                  height={200}
-                  alt={characterName}
-                  priority
-                />
-              </div>
-              <div className='flex items-center gap-8'>
-                <div className='flex flex-col'>
-                  <div className='flex items-center justify-between gap-6'>
-                    <div className='flex items-center justify-center gap-3'>
-                      <Typography weight='black' size='xl' color='primary-600'>
-                        LV {level}
-                      </Typography>
-                      <div className='w-[72px] line-clamp-1'>
-                        <Typography weight='bold' size='xl'>
-                          {session && session.user.name}님
-                        </Typography>
-                      </div>
-                    </div>
-                    <div className='text-secondary-yellow'>
-                      <Typography weight='bold' color='secondary-yellow' size='sm'>
-                        {remainingExp} 경험치만 더 쌓으면
-                      </Typography>
-                      <Typography weight='bold' color='secondary-yellow' size='sm' align='right'>
-                        레벨이 올라요!
-                      </Typography>
-                    </div>
-                  </div>
-                  <CharacterExpBar type='main' percent={percent} />
-                </div>
-                {/* 추후 tag 공통 컴포넌트 */}
-                <div className='rounded-3xl border border-black px-10 py-1 text-center font-normal text-cool-gray-900'>
-                  {characterName}
+      <div className='flex w-full flex-col items-center justify-center gap-4'>
+        <Image
+          src={`/assets/character/card/${type}-level${level}.png`}
+          width={200}
+          height={200}
+          alt={characterName}
+          priority
+        />
+        <div className='flex w-full items-center'>
+          <div className='flex-1 border-t'></div>
+        </div>
+        <div className='flex w-full items-center gap-8'>
+          <div className='flex w-full flex-col'>
+            <div className='flex items-center w-full justify-between gap-6'>
+              <div className='flex items-center justify-center gap-3'>
+                <Typography weight='black' size='xl' color='primary-600'>
+                  LV {level}
+                </Typography>
+                <div className='line-clamp-1 w-[72px]'>
+                  <Typography weight='bold'>{session && session.user.name}님</Typography>
                 </div>
               </div>
+              <Typography weight='normal'>{characterName}</Typography>
+            </div>
+            <CharacterExpBar type='main' percent={percent} />
+            <div className='text-secondary-yellow'>
+              <Typography weight='normal' color='gray-500' size='xs' align='center'>
+                앞으로 {remainingExp} 경험치만 더 쌓으면 레벨이 올라요!
+              </Typography>
             </div>
           </div>
         </div>

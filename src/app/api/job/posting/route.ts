08073b4c59fe5b3e099701d900e541bcb045beb7
choichangyeon <changyon99@naver.com>
2025-04-19 01:16:26 +0900

    refactor:#162 필터링 기능 리팩토링

--- src/app/api/job/posting/route.ts
+++ src/app/api/job/posting/route.ts
@@ -1,13 +1,9 @@
 import { DB_MESSAGE } from '@/constants/message-constants';
-import { USER_META_DATA_KEY } from '@/constants/user-meta-data-constants';
 import { prisma } from '@/lib/prisma';
-import { JobPosting } from '@/types/job-posting';
 import { sanitizeQueryParams } from '@/utils/sanitize-query-params';
 import { NextRequest, NextResponse } from 'next/server';
+import { JobPosting } from '@prisma/client';
 
-/**
- * GET
- */
 // TODO: 필터링 기준 상수화
 const educationOrder = {
   '학력무관': 0,
@@ -18,37 +14,35 @@ const educationOrder = {
   '박사졸업': 5,
 };
 
+/**
+ * GET
+ */
 export const GET = async (request: NextRequest): Promise<NextResponse> => {
   const { DB_REQUEST_ERROR, DB_SERVER_ERROR, DB_URL_ERROR } = DB_MESSAGE.ERROR;
-  const { MAIN_REGION } = USER_META_DATA_KEY;
   try {
     // searchParams로 정보 가져오기
-    // TODO: mainRegion -> location으로 바꾸기
     const searchParams = request.nextUrl.searchParams;
-    const { educationLevel, mainRegion, experienceType, jobType } = sanitizeQueryParams(searchParams);
+    const { requiredEducationName, locationName, experienceName, jobMidCodeName } = sanitizeQueryParams(searchParams);
 
     if (!searchParams) {
       return NextResponse.json({ message: DB_URL_ERROR }, { status: 400 });
     }
 
-    if (!educationLevel || !mainRegion || !experienceType || !jobType) {
+    if (!requiredEducationName || !locationName || !experienceName || !jobMidCodeName) {
       return NextResponse.json({ message: DB_REQUEST_ERROR }, { status: 400 });
     }
 
     // const mainRegion = JSON.parse(location).mainRegion;
-    const userLevelNum = educationOrder[educationLevel as keyof typeof educationOrder];
+    const userLevelNum = educationOrder[requiredEducationName as keyof typeof educationOrder];
     const response: JobPosting[] = await prisma.jobPosting.findMany({
       where: {
-        educationLevel: {
-          in: Object.entries(educationOrder)
-            .filter(([_, levelNum]) => levelNum <= userLevelNum)
-            .map(([key]) => key),
+        requiredEducationCode: {
+          lte: userLevelNum,
         },
-        experienceType,
-        jobType,
-        location: {
-          path: [MAIN_REGION],
-          equals: mainRegion,
+        experienceName,
+        jobMidCodeName,
+        locationName: {
+          contains: locationName,
         },
       },
     });

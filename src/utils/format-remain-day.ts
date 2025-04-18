7cf8141431f321942eb70664c07754e1cc73d124
choichangyeon <changyon99@naver.com>
2025-04-18 09:03:39 +0900

    refactor:#146 타입 명확성

--- src/utils/format-remain-day.ts
+++ src/utils/format-remain-day.ts
@@ -7,14 +7,9 @@ export const formatRemainDay = (date: Date | string): number | null => {
   const MILLISECONDS_IN_ONE_DAY = 1000 * 60 * 60 * 24;
   try {
     const currentDate = new Date();
-    if (typeof date !== 'object') {
-      const formatDate = new Date(date);
-      const daysRemaining = Math.ceil((formatDate.getTime() - currentDate.getTime()) / MILLISECONDS_IN_ONE_DAY);
-      return daysRemaining;
-    } else {
-      const daysRemaining = Math.ceil((date.getTime() - currentDate.getTime()) / MILLISECONDS_IN_ONE_DAY);
-      return daysRemaining;
-    }
+    const targetDate = typeof date === 'string' ? new Date(date) : date;
+    const daysRemaining = Math.ceil((targetDate.getTime() - currentDate.getTime()) / MILLISECONDS_IN_ONE_DAY);
+    return daysRemaining;
   } catch (error) {
     return null;
   }

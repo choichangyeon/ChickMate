ebc1d0fc00575b2c1d444e95194500e17bfa2a02
ImJaeOne <dlawi0108@naver.com>
2025-04-18 10:52:37 +0900

    refactor:#152 ai 면접 경로 헤더에 추가

--- src/features/layout/utils/getTitleFromPath.ts
+++ src/features/layout/utils/getTitleFromPath.ts
@@ -2,21 +2,19 @@ import { PATH } from '@/constants/path-constant';
 
 type Props = string;
 
-const {
-  MY_PAGE,
-  RESUME: { ROOT },
-  JOB,
-} = PATH;
+const { MY_PAGE, RESUME, JOB, INTERVIEW } = PATH;
 
 export const getTitleFromPath = (pathname: Props) => {
   if (!pathname) return 'ChickMate';
 
   if (pathname.startsWith(MY_PAGE)) {
     return '마이페이지';
-  } else if (pathname.startsWith(ROOT)) {
+  } else if (pathname.startsWith(RESUME.ROOT)) {
     return '자소서 작성하기';
   } else if (pathname.startsWith(JOB)) {
     return '맞춤형 채용공고';
+  } else if (pathname.startsWith(INTERVIEW.ROOT)) {
+    return 'AI 면접';
   }
 
   return 'ChickMate';

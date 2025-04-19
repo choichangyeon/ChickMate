abd8682f5f203037241a5e02bd16f1722c9db65b
Minjo Park <consistent.312@gmail.com>
2025-04-19 14:55:20 +0900

    refactor:#159 불필요한 코드 제거

--- src/features/interview/hooks/use-audio-with-timer.ts
+++ src/features/interview/hooks/use-audio-with-timer.ts
@@ -1,48 +1,33 @@
 import { useEffect, useRef, useState } from 'react';
 import { useInterviewStore } from '@/store/use-interview-store';
-import { FEEDBACK_PROMPT, INTERVIEW_PROMPT, USER_PROMPT } from '@/constants/interview-constants';
 import { useAudioRecorder } from '@/features/interview/hooks/use-audio-recorder';
 import { useTimer } from '@/features/interview/hooks/use-timer';
 import { usePreventPageUnload } from '@/features/resume/hooks/use-prevent-page-load';
-import {
-  getOpenAIResponse,
-  patchInterviewHistory,
-  postSpeechToText,
-  postTextToSpeech,
-} from '@/features/interview/api/client-services';
-import type { Message } from '@/types/message';
-import type { InterviewHistoryWithResume } from '@/types/interview';
+import { handleVoiceToAIFlow } from '../utils/handle-voice-to-ai-flow';
+import type { InterviewHistory } from '@prisma/client';
 
-const { CALM_PROMPT, PRESSURE_PROMPT } = INTERVIEW_PROMPT;
 const INTERVIEW_LIMIT_COUNT = 7;
 
-export const useAudioWithTimer = (duration: number, interviewHistory: InterviewHistoryWithResume) => {
-  const { interviewType, resume, id } = interviewHistory;
-  const type = interviewType === 'calm' ? CALM_PROMPT : PRESSURE_PROMPT;
-  const init_state: Message[] = [
-    {
-      role: 'system',
-      content: [...type.content, { type: 'text', text: `지원자의 자기소개서: ${resume}` }],
-    },
-  ];
+type Props = {
+  duration: number;
+  interviewHistory: InterviewHistory;
+};
+
+export const useAudioWithTimer = ({ duration, interviewHistory }: Props) => {
+  const { interviewType, id: interviewId } = interviewHistory;
+
   const audioRef = useRef<HTMLAudioElement | null>(null);
 
   /** state */
   const incrementQuestionIndex = useInterviewStore((state) => state.incrementQuestionIndex);
   const [isDirty, setIsDirty] = useState<boolean>(false);
-  const [messageList, setMessageList] = useState<Message[]>(init_state);
-  const [interviewQnA, setInterviewQnA] = useState({
-    question: '간단한 자기소개 부탁드립니다',
-    answer: '',
-  });
   const [isAIVoicePlaying, setIsAIVoicePlaying] = useState(false);
+  const [aiQuestion, setAiQuestion] = useState<string | null>(null);
 
   /** hook */
   const { isRecording, startRecording, stopRecording } = useAudioRecorder();
 
-  /** function */
   const handleTimerComplete = async () => {
-    stopRecording();
     await stopRecordingWithTimer();
   };
 
@@ -50,6 +35,8 @@ export const useAudioWithTimer = (duration: number, interviewHistory: InterviewH
     duration,
     onTimerComplete: handleTimerComplete,
   });
+
+  /** function */
 
   // 녹음 시작
   const startRecordingWithTimer = () => {
@@ -61,58 +48,15 @@ export const useAudioWithTimer = (duration: number, interviewHistory: InterviewH
   // 녹음 중단 (사용자 음성 -> 텍스트 변환 + DB에 저장)
   const stopRecordingWithTimer = async () => {
     stopTimer();
+    const blob = await stopRecording();
     setIsAIVoicePlaying(true);
     setIsDirty(true);
 
-    const blob = await stopRecording();
+    const { audio, aiQuestion } = await handleVoiceToAIFlow({ blob, interviewType, interviewId });
+    setAiQuestion(aiQuestion);
+    audioRef.current = audio;
 
-    // 사용자 음성을 텍스트로 변환해주는 로직
-    try {
-      const answerText = await postSpeechToText({ blob });
-      await getOpenAIInterviewContent(answerText);
-
-      const content = { question: interviewQnA.question, answer: answerText };
-      await patchInterviewHistory({ interviewId: id, content });
-
-      setInterviewQnA((prev) => ({ ...prev, answer: answerText }));
-      incrementQuestionIndex();
-    } catch (error) {
-      setIsAIVoicePlaying(false);
-      if (error instanceof Error) {
-        alert(error.message);
-      }
-    }
-  };
-
-  // AI 면접관에게 답변 보내고 질문 받아오는 로직
-  const getOpenAIInterviewContent = async (answerText: string) => {
-    try {
-      const updatedMessageList: Message[] =
-        messageList.length < INTERVIEW_LIMIT_COUNT ? [...messageList, USER_PROMPT(answerText)] : [FEEDBACK_PROMPT];
-
-      const { messageList: newMessageList, question } = await getOpenAIResponse({ messageList: updatedMessageList });
-      setMessageList(newMessageList);
-      setInterviewQnA((prev) => ({ ...prev, question }));
-
-      if (messageList.length === INTERVIEW_LIMIT_COUNT) {
-        await patchInterviewHistory({ interviewId: id, feedback: question });
-      } else {
-        // AI 면접관 텍스트를 audio url로 반환하는 로직
-        const audioUrl = await postTextToSpeech({
-          text: question,
-          type: interviewType,
-        });
-
-        audioRef.current = new Audio(audioUrl);
-        await audioRef.current.play();
-
-        audioRef.current.addEventListener('ended', () => setIsAIVoicePlaying(false));
-      }
-    } catch (error) {
-      if (error instanceof Error) {
-        alert(error);
-      }
-    }
+    audio.play();
   };
 
   usePreventPageUnload(isDirty);
@@ -129,11 +73,11 @@ export const useAudioWithTimer = (duration: number, interviewHistory: InterviewH
   }, []);
 
   return {
-    messageList,
     isRecording,
     isAIVoicePlaying,
     timeLeft,
     formattedTime: formatTime(),
+    aiQuestion,
     startRecordingWithTimer,
     stopRecordingWithTimer,
   };

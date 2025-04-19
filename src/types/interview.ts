188d617b71a992c5860ee4cf29f7f1f39dc69654
Minjo Park <consistent.312@gmail.com>
2025-04-19 14:55:40 +0900

    refactor:#159 불필요한 코드 제거

--- src/types/interview.ts
+++ src/types/interview.ts
@@ -1,13 +1,1 @@
-import { InterviewHistory } from '@prisma/client';
-import { JsonValue } from '@prisma/client/runtime/library';
-
-export type InterviewQnAData = {
-  question: string;
-  answer: string;
-};
-
-export type InterviewHistoryWithResume = InterviewHistory & {
-  resume: {
-    content: JsonValue;
-  };
-};
+export type InterviewType = 'pressure' | 'calm';

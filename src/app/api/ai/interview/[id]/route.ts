b67a670b03cf8ea0546b7b90ff5369a4f3c30efb
Minjo Park <consistent.312@gmail.com>
2025-04-19 23:40:46 +0900

    feat:#159 인터뷰 기록 status 바꾸는 로직 추가

--- src/app/api/ai/interview/[id]/route.ts
+++ src/app/api/ai/interview/[id]/route.ts
@@ -13,7 +13,7 @@ const { EXPIRED_TOKEN } = AUTH_MESSAGE.ERROR;
 const { AUTH_REQUIRED } = AUTH_MESSAGE.RESULT;
 const { DB_SERVER_ERROR, DB_REQUEST_ERROR } = DB_MESSAGE.ERROR;
 const { USER_ID_VALIDATION } = DB_MESSAGE.VALIDATION;
-const { NOT_FOUND, GET_ERROR } = INTERVIEW_HISTORY.API;
+const { NOT_FOUND, GET_ERROR, PATCH_SERVER_ERROR, FORBIDDEN } = INTERVIEW_HISTORY.API;
 
 /**
  * 원하는 인터뷰 기록 조회 요청
@@ -51,22 +51,22 @@ export const GET = async (request: NextRequest, { params }: RouteParams) => {
  * 면접 기록 등록하는 요청
  * @param request resumeId 자소서 ID
  * @param request interviewType 면접관 타입(calm / pressure)
- * @param param1
+ * @param resumeId 자소서 아이디
  * @returns
  */
 export const POST = async (request: NextRequest, { params }: RouteParams) => {
   try {
+    const token = await getToken({ req: request, secret: NEXTAUTH_SECRET });
+    if (!token) return NextResponse.json({ message: EXPIRED_TOKEN }, { status: 401 });
+
+    const session = await getServerSession(authOptions);
+    const userId = session?.user.id;
+
     const { resumeId, interviewType } = await request.json();
 
     if (resumeId !== Number(params.id)) {
       return NextResponse.json({ message: DB_REQUEST_ERROR }, { status: 400 });
     }
-    const token = await getToken({ req: request, secret: NEXTAUTH_SECRET });
-
-    if (!token) return NextResponse.json({ message: EXPIRED_TOKEN }, { status: 401 });
-
-    const session = await getServerSession(authOptions);
-    const userId = session?.user.id;
 
     if (!session || !userId) return NextResponse.json({ message: USER_ID_VALIDATION }, { status: 401 });
 
@@ -81,5 +81,48 @@ export const POST = async (request: NextRequest, { params }: RouteParams) => {
     return NextResponse.json({ response }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ message: DB_SERVER_ERROR }, { status: 500 });
+  }
+};
+
+/**
+ * 인터뷰 기록 status 변경하는 요청
+ * @param interviewId 인터뷰 기록 ID
+ * @returns interviewHistory 업데이트된 인터뷰 기록
+ */
+export const PATCH = async (request: NextRequest, { params }: RouteParams) => {
+  try {
+    const token = await getToken({ req: request, secret: NEXTAUTH_SECRET });
+    if (!token) return NextResponse.json({ message: EXPIRED_TOKEN }, { status: 401 });
+
+    const session = await getServerSession(authOptions);
+    if (!session || !session.user) {
+      return NextResponse.json({ message: AUTH_REQUIRED }, { status: 401 });
+    }
+
+    const interviewId = Number(params.id);
+
+    const interviewHistory = await prisma.interviewHistory.findUnique({
+      where: { id: interviewId },
+    });
+
+    if (!interviewHistory) {
+      return NextResponse.json({ message: NOT_FOUND }, { status: 404 });
+    }
+
+    if (interviewHistory.userId && interviewHistory.userId !== session.user.id) {
+      return NextResponse.json({ message: FORBIDDEN }, { status: 403 });
+    }
+
+    const response = await prisma.interviewHistory.update({
+      where: { id: interviewId },
+      data: {
+        userId: session.user.id,
+        status: 1,
+      },
+    });
+
+    return NextResponse.json({ response }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: PATCH_SERVER_ERROR }, { status: 500 });
   }
 };

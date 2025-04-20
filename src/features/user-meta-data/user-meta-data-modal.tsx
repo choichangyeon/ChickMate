37a33fe6aaedf1f5709336f93e0b0a00c244c3d0
Hye10 <dhye410@daum.net>
2025-04-20 10:16:10 +0900

    refactor:#161 모달 내 title 태그 h1 -> h2로 수정

--- src/features/user-meta-data/user-meta-data-modal.tsx
+++ src/features/user-meta-data/user-meta-data-modal.tsx
@@ -4,7 +4,7 @@ import UserMetaDataForm from '@/features/user-meta-data/user-meta-data-form';
 const UserMetaDataModal = () => {
   return (
     <section>
-      <Typography as='h1' size='2xl' align='center' weight='bold'>
+      <Typography as='h2' size='2xl' align='center' weight='bold'>
         주요 이력 작성하기
       </Typography>
       <UserMetaDataForm />

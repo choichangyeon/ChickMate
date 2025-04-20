0dc78f1d860dd62107c092678529965e067227ec
Minjo Park <consistent.312@gmail.com>
2025-04-21 00:42:21 +0900

    design:#156 자소서 리스트 보여주는 레이아웃 구현

--- src/features/resume-list/resume-list.tsx
+++ src/features/resume-list/resume-list.tsx
@@ -1,5 +1,20 @@
+'use client';
+
+import ResumeItem from '@/features/resume-list/resume-item';
+import { useResumeListQuery } from '@/features/resume-list/hooks/use-resume-list-infinite-query';
+
 const ResumeList = () => {
-  return <div>ResumeList</div>;
+  const { data: resumeList, isPending, isError } = useResumeListQuery();
+
+  return (
+    <div>
+      <ul className='h-full overflow-y-auto scrollbar-hide'>
+        {resumeList.map((resume) => {
+          return <ResumeItem key={resume.id} resume={resume} />;
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default ResumeList;

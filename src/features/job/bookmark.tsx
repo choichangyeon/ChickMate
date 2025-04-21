a80bd1599a00787afa41340a16ed468d5961f514
choichangyeon <changyon99@naver.com>
2025-04-21 21:28:16 +0900

    refactor:#183 토큰 삭제 오류 처리

--- src/features/job/bookmark.tsx
+++ src/features/job/bookmark.tsx
@@ -2,6 +2,10 @@
 
 import { useBookmarkMutation } from '@/features/job/hooks/use-bookmark-mutation';
 import { Star } from '@/components/icons/star';
+import { PATH } from '@/constants/path-constant';
+import { useRouter } from 'next/navigation';
+
+const { SIGN_IN } = PATH.AUTH;
 
 type Props = {
   jobPostingId: number;
@@ -10,12 +14,18 @@ type Props = {
 };
 
 const Bookmark = ({ jobPostingId, isBookmarked, userId }: Props) => {
-  const { mutate: bookmarkMutate } = useBookmarkMutation({ jobPostingId, userId });
+  const { mutate: bookmarkMutate, isError } = useBookmarkMutation({ jobPostingId, userId });
+  const router = useRouter();
 
   const handleClick = () => {
     bookmarkMutate(isBookmarked);
     // TODO: alert 구현
   };
+
+  if (isError) {
+    alert('로그인이 다시 진행해주시길 바랍니다.');
+    router.replace(SIGN_IN);
+  }
 
   return isBookmarked ? (
     <button type='button' onClick={handleClick} aria-label='북마크 버튼'>

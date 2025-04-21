e5ad0085d8968efeeb2679a9d487496b323893b9
Minjo Park <consistent.312@gmail.com>
2025-04-21 21:20:56 +0900

    feat:#156 자소서 등록 mutation 적용

---
+++ src/features/resume/hooks/use-add-resume-mutation.ts
@@ -0,0 +1,39 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { submitResume } from '@/features/resume/api/client-services';
+import { QUERY_KEY } from '@/constants/query-key';
+import type { ResumeData } from '@/types/resume';
+import type { Resume } from '@prisma/client';
+
+const { RESUMES } = QUERY_KEY;
+
+type Props = {
+  resumeId: number | null;
+  data: ResumeData;
+};
+
+export const useAddResumeMutation = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: ({ resumeId, data }: Props) => submitResume({ resumeId, data }),
+    onMutate: async ({ resumeId, data }) => {
+      await queryClient.cancelQueries({ queryKey: [RESUMES] });
+      const previousResume = queryClient.getQueryData([RESUMES]) as Resume[];
+
+      queryClient.setQueryData([RESUMES], (old: Resume[] = []) => {
+        if (resumeId !== null) {
+          return old.map((resume) => (resume.id === resumeId ? { ...resume, ...data } : resume));
+        }
+        return [...old, data];
+      });
+
+      return { previousResume };
+    },
+    onError: (error) => {
+      throw error;
+    },
+    onSettled: (data, error, resumeId) => {
+      queryClient.invalidateQueries({ queryKey: [RESUMES] });
+    },
+  });
+};

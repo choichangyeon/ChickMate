ac09cd384aaf4a0cac4fbf3dac095c9064539328
Minjo Park <consistent.312@gmail.com>
2025-04-21 13:56:03 +0900

    feat:#156 자소서 리스트 불러오는 로직 연결

---
+++ src/features/resume-list/hooks/use-resume-list-infinite-query.ts
@@ -0,0 +1,17 @@
+import { useQuery } from '@tanstack/react-query';
+import { QUERY_KEY } from '@/constants/query-key';
+import { STALE_TIME } from '@/constants/time-constants';
+import { RESUME_STATUS } from '@/constants/resume-constants';
+import { getResumeList } from '@/features/resume/api/client-services';
+
+const { RESUME } = QUERY_KEY;
+const { MIN } = STALE_TIME;
+const { SUBMIT } = RESUME_STATUS;
+
+export const useResumeListQuery = () => {
+  return useQuery({
+    queryKey: [RESUME],
+    queryFn: () => getResumeList(SUBMIT),
+    staleTime: MIN,
+  });
+};

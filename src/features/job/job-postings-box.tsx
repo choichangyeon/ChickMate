63a1863852385786388e3f4985a538466ad132dc
ImJaeOne <dlawi0108@naver.com>
2025-04-23 01:24:57 +0900

    design:#189 justify-between 삭제

--- src/features/job/job-postings-box.tsx
+++ src/features/job/job-postings-box.tsx
@@ -76,8 +76,8 @@ const JobPostingsBox = ({ userId }: Props) => {
       ) : data.jobPostingList.length === 0 ? (
         <JobPostingBlockComponent type='no-bookmark' />
       ) : (
-        <div className='flex flex-col items-center gap-6'>
-          <section className='flex flex-wrap justify-between gap-5 self-stretch scrollbar-hide'>
+        <div className='flex flex-col items-center justify-between gap-6'>
+          <section className='flex flex-wrap gap-5 self-stretch scrollbar-hide'>
             {data.jobPostingList.map((jobPosting) => (
               <JobPostingCard key={jobPosting.id} userId={userId} jobPosting={jobPosting} />
             ))}

ce81ba6d67daf14902bb89a2b35d9d0f410d4e73
Minjo Park <consistent.312@gmail.com>
2025-04-22 21:42:53 +0900

    comment:#190 주석 추가

--- src/utils/show-notiflix-confirm.ts
+++ src/utils/show-notiflix-confirm.ts
@@ -9,6 +9,12 @@ type Props = {
   cancelFunction?: () => void;
 };
 
+/**
+ * Confirm창 보여주는 함수
+ * @param {String} message confirm창에서 보여줄 메시지
+ * @param {Function} okFunction 확인 버튼을 눌렀을 때 실행하는 함수
+ * @param {Function} cancelFunction 취소 버튼을 눌렀을 때 실행하는 함수
+ */
 export const showNotiflixConfirm = ({ message, okFunction, cancelFunction }: Props) => {
   Confirm.show(
     CONFIRM,

5cfbf23083e88dac4399bac2cca421826c1607be
choichangyeon <changyon99@naver.com>
2025-04-22 22:12:41 +0900

    refactor:#188 상수 구조분해 할당

--- src/features/interview/hooks/use-interview-history-mutation.ts
+++ src/features/interview/hooks/use-interview-history-mutation.ts
@@ -2,6 +2,8 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { QUERY_KEY } from '@/constants/query-key';
 import { patchInterviewHistoryStatus } from '@/features/interview/api/client-services';
 
+const { HISTORY } = QUERY_KEY;
+
 export const usePatchInterviewHistoryMutation = () => {
   const queryClient = useQueryClient();
 
@@ -9,7 +11,7 @@ export const usePatchInterviewHistoryMutation = () => {
     mutationFn: ({ interviewId, status }: { interviewId: number; status: number }) =>
       patchInterviewHistoryStatus({ interviewId, status }),
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: [QUERY_KEY.HISTORY] });
+      queryClient.invalidateQueries({ queryKey: [HISTORY] });
     },
     onError: (error) => {
       throw error;

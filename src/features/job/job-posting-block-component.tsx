d3fa9ce2d47723acd162122fbf4ab2ca1edd98b5
ImJaeOne <dlawi0108@naver.com>
2025-04-22 16:42:26 +0900

    refactor:#182 공통된 BlockComponent 컴포넌트화

---
+++ src/features/job/job-posting-block-component.tsx
@@ -0,0 +1,53 @@
+'use client';
+
+import BlockComponent from '@/components/common/block-component';
+import { PATH } from '@/constants/path-constant';
+import { useRouter } from 'next/navigation';
+import React from 'react';
+
+type Props = {
+  type: 'unauthenticated' | 'no-profile' | 'no-job-data' | 'fetch-error';
+};
+
+const {
+  AUTH: { SIGN_IN },
+  MY_PAGE,
+} = PATH;
+
+export const JobPostingBlockComponent = ({ type }: Props) => {
+  const router = useRouter();
+  const content = {
+    'unauthenticated': {
+      firstLine: '이런! 로그인을 하지 않았네요!',
+      secondLine: '로그인이 필요합니다!',
+      thirdLine: '맞춤형 채용공고는 내 정보를 기반으로 진행됩니다',
+      buttonName: '로그인하러 가기',
+      href: SIGN_IN,
+    },
+    'no-profile': {
+      firstLine: '이런! 사용자 정보를 설정하지 않았네요!',
+      secondLine: '내 정보를 작성해볼까요?',
+      thirdLine: '맞춤형 채용공고는 내 정보를 기반으로 진행됩니다',
+      buttonName: '내 정보 설정하기',
+      href: MY_PAGE,
+    },
+    'no-job-data': {
+      firstLine: '이런! 나에게 맞는 채용공고가 없어요!',
+      secondLine: '채용공고를 다시 요청할까요?',
+      thirdLine: '맞춤형 채용공고는 내 정보를 기반으로 진행됩니다',
+      buttonName: '채용공고 불러오기!',
+      onClick: () => router.refresh(),
+    },
+    'fetch-error': {
+      firstLine: '이런! 사용자 정보를 설정하지 않았네요!',
+      secondLine: '내 정보를 작성해볼까요?',
+      thirdLine: '맞춤형 채용공고는 내 정보를 기반으로 진행됩니다',
+    },
+  }[type];
+
+  return (
+    <section className='flex h-[400px] flex-col items-center justify-center self-stretch'>
+      <BlockComponent {...content} />
+    </section>
+  );
+};

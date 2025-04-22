91ab96165fc0cc746b153fe623cad053b9e2592e
ImJaeOne <dlawi0108@naver.com>
2025-04-23 02:06:42 +0900

    refactor:#189 불필요한 prefetch 삭제

--- src/features/job/job-posting-section.tsx
+++ src/features/job/job-posting-section.tsx
@@ -1,35 +1,18 @@
-import { serverActionWithSentry } from '@/utils/server-action-with-sentry';
-import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
 import { Session } from 'next-auth';
-import { QUERY_KEY } from '@/constants/query-key';
-import { STALE_TIME } from '@/constants/time-constants';
 import JobPostingsBox from '@/features/job/job-postings-box';
-import { getJobByUserMetaData } from '@/features/job/api/server-services';
 import { JobPostingBlockComponent } from '@/features/job/job-posting-block-component';
 
 type Props = {
   session: Session | null;
 };
 
-const { JOB_POSTING } = QUERY_KEY;
-const { AN_HOUR } = STALE_TIME;
-
 const JobPostingSection = async ({ session }: Props) => {
   if (!session) {
     return <JobPostingBlockComponent type='unauthenticated' />;
   }
-
-  const queryClient = new QueryClient();
-  await queryClient.prefetchQuery({
-    queryKey: [JOB_POSTING, session.user.id],
-    queryFn: () => serverActionWithSentry(getJobByUserMetaData),
-    staleTime: AN_HOUR,
-  });
   return (
     <article>
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <JobPostingsBox userId={session.user.id} />
-      </HydrationBoundary>
+      <JobPostingsBox userId={session.user.id} />
     </article>
   );
 };

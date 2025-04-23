c06a8ff42e867b94c1cc30d8939c1128ad8ecf71
Minjo Park <consistent.312@gmail.com>
2025-04-24 01:48:45 +0900

    design:#200 비밀번호 보이게 하는 아이콘 적용

--- src/features/sign/auth-input.tsx
+++ src/features/sign/auth-input.tsx
@@ -1,4 +1,9 @@
+'use client';
+
+import EyeIcon from '@/components/icons/eye-icon';
+import EyeOffIcon from '@/components/icons/eye-off-icon';
 import Typography from '@/components/ui/typography';
+import { useState } from 'react';
 import { FieldValues, Path, UseFormRegister } from 'react-hook-form';
 
 type Props<T extends FieldValues> = {
@@ -10,23 +15,37 @@ type Props<T extends FieldValues> = {
   placeholder?: string;
 };
 
-const AuthInput = <T extends FieldValues>({ label, id, register, error, type, placeholder }: Props<T>) => (
-  <div className='flex w-full flex-col gap-1 px-3'>
-    <Typography as='h2' weight='bold' size='sm'>
-      {label}
-    </Typography>
-    <input
-      {...register(id)}
-      type={type}
-      placeholder={placeholder}
-      className='border-b border-cool-gray-300 placeholder-cool-gray-300 focus:outline-0'
-    />
-    {error && (
-      <Typography size='xs' color='primary-600'>
-        {error.message}
+const AuthInput = <T extends FieldValues>({ label, id, register, error, type, placeholder }: Props<T>) => {
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+  const isPassword = id === 'password' || id === 'passwordCheck';
+
+  const toggleVisibility = () => setIsPasswordVisible((prev) => !prev);
+
+  return (
+    <div className='flex w-full flex-col gap-1 px-3'>
+      <Typography as='h2' weight='bold' size='sm'>
+        {label}
       </Typography>
-    )}
-  </div>
-);
+      <div className='relative'>
+        <input
+          {...register(id)}
+          type={isPassword ? (isPasswordVisible ? 'text' : 'password') : type}
+          placeholder={placeholder}
+          className='w-full border-b border-cool-gray-300 pr-8 placeholder-cool-gray-300 focus:outline-0'
+        />
+        {isPassword && (
+          <button type='button' onClick={toggleVisibility} className='absolute right-0 top-1/2 -translate-y-1/2'>
+            {isPasswordVisible ? <EyeOffIcon /> : <EyeIcon />}
+          </button>
+        )}
+      </div>
+      {error && (
+        <Typography size='xs' color='primary-600'>
+          {error.message}
+        </Typography>
+      )}
+    </div>
+  );
+};
 
 export default AuthInput;

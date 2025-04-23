fddecb01de07f73e97f9271c50883a481907fca5
ImJaeOne <dlawi0108@naver.com>
2025-04-23 20:13:41 +0900

    fix:#205 캐릭터 히스토리 기준으로 경험치 획득할 수 있도록 로직 수정

--- src/app/api/resume/count/route.ts
+++ src/app/api/resume/count/route.ts
@@ -1,5 +1,6 @@
+import { CHARACTER_HISTORY_KOR } from '@/constants/character-constants';
 import { ENV } from '@/constants/env-constants';
-import { AUTH_MESSAGE, RESUME_MESSAGE } from '@/constants/message-constants';
+import { AUTH_MESSAGE, CHARACTER_MESSAGE, RESUME_MESSAGE } from '@/constants/message-constants';
 import { prisma } from '@/lib/prisma';
 import { authOptions } from '@/utils/auth-option';
 import { getServerSession } from 'next-auth';
@@ -7,11 +8,13 @@ import { getToken } from 'next-auth/jwt';
 import { NextRequest, NextResponse } from 'next/server';
 
 const { NEXTAUTH_SECRET } = ENV;
+const { RESUME_SUBMISSION } = CHARACTER_HISTORY_KOR;
 const {
   ERROR: { EXPIRED_TOKEN },
   RESULT: { AUTH_REQUIRED },
 } = AUTH_MESSAGE;
 const { GET_COUNT_ERROR } = RESUME_MESSAGE;
+const { INFO: GET_DATA_NULL } = CHARACTER_MESSAGE;
 const MAX_COUNT_TO_GET_EXP_DURING_A_DAY = 3;
 export const GET = async (request: NextRequest) => {
   try {
@@ -25,15 +28,30 @@ export const GET = async (request: NextRequest) => {
     const startOfDay = new Date(today.setHours(0, 0, 0, 0));
     const endOfDay = new Date(today.setHours(23, 59, 59, 999));
 
-    const response = await prisma.resume.count({
+    const characterId = await prisma.character.findFirst({
       where: {
         userId: session.user.id,
+      },
+      select: {
+        id: true,
+      },
+    });
+
+    if (!characterId) {
+      return NextResponse.json({ response: { isAbleToGetEXP: false } }, { status: 200 });
+    }
+
+    const response = await prisma.characterHistory.count({
+      where: {
+        characterId: characterId.id,
+        history: RESUME_SUBMISSION,
         createdAt: {
           gte: startOfDay,
           lte: endOfDay,
         },
       },
     });
+
     if (response <= MAX_COUNT_TO_GET_EXP_DURING_A_DAY) {
       return NextResponse.json({ response: { isAbleToGetEXP: true } }, { status: 200 });
     }

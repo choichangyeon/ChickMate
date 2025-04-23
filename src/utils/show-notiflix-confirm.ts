73b2186cd99b3cf766150c09c024a1936581989a
Minjo Park <consistent.312@gmail.com>
2025-04-23 18:02:48 +0900

    refactor:#200 props 추가적으로 받을 수 있도록 수정

--- src/utils/show-notiflix-confirm.ts
+++ src/utils/show-notiflix-confirm.ts
@@ -1,28 +1,41 @@
-import { NOTIFLIX_CONFIRM } from '@/constants/notiflix-constants';
 import { Confirm } from 'notiflix';
+import { NOTIFLIX_CONFIRM_DEFAULT } from '@/constants/notiflix-constants';
 
-const { OK, CANCEL, CONFIRM } = NOTIFLIX_CONFIRM;
+const { OK_BUTTON_TEXT, CANCEL_BUTTON_TEXT, CONFIRM } = NOTIFLIX_CONFIRM_DEFAULT;
 
 type Props = {
+  title?: string;
   message: string;
+  okButtonText?: string;
+  cancelButtonText?: string;
   okFunction: () => void;
   cancelFunction?: () => void;
 };
 
 /**
  * Confirm창 보여주는 함수
+ * @param {String} title confirm창에서 보여줄 메시지의 제목
  * @param {String} message confirm창에서 보여줄 메시지
+ * @param {String} okButtonText 확인 버튼명
+ * @param {String} cancelButtonText 취소 버튼명
  * @param {Function} okFunction 확인 버튼을 눌렀을 때 실행하는 함수
  * @param {Function} cancelFunction 취소 버튼을 눌렀을 때 실행하는 함수
  */
-export const showNotiflixConfirm = ({ message, okFunction, cancelFunction }: Props) => {
+export const showNotiflixConfirm = ({
+  title = CONFIRM,
+  message,
+  okButtonText = OK_BUTTON_TEXT,
+  cancelButtonText = CANCEL_BUTTON_TEXT,
+  okFunction,
+  cancelFunction,
+}: Props) => {
   document.body.classList.add('confirm-open');
 
   Confirm.show(
-    CONFIRM,
-    `${message}`,
-    OK,
-    CANCEL,
+    title,
+    message,
+    okButtonText,
+    cancelButtonText,
     () => {
       document.body.classList.remove('confirm-open');
       okFunction();

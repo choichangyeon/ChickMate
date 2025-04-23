348e4855fd1923e740514eebbd4bfe936cbc30ab
ImJaeOne <dlawi0108@naver.com>
2025-04-23 12:14:46 +0900

    refactor:#189 select box 하드 코딩 리팩토링

--- src/features/job/job-posting-select-box.tsx
+++ src/features/job/job-posting-select-box.tsx
@@ -1,24 +1,32 @@
+// features/job/job-posting-select-box.tsx
 import { ChangeEvent } from 'react';
-import { SortOption } from '@/features/job/job-postings-box';
+import type { SortOption } from '@/features/job/job-postings-box';
+
+const JOB_SORT_OPTIONS: { name: string; value: SortOption }[] = [
+  { name: '마감순', value: 'deadline' },
+  { name: '최신순', value: 'latest' },
+  { name: '오래된 순', value: 'oldest' },
+  { name: '기업명 순', value: 'company' },
+  { name: '북마크한 공고', value: 'bookmark' },
+];
 
 type Props = {
   sortOption: SortOption;
   changeNewParams: (e: ChangeEvent<HTMLSelectElement>) => void;
 };
+
 const JobPostingSelectBox = ({ sortOption, changeNewParams }: Props) => {
   return (
     <select
       value={sortOption}
-      onChange={(e) => {
-        changeNewParams(e);
-      }}
+      onChange={changeNewParams}
       className='mb-4 rounded-md border px-2 py-1 text-sm shadow-sm'
     >
-      <option value='latest'>최신순</option>
-      <option value='oldest'>오래된 순</option>
-      <option value='deadline'>마감 임박 순</option>
-      <option value='company'>기업명 순</option>
-      <option value='bookmark'>북마크한 공고</option>
+      {JOB_SORT_OPTIONS.map(({ name, value }) => (
+        <option key={value} value={value}>
+          {name}
+        </option>
+      ))}
     </select>
   );
 };

b77544c0f96615d4ac707d94df775fcfe9a175b9
choichangyeon <changyon99@naver.com>
2025-04-23 20:11:30 +0900

    feat:#188 삭제 Message 상수화

--- src/constants/notiflix-constants.ts
+++ src/constants/notiflix-constants.ts
@@ -11,3 +11,11 @@ export const NOTIFLIX_CONFIRM_SAMPLE = {
   OK_BUTTON_TEXT: 'sample ok',
   CANCEL_BUTTON_TEXT: 'cancel CANCEL',
 };
+
+export const NOTIFLIX_WARNING_INTERVIEW_IN_PROGRESS = {
+  WARNING: 'Warning',
+  OK_BUTTON_TEXT: '이어하기',
+  CANCEL_BUTTON_TEXT: '삭제하기',
+  MESSAGE:
+    '면접 진행 중인 내용이 있습니다. <br/> 이어서 면접을 진행하시겠습니까?<br/> (이어가지 않을 시 면접 기록이 삭제됩니다.)',
+};

1f127ddc0808725004ee6f600f5e951a408be98a
choichangyeon <changyon99@naver.com>
2025-04-23 20:12:45 +0900

    feat:#188 alert InProgress 기능 구현

--- src/features/interview/alert-in-progress.tsx
+++ src/features/interview/alert-in-progress.tsx
@@ -1,21 +1,35 @@
 'use client';
 
+import { NOTIFLIX_WARNING_INTERVIEW_IN_PROGRESS } from '@/constants/notiflix-constants';
 import { showNotiflixConfirm } from '@/utils/show-notiflix-confirm';
 import { useEffect, useState } from 'react';
+import { deleteInterviewHistory } from '@/features/interview/api/client-services';
+import { useRouter } from 'next/navigation';
+import { PATH } from '@/constants/path-constant';
 
-const AlertInProgress = () => {
+const { WARNING, OK_BUTTON_TEXT, CANCEL_BUTTON_TEXT, MESSAGE } = NOTIFLIX_WARNING_INTERVIEW_IN_PROGRESS;
+const { LIVE } = PATH.INTERVIEW;
+
+type Props = {
+  interviewId: number;
+};
+
+const AlertInProgress = ({ interviewId }: Props) => {
   const [isAlert, setIsAlert] = useState(false);
+  const router = useRouter();
   useEffect(() => {
     if (!isAlert) {
       showNotiflixConfirm({
-        message: `면접 진행 중인 내용이 있습니다. 이어서 면접을 진행하시겠습니까? \n
-        (취소 시 면접 기록이 삭제됩니다.)`,
-        okFunction: () => console.log('ok'),
-        cancelFunction: () => console.log('cancel'),
+        title: WARNING,
+        message: MESSAGE,
+        okButtonText: OK_BUTTON_TEXT,
+        cancelButtonText: CANCEL_BUTTON_TEXT,
+        okFunction: () => router.push(LIVE(interviewId)),
+        cancelFunction: () => deleteInterviewHistory({ interviewId }),
       });
       setIsAlert(true);
     }
-  }, []);
+  }, [isAlert]);
 
   return null;
 };

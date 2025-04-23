7d27e92174aecef42dc019114a1b48d94257bfa4
Minjo Park <consistent.312@gmail.com>
2025-04-24 01:29:43 +0900

    refactor:#200 h1 태그로 변경

--- src/features/sign/sign-up-auth-form.tsx
+++ src/features/sign/sign-up-auth-form.tsx
@@ -39,7 +39,7 @@ const SignUpAuthForm = () => {
   return (
     <main className='mx-auto flex w-full max-w-md flex-col gap-8 rounded-3xl border border-cool-gray-200 bg-white p-6'>
       <div>
-        <Typography size='2xl' weight='normal' align='center'>
+        <Typography as='h1' size='2xl' weight='normal' align='center'>
           만나서 반가워요.<span className='font-bold'>칰칰</span>씨!
         </Typography>
         <Typography color='gray-700' weight='normal' align='center'>

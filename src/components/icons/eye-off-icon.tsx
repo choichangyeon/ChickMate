e468784a6370a9ed9099432afcf0e9dd0165dadf
Minjo Park <consistent.312@gmail.com>
2025-04-24 01:35:50 +0900

    design:#200 눈 아이콘 추가

---
+++ src/components/icons/eye-off-icon.tsx
@@ -0,0 +1,17 @@
+const EyeOffIcon = () => {
+  return (
+    <svg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <path
+        d='M8 11L8.42229 11.2111C10.6745 12.3373 13.3255 12.3373 15.5777 11.2111L16 11'
+        stroke='#33363F'
+        strokeWidth='2'
+        strokeLinecap='round'
+      />
+      <path d='M12 12.5V14' stroke='#33363F' strokeWidth='2' strokeLinecap='round' />
+      <path d='M9 12L8.5 13' stroke='#33363F' strokeWidth='2' strokeLinecap='round' />
+      <path d='M15 12L15.5 13' stroke='#33363F' strokeWidth='2' strokeLinecap='round' />
+    </svg>
+  );
+};
+
+export default EyeOffIcon;

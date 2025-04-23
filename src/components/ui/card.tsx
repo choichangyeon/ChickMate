2f27968b478e6287a7fb78a70fb67ecb0a014277
Hye10 <dhye410@daum.net>
2025-04-23 19:03:23 +0900

    style:#208 card component min-height 적용

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -23,7 +23,7 @@ export default Card;
 //TODO: bg-emerald-900/0 -> color 수정, outline-gray-200 -> color 수정
 const cardClassName = clsx(
   'relative flex-shrink-0 items-start justify-between rounded-lg bg-emerald-900/0 outline outline-1 outline-offset-[-1px] outline-gray-200',
-  'min-w-[380px] p-6'
+  'min-w-[380px] min-h-[180px] p-6'
 );
 
 const contentClassName = clsx('flex h-full flex-col justify-between');

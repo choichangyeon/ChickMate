26b89808afedc1e5c8974a78cccae91f70d6a8d7
Minjo Park <consistent.312@gmail.com>
2025-04-23 17:03:34 +0900

    refactor:#200 로그인한 사용자가 로그인/회원가입 페이지로 이동하지 못하도록 막음

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,11 +3,12 @@ import { NextRequest, NextResponse } from 'next/server';
 import { PATH, QUERY_PARAMS } from '@/constants/path-constant';
 
 const {
-  AUTH: { SIGN_IN },
+  AUTH: { SIGN_IN, SIGN_UP },
   MY_PAGE,
   INTERVIEW,
   RESUME,
   JOB,
+  ON_BOARDING,
 } = PATH;
 
 const { UNAUTH } = QUERY_PARAMS;
@@ -16,16 +17,22 @@ export const middleware = async (request: NextRequest) => {
   const token = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET });
   const pathname = request.nextUrl.pathname;
 
-  if (!token && [MY_PAGE, RESUME.ROOT, JOB, INTERVIEW.START].some((path) => pathname.startsWith(path))) {
+  const protectedPaths = [MY_PAGE, RESUME.ROOT, JOB, INTERVIEW.START];
+  if (!token && protectedPaths.some((path) => pathname.startsWith(path))) {
     const signInUrl = new URL(SIGN_IN, request.url);
     signInUrl.searchParams.set(UNAUTH, 'true');
     signInUrl.searchParams.set('prevUrl', pathname);
     return NextResponse.redirect(signInUrl);
   }
 
+  const authPaths = [SIGN_IN, SIGN_UP];
+  if (token && authPaths.some((authPath) => pathname.startsWith(authPath))) {
+    return NextResponse.redirect(new URL(ON_BOARDING, request.url));
+  }
+
   return NextResponse.next();
 };
 
 export const config = {
-  matcher: ['/auth/sign-in', '/interview/live/:path*', '/my-page'],
+  matcher: ['/auth/sign-in', '/auth/sign-up', '/interview/live/:path*', '/my-page'],
 };

7d6762e4d20b75ec39d8102ce044901a70ba6399
choichangyeon <changyon99@naver.com>
2025-04-24 12:39:01 +0900

    refactor:#188 notiflix 상수 변경

--- src/constants/notiflix-constants.ts
+++ src/constants/notiflix-constants.ts
@@ -14,8 +14,12 @@ export const NOTIFLIX_CONFIRM_SAMPLE = {
 
 export const NOTIFLIX_WARNING_INTERVIEW_IN_PROGRESS = {
   WARNING: 'Warning',
-  OK_BUTTON_TEXT: '이어하기',
-  CANCEL_BUTTON_TEXT: '삭제하기',
-  MESSAGE:
-    '면접 진행 중인 내용이 있습니다. <br/> 이어서 면접을 진행하시겠습니까?<br/> (이어가지 않을 시 면접 기록이 삭제됩니다.)',
+  DEFAULT: {
+    OK_BUTTON_TEXT: '이어하기',
+    CANCEL_BUTTON_TEXT: '삭제하기',
+    MESSAGE: '진행 중인 면접이 있습니다. 이어서 진행하시겠습니까?',
+  },
+  ERROR: {
+    ERROR_MESSAGE: '면접 기록 요청에 오류가 생겼습니다.',
+  },
 };

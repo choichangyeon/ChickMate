9afb0b69d9413a795a98289684616093f3cd6705
choichangyeon <changyon99@naver.com>
2025-04-24 12:40:24 +0900

    refactor:#188 서버액션 삭제

--- src/features/interview/api/server-services.ts
+++ src/features/interview/api/server-services.ts
@@ -4,12 +4,9 @@ import { getServerSession } from 'next-auth';
 import { authOptions } from '@/utils/auth-option';
 import { prisma } from '@/lib/prisma';
 import { AUTH_MESSAGE, INTERVIEW_HISTORY } from '@/constants/message-constants';
-import { INTERVIEW_HISTORY_STATUS } from '@/constants/interview-constants';
-import { InterviewHistoryType } from '@/types/DTO/interview-history-dto';
 
 const { GET_ERROR } = INTERVIEW_HISTORY.API;
 const { SESSION_NO_USER } = AUTH_MESSAGE.ERROR;
-const { IN_PROGRESS } = INTERVIEW_HISTORY_STATUS;
 
 /**
  * 원하는 인터뷰 기록 불러오기 요청
@@ -60,37 +57,6 @@ export const getInterviewQnA = async (interviewId: number) => {
     });
 
     return response;
-  } catch (error) {
-    throw new Error(GET_ERROR);
-  }
-};
-
-/**
- *
- * @returns InProgress 상태의 인터뷰 히스토리 가져오기
- */
-export const getInterviewHistoryAboutInProgress = async () => {
-  try {
-    const session = await getServerSession(authOptions);
-    if (!session || !session.user) {
-      throw new Error(SESSION_NO_USER);
-    }
-
-    const response: InterviewHistoryType[] = await prisma.interviewHistory.findMany({
-      where: {
-        userId: session.user.id,
-        status: IN_PROGRESS,
-      },
-      orderBy: {
-        createdAt: 'desc',
-      },
-    });
-
-    if (response.length === 0) {
-      return null;
-    }
-
-    return response[0];
   } catch (error) {
     throw new Error(GET_ERROR);
   }

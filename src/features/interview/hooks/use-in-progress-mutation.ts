3c7b1580f58c600d55d68bf16cb99246b2a70def
choichangyeon <changyon99@naver.com>
2025-04-24 12:40:03 +0900

    feat:#188 tanstack query 상태 관리

---
+++ src/features/interview/hooks/use-in-progress-mutation.ts
@@ -0,0 +1,27 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { QUERY_KEY } from '@/constants/query-key';
+import { deleteInterviewHistory } from '@/features/interview/api/client-services';
+import type { InterviewHistoryType } from '@/types/DTO/interview-history-dto';
+import type { UserType } from '@/types/DTO/user-dto';
+
+const { IN_PROGRESS } = QUERY_KEY;
+
+type Props = {
+  interviewId: InterviewHistoryType['id'];
+  status: InterviewHistoryType['status'];
+  options: string;
+};
+
+export const useInProgressMutation = (userId: UserType['id']) => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: ({ interviewId, options, status }: Props) => deleteInterviewHistory({ interviewId, options, status }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [IN_PROGRESS, userId] });
+    },
+    onError: (error) => {
+      throw error;
+    },
+  });
+};
---
+++ src/features/interview/hooks/use-in-progress-query.ts
@@ -0,0 +1,16 @@
+import { useQuery } from '@tanstack/react-query';
+import { getInterviewHistoryAboutInProgress } from '@/features/interview/api/client-services';
+import { QUERY_KEY } from '@/constants/query-key';
+import { STALE_TIME } from '@/constants/time-constants';
+import type { UserType } from '@/types/DTO/user-dto';
+
+const { IN_PROGRESS } = QUERY_KEY;
+const { MIN } = STALE_TIME;
+
+export const useInProgressQuery = (userId: UserType['id']) => {
+  return useQuery({
+    queryKey: [IN_PROGRESS, userId],
+    queryFn: () => getInterviewHistoryAboutInProgress({ userId }),
+    staleTime: MIN,
+  });
+};

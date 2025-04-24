24dae9bdee4766a94a5302e055aa0923370ad4c7
choichangyeon <changyon99@naver.com>
2025-04-24 22:04:02 +0900

    rename:#188 아이콘명 수정

--- src/components/icons/block-camera.tsx
+++ src/components/icons/block-camera.tsx
@@ -1,6 +1,6 @@
 import { SVGProps } from 'react';
 
-export const Camera = (props: SVGProps<SVGSVGElement>) => {
+export const BlockCamera = (props: SVGProps<SVGSVGElement>) => {
   return (
     <svg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg' {...props}>
       <path

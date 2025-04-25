757700c1c2439084a0d514417febff2185fb691d
choichangyeon <changyon99@naver.com>
2025-04-26 00:54:40 +0900

    refactor:#188 redo

--- src/features/interview/hooks/use-interview-history-mutation.ts
+++ src/features/interview/hooks/use-interview-history-mutation.ts
@@ -1,6 +1,9 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { patchInterviewHistoryStatus } from '@/features/interview/api/client-services';
 import type { InterviewHistoryType } from '@/types/DTO/interview-history-dto';
+import { QUERY_KEY } from '@/constants/query-key';
+
+const { HISTORY } = QUERY_KEY;
 
 type Props = {
   interviewId: InterviewHistoryType['id'];
@@ -8,8 +11,12 @@ type Props = {
 };
 
 export const usePatchInterviewHistoryMutation = () => {
+  const queryClient = useQueryClient();
   return useMutation({
     mutationFn: ({ interviewId, status }: Props) => patchInterviewHistoryStatus({ interviewId, status }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [HISTORY] });
+    },
     onError: (error) => {
       throw error;
     },

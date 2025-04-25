306f9c1b37606f1eea46697ba080430f0ae2d372
Hye10 <dhye410@daum.net>
2025-04-25 22:20:44 +0900

    style:#196 공통 에러컴포넌트 스타일 적용 (-> 아직 UI 미반영/ 임시 스타일)

--- src/components/common/error-component.tsx
+++ src/components/common/error-component.tsx
@@ -1,12 +1,20 @@
 'use client';
 
 import Typography from '@/components/ui/typography';
+import Button from '@/components/ui/button';
 
+const DEFAULT_ERROR_MESSAGE = '에러가 발생했습니다.🥲';
 const ErrorComponent = () => {
   return (
-    <section>
-      <Typography>에러가 발생했습니다.🥲</Typography>
-      <button onClick={() => window.location.reload()}>새로고침</button>
+    <section className='flex w-full flex-col items-center'>
+      <Typography as='h2' size='xl'>
+        {DEFAULT_ERROR_MESSAGE}
+      </Typography>
+      <div className='mt-4'>
+        <Button size='large' onClick={() => window.location.reload()}>
+          새로고침
+        </Button>
+      </div>
     </section>
   );
 };

2eee995fc094804032ad42383ca079f26da76ea3
Hye10 <dhye410@daum.net>
2025-04-26 17:47:32 +0900

    refactor:#196 면접 상세 결과 페이지 > 마지막 영역에서는 blur 처리 제외

--- src/features/interview-history/interview-detail-history.tsx
+++ src/features/interview-history/interview-detail-history.tsx
@@ -1,4 +1,7 @@
 import Image from 'next/image';
+import { useRef } from 'react';
+import { useInterSectionObserverHook } from '@/features/interview-history/hook/use-intersection-observer-hook';
+import { getInterviewerName } from '@/features/interview-history/utils/get-interviewer-name';
 import Typography from '@/components/ui/typography';
 import type { InterviewHistoryType } from '@/types/DTO/interview-history-dto';
 import type { InterviewQnAType } from '@/types/DTO/interview-qna-dto';
@@ -13,17 +16,19 @@ type Props = {
 
 const InterviewDetailHistory = ({ data }: Props) => {
   const history = data.InterviewQnAList;
+  const lastItemRef = useRef<HTMLLIElement | null>(null);
+  const { isVisible } = useInterSectionObserverHook(lastItemRef);
 
   return (
     <section className='relative flex-1 overflow-hidden'>
-      <div className='z-overlay pointer-events-none absolute bottom-0 left-0 right-0 h-10 bg-gradient-to-t from-white to-transparent' />
-
+      {!isVisible && (
+        <div className='pointer-events-none absolute bottom-0 left-0 right-0 z-overlay h-10 bg-gradient-to-t from-white to-transparent' />
+      )}
       <ol className='flex h-full flex-col gap-4 overflow-y-auto pr-2 scrollbar-hide'>
         {history.map((interviewQna, idx) => (
-          <li key={idx}>
+          <li key={idx} ref={history.length === idx + 1 ? lastItemRef : null}>
             <div className='flex gap-4'>
               <div>
-                {/* 면접관 원형 이미지 추가 시 calm, pressure에 따른 이미지 변경 */}
                 <Image
                   src={`/assets/character/interviewer/poly-interviewer-icon-${data.interviewType}.png`}
                   width={48}
@@ -33,7 +38,7 @@ const InterviewDetailHistory = ({ data }: Props) => {
               </div>
               <div>
                 <Typography weight='bold' color='primary-600'>
-                  {data.interviewType}
+                  {getInterviewerName(data.interviewType)}
                 </Typography>
                 <Typography size='sm' weight='normal' color='gray-500'>
                   {data.resume.title}
@@ -46,9 +51,7 @@ const InterviewDetailHistory = ({ data }: Props) => {
               </Typography>
             </div>
             <div>
-              <Typography color='secondary-amber' align='right'>
-                내 답변
-              </Typography>
+              <span className='block text-right text-secondary-amber'>내 답변</span>
               <div className='py-2'>
                 <Typography weight='normal' color='gray-700' align='right'>
                   {interviewQna.answer}

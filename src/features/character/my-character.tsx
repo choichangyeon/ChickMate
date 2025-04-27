dc02fe8af3ffc0016b78fea4914314abb16f4185
Minjo Park <consistent.312@gmail.com>
2025-04-27 16:12:55 +0900

    design:#201 배경 이미지 적용

--- src/features/character/my-character.tsx
+++ src/features/character/my-character.tsx
@@ -23,8 +23,8 @@ const MyCharacter = ({ session, characterData = defaultCharacter, overlayText, r
     session,
   });
   return (
-    <>
-      <div className='flex w-full flex-col items-center justify-center gap-4'>
+    <div className='flex w-full flex-col items-center justify-center gap-4'>
+      <div className='flex h-[226px] w-full items-center justify-center bg-[url("/assets/visual_assets.png")] bg-cover'>
         <Image
           src={`/assets/character/card/${type}-level${level}.png`}
           width={200}
@@ -32,32 +32,32 @@ const MyCharacter = ({ session, characterData = defaultCharacter, overlayText, r
           alt={characterName}
           priority
         />
-        <div className='flex w-full items-center'>
-          <div className='flex-1 border-t'></div>
-        </div>
-        <div className='flex w-full items-center gap-8'>
-          <div className='flex w-full flex-col'>
-            <div className='flex w-full items-center justify-between gap-6'>
-              <div className='flex items-center justify-center gap-3'>
-                <Typography weight='black' size='xl' color='primary-600'>
-                  LV {level}
-                </Typography>
-                <div className='line-clamp-1 min-w-56'>
-                  <Typography weight='bold'>{session?.user.name}님</Typography>
-                </div>
-              </div>
-              <Typography weight='normal'>{characterName}</Typography>
-            </div>
-            <CharacterExpBar type='main' percent={percent} />
-            <div className='text-secondary-yellow'>
-              <Typography weight='normal' color='gray-500' size='xs' align='center'>
-                앞으로 {remainingExp} 경험치만 더 쌓으면 레벨이 올라요!
+      </div>
+      <div className='flex w-full items-center'>
+        <div className='flex-1 border-t'></div>
+      </div>
+      <div className='flex w-full items-center gap-8'>
+        <div className='flex w-full flex-col'>
+          <div className='flex w-full items-center justify-between gap-6'>
+            <div className='flex items-center justify-center gap-3'>
+              <Typography weight='black' size='xl' color='primary-600'>
+                LV {level}
               </Typography>
+              <div className='line-clamp-1 min-w-56'>
+                <Typography weight='bold'>{session?.user.name}님</Typography>
+              </div>
             </div>
+            <Typography weight='normal'>{characterName}</Typography>
+          </div>
+          <CharacterExpBar type='main' percent={percent} />
+          <div className='text-secondary-yellow'>
+            <Typography weight='normal' color='gray-500' size='xs' align='center'>
+              앞으로 {remainingExp} 경험치만 더 쌓으면 레벨이 올라요!
+            </Typography>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 

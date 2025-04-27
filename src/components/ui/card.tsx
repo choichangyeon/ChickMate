280185f425162b00a8ba3b9897efa3bbd4ef3c72
ImJaeOne <dlawi0108@naver.com>
2025-04-27 23:23:38 +0900

    design:#224 Card w-full 추가

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -20,10 +20,9 @@ const Card = ({ children, className, onClick }: Props) => {
 };
 
 export default Card;
-//TODO: bg-emerald-900/0 -> color 수정, outline-gray-200 -> color 수정
 const cardClassName = clsx(
   'relative flex-shrink-0 items-start justify-between rounded-lg bg-cool-gray-10 outline outline-1 outline-offset-[-1px] outline-cool-gray-200',
   'min-w-[380px] min-h-[180px] p-6'
 );
 
-const contentClassName = clsx('flex h-full flex-col justify-between');
+const contentClassName = clsx('flex h-full w-full flex-col justify-between');

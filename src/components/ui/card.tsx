e63412c454dd813eb1679c424ea2bbcf3a944436
ImJaeOne <dlawi0108@naver.com>
2025-04-27 23:05:24 +0900

    design:#224 색상 변경

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -22,7 +22,7 @@ const Card = ({ children, className, onClick }: Props) => {
 export default Card;
 //TODO: bg-emerald-900/0 -> color 수정, outline-gray-200 -> color 수정
 const cardClassName = clsx(
-  'relative flex-shrink-0 items-start justify-between rounded-lg bg-emerald-900/0 outline outline-1 outline-offset-[-1px] outline-gray-200',
+  'relative flex-shrink-0 items-start justify-between rounded-lg bg-cool-gray-10 outline outline-1 outline-offset-[-1px] outline-cool-gray-200',
   'min-w-[380px] min-h-[180px] p-6'
 );
 

eb6109a57ac3550329a869f9357c2651ab05642a
choichangyeon <changyon99@naver.com>
2025-04-28 20:32:43 +0900

    refactor:#188 prefetch 수정

--- src/app/(with-nav)/(with-header)/interview/start/page.tsx
+++ src/app/(with-nav)/(with-header)/interview/start/page.tsx
@@ -3,13 +3,14 @@ import Typography from '@/components/ui/typography';
 import ResumeCardsBox from '@/features/interview/resume-cards-box';
 import { QUERY_KEY } from '@/constants/query-key';
 import { QueryClient, HydrationBoundary, dehydrate } from '@tanstack/react-query';
-import { getResumeList } from '@/features/resume/api/client-services';
+
 import { RESUME_STATUS } from '@/constants/resume-constants';
 import AlertInProgress from '@/features/interview/alert-in-progress';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/utils/auth-option';
 import ResumeAllButton from '@/features/interview/resume-all-button';
 import { Metadata } from 'next';
+import { getResumeList } from '@/features/resume/api/server-services';
 
 export const metadata: Metadata = {
   title: 'AI 면접',
@@ -17,14 +18,14 @@ export const metadata: Metadata = {
 };
 
 const InterviewStartPage = async () => {
-  const { RESUME_SUBMIT } = QUERY_KEY;
+  const { RESUMES } = QUERY_KEY;
   const { SUBMIT } = RESUME_STATUS;
 
   const queryClient = new QueryClient();
   const session = await getServerSession(authOptions);
 
   await queryClient.prefetchQuery({
-    queryKey: [RESUME_SUBMIT],
+    queryKey: [RESUMES],
     queryFn: () => getResumeList(SUBMIT),
   });
 

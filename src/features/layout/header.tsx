2f1f2974d37fe9bca9613a25984b06374d2a3a68
Hye10 <dhye410@daum.net>
2025-04-29 09:57:14 +0900

    feature:#243 mobile에서 header back-button 추가

--- src/features/layout/header.tsx
+++ src/features/layout/header.tsx
@@ -2,13 +2,17 @@ import HeaderCharacter from '@/features/character/header-character';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/utils/auth-option';
 import { HeaderTitle } from './header-title';
+import BackButton from './back-button';
 
 export const Header = async () => {
   const session = await getServerSession(authOptions);
 
   return (
-    <header className='mobile:w-full mobile:px-3 fixed z-modal flex h-20 w-[calc(100vw-52px)] items-center justify-between border-b border-cool-gray-500 bg-white py-4'>
-      <HeaderTitle />
+    <header className='fixed z-modal flex h-20 w-[calc(100vw-52px)] items-center justify-between border-b border-cool-gray-500 bg-white py-4 mobile:w-full mobile:px-3'>
+      <div className='mobile:flex mobile:gap-2'>
+        <BackButton />
+        <HeaderTitle />
+      </div>
       <HeaderCharacter session={session} />
     </header>
   );

896bc94a9145520cb9171b9e18d308d923c8938f
Hye10 <dhye410@daum.net>
2025-04-30 00:29:29 +0900

    style:#243 메인 페이지 기기별 배경화면 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,24 +5,24 @@ import { PATH } from '@/constants/path-constant';
 const { ON_BOARDING } = PATH;
 const Home = () => {
   return (
-    <main className='flex w-screen flex-col items-center justify-center bg-[url("/assets/background.png"),url("/assets/sub_background.png")] bg-cover bg-center'>
-      <div className='mobile:text-2xl mb-7 break-words text-6xl font-thin tablet:text-4xl'>
+    <main className='main-bg-class tablet:tablet-bg-class mobile:mobile-bg-class flex w-screen flex-col items-center justify-center'>
+      <div className='mb-7 break-words text-6xl font-thin mobile:text-2xl tablet:text-4xl'>
         우리는 언제 불닭이 될 수 있을까?
         <br />
       </div>
-      <div className='mobile:text-2xl mb-7 flex items-center text-6xl font-thin tablet:text-4xl'>
+      <div className='mb-7 flex items-center text-6xl font-thin mobile:text-2xl tablet:text-4xl'>
         스마트 면접 코칭
-        <Typography as='h1' className='mobile:text-2xl text-6xl font-black text-primary-orange-600 tablet:text-4xl'>
+        <Typography as='h1' className='text-6xl font-black text-primary-orange-600 mobile:text-2xl tablet:text-4xl'>
           Chick Mate
         </Typography>
       </div>
 
-      <div className='mobile:text-lg mb-16 text-xl font-thin tablet:text-2xl'>
+      <div className='mb-16 text-xl font-thin mobile:text-lg tablet:text-2xl'>
         칰메이트와 함께하는 <span className='font-semibold'>AI 스마트 에이전트!</span>
       </div>
       <Link
         href={ON_BOARDING}
-        className='button-hover-focus mobile:text-xl mobile:w-[70vw] block w-[536px] rounded-[50px] border border-cool-gray-900 p-[10px] text-center text-2xl font-thin'
+        className='button-hover-focus block w-[536px] rounded-[50px] border border-cool-gray-900 p-[10px] text-center text-2xl font-thin mobile:w-[70vw] mobile:text-xl'
       >
         지금 바로 시작해볼까요?
       </Link>

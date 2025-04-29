2b1fcfca26e6ac2d100a080a57b32f1e007a69cd
Hye10 <dhye410@daum.net>
2025-04-29 09:42:37 +0900

    style:#243 on-boarding page tablet,mobile 반응형 추가

--- src/app/(with-nav)/on-boarding/page.tsx
+++ src/app/(with-nav)/on-boarding/page.tsx
@@ -8,16 +8,16 @@ const OnBoarding = async () => {
   const session = await getServerSession(authOptions);
 
   return (
-    <main className='flex h-[100dvh] w-full items-center justify-center'>
-      <div className='flex w-[62%] max-w-[893px] flex-col justify-center'>
+    <main className='flex h-[100dvh] w-full items-center justify-center mobile:p-4'>
+      <div className='flex w-[62%] max-w-[893px] flex-col justify-center mobile:w-full mobile:max-w-full tablet:w-[90%]'>
         {/* ----- Intro 영역 ----- */}
-        <div className='mb-9'>
-          <div className='text-3xl font-bold'>오늘도 힘차게 성장해 볼까요?</div>
-          <div className='text-xl text-cool-gray-500'>우리는 언제 불닭이 될 수 있을까?</div>
+        <div className='mb-9 mobile:mb-4 mobile:text-center'>
+          <div className='text-3xl font-bold mobile:text-lg'>오늘도 힘차게 성장해 볼까요?</div>
+          <div className='text-base text-cool-gray-500 mobile:text-sm'>우리는 언제 불닭이 될 수 있을까?</div>
         </div>
 
         {/* ----- 본문 영역 ----- */}
-        <div className='flex h-[398px] items-center justify-between gap-5'>
+        <div className='flex h-full items-center justify-between gap-5 mobile:w-full mobile:flex-col-reverse mobile:gap-3 desktop:h-[398px]'>
           <div className='flex h-full flex-1 flex-col justify-between'>
             {onBoardingList.map((list, index) => (
               <ListCard key={`on_boarding_list_${index}`} {...list} />
--- src/features/on-boarding/list-card.tsx
+++ src/features/on-boarding/list-card.tsx
@@ -5,13 +5,16 @@ import type { OnBoardingCard } from '@/features/on-boarding/data/on-boarding-lis
 const ListCard = ({ title, content, icon, path }: OnBoardingCard) => {
   return (
     <Link href={path}>
-      <section role='link' className='flex h-[122px] items-center rounded-xl border p-4'>
-        <div className='mr-4'>{icon}</div>
+      <section
+        role='link'
+        className='mb-4 flex h-[122px] items-center rounded-xl border p-4 mobile:mb-2 mobile:h-24 mobile:min-w-72 tablet:h-[150px] desktop:mb-0'
+      >
+        <div className='mr-4 mobile:mr-0'>{icon}</div>
         <div>
-          <Typography as='h2' weight='bold'>
+          <Typography as='h2' className='font-bold mobile:text-sm'>
             {title}
           </Typography>
-          <p className='mt-1 whitespace-pre-line'>{content}</p>
+          <p className='mt-1 whitespace-pre-line mobile:hidden'>{content}</p>
         </div>
       </section>
     </Link>

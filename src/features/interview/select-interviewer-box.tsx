2e3d1fefe5ffbc79f2f2090cf3e96b1f4175cd8a
choichangyeon <changyon99@naver.com>
2025-04-29 10:40:56 +0900

    feat:#239 반응형 면접관 선택 구현

--- src/features/interview/select-interviewer-box.tsx
+++ src/features/interview/select-interviewer-box.tsx
@@ -10,7 +10,9 @@ import { useInterviewStore } from '@/store/use-interview-store';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import Sunset from '@/lottie/sunset.json';
+import Sunset2 from '@/lottie/sunset_2.json';
 import Pressure from '@/lottie/pressure.json';
+import Pressure2 from '@/lottie/pressure_2.json';
 import LottieAnimation from '@/components/common/lottie-animation';
 import { Session } from 'next-auth';
 import { Notify } from 'notiflix';
@@ -22,7 +24,7 @@ const {
 } = AUTH_MESSAGE;
 const { LIVE } = PATH.INTERVIEW;
 
-const activeClass = 'border-primary-orange-600 bg-cool-gray-10';
+const activeClass = 'border-primary-orange-600 bg-cool-gray-10 border-2 shadow-lg';
 const deActiveClass = 'border-cool-gray-300';
 
 type Props = {
@@ -51,25 +53,24 @@ const SelectInterviewerBox = ({ session }: Props) => {
       }
     }
   };
-
   return (
-    <section className='flex w-full flex-wrap gap-5'>
-      <aside className='flex h-80 items-start justify-start gap-5 tablet:w-full tablet:justify-evenly mobile:flex-wrap mobile:justify-center'>
+    <section className='flex w-full flex-wrap gap-5 mobile:flex-col'>
+      {/* desktop & tablet */}
+      <aside className='flex h-80 items-start justify-start gap-5 tablet:h-64 tablet:w-full mobile:hidden'>
         <div
           onClick={() => setInterviewType(CALM)}
-          className={`w-88 h-72 flex-shrink-0 cursor-pointer overflow-hidden rounded-lg border tablet:self-stretch mobile:w-56 ${interviewType === CALM ? activeClass : deActiveClass}`}
+          className={`w-88 h-72 flex-shrink-0 cursor-pointer overflow-hidden rounded-lg border tablet:h-56 tablet:w-72 ${interviewType === CALM ? activeClass : deActiveClass}`}
         >
           <LottieAnimation active={interviewType === CALM} animationData={Sunset} />
         </div>
         <div
           onClick={() => setInterviewType(PRESSURE)}
-          className={`w-88 h-72 flex-shrink-0 cursor-pointer overflow-hidden rounded-lg border tablet:self-stretch mobile:w-56 ${interviewType === PRESSURE ? activeClass : deActiveClass}`}
+          className={`w-88 h-72 flex-shrink-0 cursor-pointer overflow-hidden rounded-lg border tablet:h-56 tablet:w-72 ${interviewType === PRESSURE ? activeClass : deActiveClass}`}
         >
           <LottieAnimation active={interviewType === PRESSURE} animationData={Pressure} speed={0.4} />
         </div>
       </aside>
-      <aside className='hidden mobile:block'> 예시</aside>
-      <aside className='flex h-72 w-80 min-w-36 flex-col items-center justify-center overflow-hidden rounded-lg border border-secondary-amber bg-cool-gray-10 tablet:h-52 tablet:w-full mobile:hidden'>
+      <aside className='flex h-72 w-80 min-w-36 flex-col items-center justify-center overflow-hidden rounded-lg border border-secondary-amber bg-cool-gray-10 tablet:h-52 tablet:w-[600px] mobile:hidden'>
         <div className='mb-4'>
           {/* TODO: 이 부분은 면접관을 설명하는 부분, 추후 수정 가능 */}
           {interviewType === 'calm' ? (
@@ -100,6 +101,38 @@ const SelectInterviewerBox = ({ session }: Props) => {
                 통해 면접자를 평가합니다.
               </Typography>
             </>
+          )}
+        </div>
+        {resumeId && (
+          <Button onClick={handleClickSetInterviewType} fontWeight='bold'>
+            면접 시작하기
+          </Button>
+        )}
+      </aside>
+      {/* mobile */}
+      <aside className='hidden w-full flex-wrap items-center justify-center gap-5 mobile:flex'>
+        <div
+          onClick={() => setInterviewType(CALM)}
+          className={`h-40 w-56 flex-shrink-0 cursor-pointer overflow-hidden rounded-lg border ${interviewType === CALM ? activeClass : deActiveClass}`}
+        >
+          <LottieAnimation active={interviewType === CALM} animationData={Sunset2} />
+        </div>
+        <div
+          onClick={() => setInterviewType(PRESSURE)}
+          className={`h-40 w-56 flex-shrink-0 cursor-pointer overflow-hidden rounded-lg border ${interviewType === PRESSURE ? activeClass : deActiveClass}`}
+        >
+          <LottieAnimation active={interviewType === PRESSURE} animationData={Pressure2} />
+        </div>
+      </aside>
+      <aside className='hidden w-full flex-col items-center justify-center gap-2 mobile:flex'>
+        <div className='flex flex-row'>
+          <Typography weight='black' color='primary-600'>
+            Chick Mate&nbsp;
+          </Typography>
+          {interviewType === 'calm' ? (
+            <Typography weight='bold'>햇살 면접관</Typography>
+          ) : (
+            <Typography weight='bold'>불타는 면접관</Typography>
           )}
         </div>
         {resumeId && (

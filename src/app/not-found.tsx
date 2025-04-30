fe03ccff8b0bffcb90704ed4f97f3b3db2c150f2
choichangyeon <changyon99@naver.com>
2025-04-30 11:25:08 +0900

    feat:#253 NOT FOUND 페이지 구현

---
+++ src/app/not-found.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image';
+import MainLayout from '@/app/(with-nav)/layout';
+import BlockComponent from '@/components/common/block-component';
+import { PATH } from '@/constants/path-constant';
+
+const { ON_BOARDING } = PATH;
+
+const NotFound = () => {
+  return (
+    <MainLayout>
+      <section className='flex h-full flex-col items-center justify-center'>
+        <div>
+          <Image src='/assets/notfound.png' width={480} height={310} alt='404 NOT FOUND' />
+          <BlockComponent
+            firstLine=''
+            secondLine='이런! 페이지를 찾을 수 없습니다'
+            thirdLine='괜찮아요. 우리는 언제나 길을 잃을 수 있어요.'
+            buttonName='메인으로 돌아가기'
+            href={ON_BOARDING}
+            className='h-[180px] mobile:h-[80px]'
+          />
+        </div>
+      </section>
+    </MainLayout>
+  );
+};
+
+export default NotFound;

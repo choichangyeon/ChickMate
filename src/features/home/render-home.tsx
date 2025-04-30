455a2c6770d51e525e725f8a468ea9894729f8c5
Hye10 <dhye410@daum.net>
2025-04-30 16:29:12 +0900

    bugfix:#243 새로고침 시에 desktop 화면 잠시 노출되는 오류 수정

--- src/features/home/render-home.tsx
+++ src/features/home/render-home.tsx
@@ -3,9 +3,11 @@ import { DEVICE, useDeviceType } from '@/hooks/customs/use-device-type';
 import DesktopHome from '@/features/home/desktop-home';
 import TabletHome from '@/features/home/tablet-home';
 import MobileHome from './mobile-home';
+import LoadingAnimation from '@/components/common/loading-animation';
 const { MOBILE, TABLET, DESKTOP } = DEVICE;
 const RenderHome = () => {
   const device = useDeviceType();
+  if (!device) return <LoadingAnimation />;
   switch (device) {
     case DESKTOP:
       return <DesktopHome />;
--- src/hooks/customs/use-device-type.ts
+++ src/hooks/customs/use-device-type.ts
@@ -1,6 +1,5 @@
 'use client';
-import { useEffect, useState } from 'react';
-import { useFuncDebounce } from './use-func-debounce';
+import { useEffect, useLayoutEffect, useState } from 'react';
 
 export const DEVICE = {
   MOBILE: 'mobile',
@@ -16,17 +15,19 @@ const TABLET_MIN = 744;
 const MOBILE_MAX = 743;
 
 const getDeviceType = () => {
+  if (typeof window === 'undefined') return null;
   const width = window.innerWidth;
   if (width <= MOBILE_MAX) return MOBILE;
   if (width <= TABLET_MAX && width >= TABLET_MIN) return TABLET;
   else return DESKTOP;
 };
 
-export const useDeviceType = (): DeviceType => {
-  const [deviceType, setDeviceType] = useState<DeviceType>(DESKTOP);
+export const useDeviceType = () => {
+  const [deviceType, setDeviceType] = useState<DeviceType | null>(() => getDeviceType());
 
   useEffect(() => {
-    const handleResize = () => setDeviceType(getDeviceType());
+    const handleResize = () => setDeviceType(getDeviceType);
+    handleResize();
     window.addEventListener('resize', handleResize);
     return () => window.removeEventListener('resize', handleResize);
   }, []);
